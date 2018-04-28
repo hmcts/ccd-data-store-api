f6573416d03957354a8eb36043220882ed771508
Adrian Cho <waihung.cho@hmcts.net>
2018-04-28 21:19:51 +0100

    Add timeout test case

--- src/test/java/uk/gov/hmcts/ccd/RestTemplateConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/ccd/RestTemplateConfigurationTest.java
@@ -9,22 +9,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.apache.http.protocol.HTTP.CONTENT_TYPE;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.PUT;
+import static wiremock.org.apache.http.entity.ContentType.TEXT_PLAIN;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@TestPropertySource(properties = {"http.client.connection.timeout=1500"})
 public class RestTemplateConfigurationTest {
 
     @Rule
@@ -33,26 +41,34 @@ public class RestTemplateConfigurationTest {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Test
-    public void restTemplateShouldBeUsable() {
+    private static final String RESPONSE_BODY = "Of course la la land";
+    private static final String URL = "/ng/itb";
+    private static final String MIME_TYPE = TEXT_PLAIN.getMimeType();
+
+    @Test public void restTemplateShouldBeUsable() {
         assertNotNull(restTemplate);
 
-        final String body = "Of course la la land";
-        final String contentType = "Content-Type";
-        final String textPlain = "text/plain";
-        final int statusCode = 200;
-        final String url = "/ng/itb";
-
-        stubFor(put(urlEqualTo(url)).willReturn(aResponse().withStatus(statusCode)
-                                                                 .withHeader(contentType, textPlain)
-                                                                 .withBody(body)));
+        stubFor(put(urlEqualTo(URL)).willReturn(aResponse().withStatus(SC_OK)
+                                                           .withHeader(CONTENT_TYPE, MIME_TYPE)
+                                                           .withBody(RESPONSE_BODY)));
         RequestEntity<String>
             request =
-            new RequestEntity<>("things", PUT, URI.create("http://localhost:" + wireMockServer.port() + url));
+            new RequestEntity<>(PUT, URI.create("http://localhost:" + wireMockServer.port() + URL));
 
         ResponseEntity<String> response = restTemplate.exchange(request, String.class);
-        assertThat(response.getBody(), is(body));
-        assertThat(response.getHeaders().get(contentType), contains(textPlain));
-        assertThat(response.getStatusCode().value(), is(statusCode));
+        assertThat(response.getBody(), is(RESPONSE_BODY));
+        assertThat(response.getHeaders().get(CONTENT_TYPE), contains(MIME_TYPE));
+        assertThat(response.getStatusCode().value(), is(SC_OK));
+    }
+
+    @Test(expected = ResourceAccessException.class) public void shouldTimeOut() {
+        assertNotNull(restTemplate);
+        stubFor(get(urlEqualTo(URL)).willReturn(aResponse().withStatus(SC_OK).withFixedDelay(2000)));
+
+        RequestEntity<String>
+            request =
+            new RequestEntity<>(GET, URI.create("http://localhost:" + wireMockServer.port() + URL));
+
+        restTemplate.exchange(request, String.class);
     }
 }
