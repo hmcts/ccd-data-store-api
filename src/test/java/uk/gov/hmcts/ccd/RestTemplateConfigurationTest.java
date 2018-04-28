d143c7ce597f282473108326f5a0379137d53406
Adrian Cho <waihung.cho@hmcts.net>
2018-04-28 22:48:23 +0100

    Added test case ascertaining that the restTemplate can be used more than once

--- src/test/java/uk/gov/hmcts/ccd/RestTemplateConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/ccd/RestTemplateConfigurationTest.java
@@ -15,24 +15,34 @@ import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.apache.http.protocol.HTTP.CONTENT_TYPE;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.PUT;
+import static wiremock.com.google.common.collect.Lists.newArrayList;
 import static wiremock.org.apache.http.entity.ContentType.TEXT_PLAIN;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@TestPropertySource(properties = {"http.client.connection.timeout=1500"})
+@TestPropertySource(properties = {"http.client.connection.timeout=1500",
+    "http.client.max.total=1",
+    "http.client.seconds.idle.connection=1",
+    "http.client.max.client_per_route=2",
+    "http.client.validate.after.inactivity=1"})
 public class RestTemplateConfigurationTest {
 
     @Rule
@@ -45,30 +55,66 @@ public class RestTemplateConfigurationTest {
     private static final String URL = "/ng/itb";
     private static final String MIME_TYPE = TEXT_PLAIN.getMimeType();
 
-    @Test public void restTemplateShouldBeUsable() {
+    @Test
+    public void restTemplateShouldBeUsable() {
         assertNotNull(restTemplate);
 
-        stubFor(put(urlEqualTo(URL)).willReturn(aResponse().withStatus(SC_OK)
-                                                           .withHeader(CONTENT_TYPE, MIME_TYPE)
-                                                           .withBody(RESPONSE_BODY)));
-        RequestEntity<String>
+        stubResponse();
+
+        final RequestEntity<String>
             request =
             new RequestEntity<>(PUT, URI.create("http://localhost:" + wireMockServer.port() + URL));
 
-        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
-        assertThat(response.getBody(), is(RESPONSE_BODY));
-        assertThat(response.getHeaders().get(CONTENT_TYPE), contains(MIME_TYPE));
-        assertThat(response.getStatusCode().value(), is(SC_OK));
+        final ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+        assertResponse(response);
     }
 
-    @Test(expected = ResourceAccessException.class) public void shouldTimeOut() {
+    @Test(expected = ResourceAccessException.class)
+    public void shouldTimeOut() {
         assertNotNull(restTemplate);
         stubFor(get(urlEqualTo(URL)).willReturn(aResponse().withStatus(SC_OK).withFixedDelay(2000)));
 
-        RequestEntity<String>
+        final RequestEntity<String>
             request =
             new RequestEntity<>(GET, URI.create("http://localhost:" + wireMockServer.port() + URL));
 
         restTemplate.exchange(request, String.class);
+    }
+
+    @Test
+    public void shouldBeAbleToUseMultipleTimes() throws Exception {
+        stubResponse();
+        final List<Future<Integer>> futures = newArrayList();
+        final ExecutorService executorService = Executors.newFixedThreadPool(25);
+        final int totalNumberOfCalls = 200;
+
+        for (int i = 0; i < totalNumberOfCalls; i++) {
+            futures.add(executorService.submit(() -> {
+                final RequestEntity<String>
+                    request =
+                    new RequestEntity<>(PUT, URI.create("http://localhost:" + wireMockServer.port() + URL));
+                final ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+                assertResponse(response);
+                return response.getStatusCode().value();
+            }));
+        }
+
+        assertThat(futures, hasSize(totalNumberOfCalls));
+
+        for (Future<Integer> future: futures) {
+            assertThat(future.get(), is(SC_OK));
+        }
+    }
+
+    private void stubResponse() {
+        stubFor(put(urlEqualTo(URL)).willReturn(aResponse().withStatus(SC_OK)
+                                                           .withHeader(CONTENT_TYPE, MIME_TYPE)
+                                                           .withBody(RESPONSE_BODY)));
+    }
+
+    private void assertResponse(final ResponseEntity<String> response) {
+        assertThat(response.getBody(), is(RESPONSE_BODY));
+        assertThat(response.getHeaders().get(CONTENT_TYPE), contains(MIME_TYPE));
+        assertThat(response.getStatusCode().value(), is(SC_OK));
     }
 }
