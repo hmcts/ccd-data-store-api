752a7e4129a6ab25298a17f4b44a93d377c05de2
Adrian Cho <waihung.cho@hmcts.net>
2018-05-09 11:51:34 +0100

    Try to get preview to build

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -91,29 +91,29 @@ module "ccd-data-store-vault" {
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name = "${local.app_full_name}-POSTGRES-USER"
   value = "${module.ccd-data-store-api.user_name}"
-  vault_uri = "${module.ccd-data-store-vault.key_vault_uri}"
+  vault_uri = "${local.vaultUri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name = "${local.app_full_name}-POSTGRES-PASS"
   value = "${module.ccd-data-store-api.postgresql_password}"
-  vault_uri = "${module.ccd-data-store-vault.key_vault_uri}"
+  vault_uri = "${local.vaultUri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name = "${local.app_full_name}-POSTGRES-HOST"
   value = "${module.ccd-data-store-api.host_name}"
-  vault_uri = "${module.ccd-data-store-vault.key_vault_uri}"
+  vault_uri = "${local.vaultUri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   name = "${local.app_full_name}-POSTGRES-PORT"
   value = "${module.ccd-data-store-api.postgresql_listen_port}"
-  vault_uri = "${module.ccd-data-store-vault.key_vault_uri}"
+  vault_uri = "${local.vaultUri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name = "${local.app_full_name}-POSTGRES-DATABASE"
   value = "${module.ccd-data-store-api.postgresql_database}"
-  vault_uri = "${module.ccd-data-store-vault.key_vault_uri}"
+  vault_uri = "${local.vaultUri}"
 }
