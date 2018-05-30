1600a1315174e1cc9ce1129816a3627fd25aec94
Valentin Laurin <me@vlaurin.eu>
2018-05-30 23:27:57 +0100

    Use OAuth2 flow for functional tests

---
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/tests/Env.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.ccd.datastore.tests;
+
+public class Env {
+
+    public static String require(String name) {
+        final String value = System.getenv(name);
+        if (null == value) {
+            throw new IllegalStateException(String.format("Environment variable `%s` is required", name));
+        }
+        return value;
+    }
+}
