f7d3da46c3fcf1c3e59a920f5fee16c12a164220
andrewf <andrzej.folga@hmcts.net>
2018-07-10 18:55:29 +0100

    RDM-2440 Code cleanup.

--- src/main/java/uk/gov/hmcts/ccd/domain/service/search/PaginatedSearchMetaDataOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/search/PaginatedSearchMetaDataOperation.java
@@ -3,30 +3,20 @@ package uk.gov.hmcts.ccd.domain.service.search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.ccd.ApplicationParams;
 import uk.gov.hmcts.ccd.data.casedetails.CachedCaseDetailsRepository;
 import uk.gov.hmcts.ccd.data.casedetails.CaseDetailsRepository;
 import uk.gov.hmcts.ccd.data.casedetails.search.MetaData;
 import uk.gov.hmcts.ccd.data.casedetails.search.PaginatedSearchMetadata;
-import uk.gov.hmcts.ccd.domain.model.draft.DraftResponse;
-import uk.gov.hmcts.ccd.domain.service.getdraft.DefaultGetDraftsOperation;
-import uk.gov.hmcts.ccd.domain.service.getdraft.GetDraftsOperation;
 
 import java.util.Map;
 
 @Service
 public class PaginatedSearchMetaDataOperation {
-    private final GetDraftsOperation getDraftsOperation;
     private final CaseDetailsRepository caseDetailsRepository;
-    private final ApplicationParams applicationParams;
 
     @Autowired
-    public PaginatedSearchMetaDataOperation(@Qualifier(DefaultGetDraftsOperation.QUALIFIER) GetDraftsOperation getDraftsOperation,
-                                            @Qualifier(CachedCaseDetailsRepository.QUALIFIER) final CaseDetailsRepository caseDetailsRepository,
-                                            final ApplicationParams applicationParams) {
-        this.getDraftsOperation = getDraftsOperation;
+    public PaginatedSearchMetaDataOperation(@Qualifier(CachedCaseDetailsRepository.QUALIFIER) final CaseDetailsRepository caseDetailsRepository) {
         this.caseDetailsRepository = caseDetailsRepository;
-        this.applicationParams = applicationParams;
     }
 
     public PaginatedSearchMetadata execute(MetaData metaData, Map<String, String> criteria) {
