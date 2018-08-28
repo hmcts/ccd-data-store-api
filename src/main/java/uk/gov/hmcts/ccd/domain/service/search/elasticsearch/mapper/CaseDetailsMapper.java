d6cc0c20bbab84c417644365906c9b031e79e3c2
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-08-28 14:56:24 +0100

    polish logging configuration

--- src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/mapper/CaseDetailsMapper.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/mapper/CaseDetailsMapper.java
@@ -1,10 +1,11 @@
-package uk.gov.hmcts.ccd.domain.service.search.elasticsearch.dto;
+package uk.gov.hmcts.ccd.domain.service.search.elasticsearch.mapper;
 
 import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
+import uk.gov.hmcts.ccd.domain.service.search.elasticsearch.dto.ElasticSearchCaseDetailsDTO;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface CaseDetailsMapper {
