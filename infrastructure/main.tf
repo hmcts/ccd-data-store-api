50c22c422e068dfa6f2d2220957dcc34b6aa4aed
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-09-10 14:33:25 +0100

    Infra: remove Hashi vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,13 +1,3 @@
-provider "vault" {
-  //  # It is strongly recommended to configure this provider through the
-  //  # environment variables described above, so that each user can have
-  //  # separate credentials set in the environment.
-  //  #
-  //  # This will default to using $VAULT_ADDR
-  //  # But can be set explicitly
-  address = "https://vault.reform.hmcts.net:6200"
-}
-
 locals {
   app_full_name = "${var.product}-${var.component}"
 
