a4baa300aca64e8c4540d957066403796932ac6d
Valentin Laurin <me@vlaurin.eu>
2018-09-25 11:51:47 +0100

    RDM-2736: Fix Sonar quality
    Move constants to a class or enum

--- src/main/java/uk/gov/hmcts/ccd/v2/V2.java
+++ src/main/java/uk/gov/hmcts/ccd/v2/V2.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.ccd.v2;
 
-public interface V2 {
+public final class V2 {
 
-    String EXPERIMENTAL_HEADER = "experimental";
-    String EXPERIMENTAL_WARNING = "Experimental! Subject to change or removal, do not use in production!";
+    public static final String EXPERIMENTAL_HEADER = "experimental";
+    public static final String EXPERIMENTAL_WARNING = "Experimental! Subject to change or removal, do not use in production!";
 
-    interface MediaType {
-        String CASE = "application/vnd.uk.gov.hmcts.ccd-data-store-api.case.v2+json;charset=UTF-8";
+    public final class MediaType {
+        public static final String CASE = "application/vnd.uk.gov.hmcts.ccd-data-store-api.case.v2+json;charset=UTF-8";
     }
 }
