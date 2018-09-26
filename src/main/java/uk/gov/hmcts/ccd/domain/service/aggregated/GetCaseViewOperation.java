ef92a6aae3bb8855ed56f5bc4a027681fc9c985a
Valentin Laurin <me@vlaurin.eu>
2018-09-26 14:24:51 +0100

    Merge Pull Request #198 from RDM-2737
    [PREVIEW] RDM-2737: API v2: Get Case view (#198)
    * RDM-2736: Enable HATEOAS with HAL
    
    Add dependency to org.springframework.hateoas
    Enable HAL support in Spring
    Configure HAL ObjectMapper
    
    * RDM-2736: Create API v2
    
    Add `GET /cases/{caseId}` endpoint with partial implementation
    
    * RDM-2736: Test CaseController
    
    Test endpoint `GET /cases/{caseId}` for 200 and 404 paths
    
    * RDM-2376: Complete CaseResource
    
    Add missing CaseResource properties
    Test CaseResource mapping and links
    
    * RDM-2736: Test HAL ObjectMapper config
    
    * RDM-2376: Add lombok config for Jacoco
    
    * RDM-2736: Add functional tests
    
    Add functional tests for GET case by ID v2
    
    * RDM-2736: Validate case reference in controller
    
    * RDM-2736: Move to external API
    
    Add explicit external API package
    Move v2 CaseController to external API
    
    Make functional tests use `experimental` and `accept` headers
    
    * RDM-2736: Fix codacy issues
    
    * RDM-2737: Get case view by case reference only
    
    Implement `execute(caseReference)` in GetCaseViewOperation and its implementations
    
    * RDM-2737: Add GET /internal/cases/{caseId}
    
    Add internal UICaseController
    Add experimental GET /internal/cases/{caseId} endpoint
    Add UICaseViewResource
    Register MediaType UI_CASE_VIEW
    
    * RDM-2737: Add functional tests for GET case view
    
    * RDM-2737: Fix merge conflicts
    
    Use final class instead of interface for V2 constants

--- src/main/java/uk/gov/hmcts/ccd/domain/service/aggregated/GetCaseViewOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/aggregated/GetCaseViewOperation.java
@@ -4,6 +4,16 @@ import uk.gov.hmcts.ccd.domain.model.aggregated.CaseView;
 
 public interface GetCaseViewOperation {
 
+    /**
+     *
+     * @param jurisdictionId
+     * @param caseTypeId
+     * @param caseReference
+     * @return When found, case for given reference, formatted for display
+     * @deprecated Use {@link #execute(String)} instead
+     */
+    @Deprecated
     CaseView execute(String jurisdictionId, String caseTypeId, String caseReference);
 
+    CaseView execute(String caseReference);
 }
