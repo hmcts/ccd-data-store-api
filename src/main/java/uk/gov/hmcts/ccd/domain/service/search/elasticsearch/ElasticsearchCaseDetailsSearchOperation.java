07efd09a701facd228b34c9b7c624c80b33d5882
gbenadikar <giri.benadikar@hmcts.net>
2018-09-27 12:26:38 +0100

    RDM-2811 - add security to search endpoint (Elasticsearch)

--- src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticsearchCaseDetailsSearchOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticsearchCaseDetailsSearchOperation.java
@@ -25,25 +25,6 @@ import uk.gov.hmcts.ccd.endpoint.exceptions.ServiceException;
 @Qualifier(ElasticsearchCaseDetailsSearchOperation.QUALIFIER)
 public class ElasticsearchCaseDetailsSearchOperation implements CaseDetailsSearchOperation {
 
-<<<<<<< HEAD:src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticSearchCaseDetailsSearchOperation.java
-    @Autowired
-    private ApplicationParams applicationParams;
-
-    @Autowired
-    private JestClient jestClient;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private CaseDetailsMapper caseDetailsMapper;
-
-    @Override
-    public CaseDetailsSearchResult execute(List<String> caseTypeIds, String query) throws IOException {
-
-        Search search = createSearchRequest(caseTypeIds, query);
-        SearchResult result = jestClient.execute(search);
-=======
     public static final String QUALIFIER = "ElasticsearchCaseDetailsSearchOperation";
 
     private final JestClient jestClient;
@@ -65,7 +46,6 @@ public class ElasticsearchCaseDetailsSearchOperation implements CaseDetailsSearc
     @Override
     public CaseDetailsSearchResult execute(String caseTypeId, String query) {
         SearchResult result = search(caseTypeId, query);
->>>>>>> c3eb9b8... RDM-2811 - add security to search endpoint (Elasticsearch):src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticsearchCaseDetailsSearchOperation.java
         if (result.isSucceeded()) {
             return toCaseDetailsSearchResult(result);
         } else {
@@ -73,19 +53,6 @@ public class ElasticsearchCaseDetailsSearchOperation implements CaseDetailsSearc
         }
     }
 
-<<<<<<< HEAD:src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticSearchCaseDetailsSearchOperation.java
-    private Search createSearchRequest(List<String> caseTypesId, String query) {
-        return new Search.Builder(query)
-                    .addIndices(indices(caseTypesId))
-                    .addType(applicationParams.getCasesIndexType())
-                    .build();
-    }
-
-    private List<String> indices(List<String> caseTypesId) {
-        return caseTypesId.stream().map(caseTypeId ->
-                String.format(applicationParams.getCasesIndexNameFormat(), caseTypeId))
-                .collect(toList());
-=======
     private SearchResult search(String caseTypeId, String query) {
         Search searchRequest = caseSearchRequestFactory.create(caseTypeId, query);
         try {
@@ -93,7 +60,6 @@ public class ElasticsearchCaseDetailsSearchOperation implements CaseDetailsSearc
         } catch (IOException e) {
             throw new ServiceException("Exception executing Elasticsearch : " + e.getMessage(), e);
         }
->>>>>>> c3eb9b8... RDM-2811 - add security to search endpoint (Elasticsearch):src/main/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticsearchCaseDetailsSearchOperation.java
     }
 
     private CaseDetailsSearchResult toCaseDetailsSearchResult(SearchResult result) {
@@ -104,8 +70,9 @@ public class ElasticsearchCaseDetailsSearchOperation implements CaseDetailsSearc
     }
 
     private List<ElasticSearchCaseDetailsDTO> toElasticSearchCasesDTO(List<String> cases) {
-        return cases.stream().map(Unchecked.function(caseDetail ->
-            objectMapper.readValue(caseDetail, ElasticSearchCaseDetailsDTO.class)
-        )).collect(toList());
+        return cases
+            .stream()
+            .map(Unchecked.function(caseDetail -> objectMapper.readValue(caseDetail, ElasticSearchCaseDetailsDTO.class)))
+            .collect(toList());
     }
 }
--- src/test/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticsearchCaseDetailsSearchOperationTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticsearchCaseDetailsSearchOperationTest.java
@@ -31,34 +31,12 @@ import uk.gov.hmcts.ccd.domain.service.search.filter.CaseSearchQuerySecurity;
 import uk.gov.hmcts.ccd.domain.service.search.filter.CaseSearchRequestFactory;
 import uk.gov.hmcts.ccd.endpoint.exceptions.BadSearchRequest;
 
-<<<<<<< HEAD:src/test/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticSearchCaseDetailsSearchOperationTest.java
-import java.io.IOException;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static java.util.stream.Collectors.toList;
-import static org.apache.commons.lang3.StringUtils.join;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ElasticSearchCaseDetailsSearchOperationTest {
-
-    private static final String INDEX_NAME_FORMAT = "%s_cases";
-    private static final List<String> CASE_TYPES_ID = newArrayList("caseTypeId1", "caseTypeId2");
-=======
 class ElasticsearchCaseDetailsSearchOperationTest {
 
     private static final String INDEX_NAME_FORMAT = "%s_cases";
     private static final String CASE_TYPE_ID = "casetypeid";
->>>>>>> c3eb9b8... RDM-2811 - add security to search endpoint (Elasticsearch):src/test/java/uk/gov/hmcts/ccd/domain/service/search/elasticsearch/ElasticsearchCaseDetailsSearchOperationTest.java
     private static final String INDEX_TYPE = "case";
-    private String caseDetailsElastic = "{some case details}";
+    private final String caseDetailsElastic = "{some case details}";
 
     @InjectMocks
     private ElasticsearchCaseDetailsSearchOperation searchOperation;
@@ -98,18 +76,18 @@ class ElasticsearchCaseDetailsSearchOperationTest {
         when(searchResult.getTotal()).thenReturn(1L);
         when(searchResult.getSourceAsStringList()).thenReturn(newArrayList(caseDetailsElastic));
         when(objectMapper.readValue(caseDetailsElastic, ElasticSearchCaseDetailsDTO.class))
-                .thenReturn(caseDetailsDTO);
+            .thenReturn(caseDetailsDTO);
         when(mapper.dtosToCaseDetailsList(newArrayList(caseDetailsDTO))).thenReturn(newArrayList(caseDetails));
         when(jestClient.execute(any(Search.class))).thenReturn(searchResult);
 
-        CaseDetailsSearchResult caseDetailsSearchResult = searchOperation.execute(CASE_TYPES_ID, "{query}");
+        CaseDetailsSearchResult caseDetailsSearchResult = searchOperation.execute(CASE_TYPE_ID, "{query}");
 
         assertThat(caseDetailsSearchResult.getCases(), equalTo(newArrayList(caseDetails)));
         assertThat(caseDetailsSearchResult.getTotal(), equalTo(1L));
         ArgumentCaptor<Search> arg = ArgumentCaptor.forClass(Search.class);
         verify(jestClient).execute(arg.capture());
         Search searchRequest = arg.getValue();
-        assertThat(searchRequest.getIndex(), equalTo(indices(CASE_TYPES_ID)));
+        assertThat(searchRequest.getIndex(), equalTo(toIndex(CASE_TYPE_ID)));
         assertThat(searchRequest.getType(), equalTo(INDEX_TYPE));
     }
 
@@ -119,12 +97,10 @@ class ElasticsearchCaseDetailsSearchOperationTest {
         when(searchResult.isSucceeded()).thenReturn(false);
         when(jestClient.execute(any(Search.class))).thenReturn(searchResult);
 
-        assertThrows(BadSearchRequest.class, () -> searchOperation.execute(CASE_TYPES_ID, "{query}"));
+        assertThrows(BadSearchRequest.class, () -> searchOperation.execute(CASE_TYPE_ID, "{query}"));
     }
 
-    private String indices(List<String> caseTypesId) {
-        return join(caseTypesId.stream().map(caseTypeId ->
-                String.format(INDEX_NAME_FORMAT, caseTypeId))
-                .collect(toList()), ",");
+    private String toIndex(String caseTypeId) {
+        return String.format(INDEX_NAME_FORMAT, caseTypeId);
     }
 }
