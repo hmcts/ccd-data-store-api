0e8fbd165f7b5fb0aa6c7cf87fd1319c5b132d19
gbenadikar <giri.benadikar@hmcts.net>
2018-10-02 10:43:34 +0100

    RDM-2811 - removed ambiguity on objectMapper

--- src/main/java/uk/gov/hmcts/ccd/CoreCaseDataConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/CoreCaseDataConfiguration.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.ccd;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,15 +9,10 @@ import org.springframework.context.annotation.Configuration;
 public class CoreCaseDataConfiguration {
 
     @Bean
-    @Qualifier("caseDetailsObjectMapper")
-    public ObjectMapper caseDetailsObjectMapper() {
+    public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         return mapper;
     }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 }
