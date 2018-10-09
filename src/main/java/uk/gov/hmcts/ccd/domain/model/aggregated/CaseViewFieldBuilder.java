d1e3da65646f293e5d1babfbf6f22c40b4bccd68
gbenadikar <giri.benadikar@hmcts.net>
2018-10-09 16:43:21 +0100

    RDM-1202 - added label and hint for event case fields

--- src/main/java/uk/gov/hmcts/ccd/domain/model/aggregated/CaseViewFieldBuilder.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/aggregated/CaseViewFieldBuilder.java
@@ -1,14 +1,16 @@
 package uk.gov.hmcts.ccd.domain.model.aggregated;
 
-import uk.gov.hmcts.ccd.domain.model.definition.CaseEventField;
-import uk.gov.hmcts.ccd.domain.model.definition.CaseField;
-
 import javax.inject.Named;
 import javax.inject.Singleton;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.Optional.ofNullable;
+
+import uk.gov.hmcts.ccd.domain.model.definition.CaseEventField;
+import uk.gov.hmcts.ccd.domain.model.definition.CaseField;
 
 @Named
 @Singleton
@@ -20,8 +22,8 @@ public class CaseViewFieldBuilder {
         field.setId(eventField.getCaseFieldId());
         field.setFieldType(caseField.getFieldType());
         field.setHidden(caseField.getHidden());
-        field.setHintText(caseField.getHintText());
-        field.setLabel(caseField.getLabel());
+        field.setHintText(ofNullable(eventField.getHintText()).orElse(caseField.getHintText()));
+        field.setLabel(ofNullable(eventField.getLabel()).orElse(caseField.getLabel()));
         field.setSecurityLabel(caseField.getSecurityLabel());
         field.setDisplayContext(eventField.getDisplayContext());
         field.setShowCondition(eventField.getShowCondition());
