a9550a4ced8bf514680210b322b4de3e556a5df2
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-10-24 08:07:36 +0100

    added read timeout and connection timeout to jest client

--- src/main/java/uk/gov/hmcts/ccd/ElasticSearchConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/ElasticSearchConfiguration.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.ccd;
 
+import java.util.concurrent.TimeUnit;
+
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -10,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
-
 @Configuration
 public class ElasticSearchConfiguration {
 
@@ -21,11 +21,14 @@ public class ElasticSearchConfiguration {
     @Bean
     public JestClient jestClient() {
 
+        //ensure maxConnectionIdleTime is less than the ES loadbalancer max idle connection time
         JestClientFactory factory = new JestClientFactory();
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
         factory.setHttpClientConfig(new HttpClientConfig.Builder(applicationParams.getElasticSearchHosts())
             .multiThreaded(true)
             .maxConnectionIdleTime(15, TimeUnit.SECONDS)
+            .connTimeout(20000)
+            .readTimeout(20000)
             .gson(gson).build());
         return factory.getObject();
     }
