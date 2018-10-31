cf6952972515b75d38a92aeb86e189358c99169a
gbenadikar <giri.benadikar@hmcts.net>
2018-10-31 08:03:53 +0000

    RDM-3125 - changed default logstash indexing delay to 2 seconds

--- src/aat/java/uk/gov/hmcts/ccd/datastore/tests/functional/elasticsearch/ElasticsearchHelper.java
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/tests/functional/elasticsearch/ElasticsearchHelper.java
@@ -4,7 +4,7 @@ import uk.gov.hmcts.ccd.datastore.tests.Env;
 
 final class ElasticsearchHelper {
 
-    private static final long DEFAULT_LOGSTASH_READ_DELAY_MILLIS = 3000;
+    private static final long DEFAULT_LOGSTASH_READ_DELAY_MILLIS = 2000;
 
     String getElasticsearchBaseUri() {
         return Env.require("ELASTIC_SEARCH_SCHEME") + "://" + Env.require("ELASTIC_SEARCH_HOST") + ":" + Env.require("ELASTIC_SEARCH_PORT");
