565a127ec93584de85bd6e77f3f9fd46f58cfb10
Mohammed Anas Khatri <mohammed.khatri@hmcts.net>
2018-11-12 13:43:40 +0000

    Removed Unused imports

--- src/main/java/uk/gov/hmcts/ccd/domain/types/FixedRadioListValidator.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/types/FixedRadioListValidator.java
@@ -2,17 +2,6 @@ package uk.gov.hmcts.ccd.domain.types;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-
-import static java.math.BigDecimal.ONE;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import uk.gov.hmcts.ccd.domain.model.definition.CaseField;
-import uk.gov.hmcts.ccd.domain.model.definition.FixedListItem;
 
 @Named
 @Singleton
