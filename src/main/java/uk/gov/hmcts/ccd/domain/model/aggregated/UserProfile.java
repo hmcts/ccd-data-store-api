b1fe8c37ef9bc7e220d61563b1a1d6bb1613fb95
andrewf <andrzej.folga@hmcts.net>
2018-11-16 10:37:54 +0000

    RDM-3288 Fix sonar.

--- src/main/java/uk/gov/hmcts/ccd/domain/model/aggregated/UserProfile.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/aggregated/UserProfile.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.ccd.domain.model.aggregated;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.ToString;
 
