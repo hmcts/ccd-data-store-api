08d058980aa0815d4d683c0b63a71da5a2ae107d
Hemanth Potipati <hrpotipati@gmail.com>
2018-12-10 14:59:59 +0000

    Revert "Revert "RDM-2455 CRUD contract on Collection Items - Standard API""

--- src/main/java/uk/gov/hmcts/ccd/domain/types/CollectionValidator.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/types/CollectionValidator.java
@@ -4,6 +4,8 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 import java.util.*;
 
+import static uk.gov.hmcts.ccd.domain.model.definition.FieldType.COLLECTION;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseField;
@@ -12,14 +14,13 @@ import uk.gov.hmcts.ccd.domain.model.definition.FieldType;
 @Named
 @Singleton
 public class CollectionValidator implements BaseTypeValidator {
-    private static final String TYPE_ID = "Collection";
     public static final String VALUE = "value";
     private static final String ID = "id";
     private static final String FIELD_SEPARATOR = ".";
 
     @Override
     public BaseType getType() {
-        return BaseType.get(TYPE_ID);
+        return BaseType.get(COLLECTION);
     }
 
     @Override
@@ -32,7 +33,10 @@ public class CollectionValidator implements BaseTypeValidator {
         }
 
         if (!dataValue.isArray()) {
-            return Collections.singletonList(new ValidationResult("Require value to be an array", dataFieldId));
+            final ValidationResult result = new ValidationResult(
+                "Require value to be an array",
+                dataFieldId);
+            return Collections.singletonList(result);
         }
 
         final ArrayNode arrayValue = (ArrayNode) dataValue;
