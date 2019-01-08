88a52f543837f5497992a012e2c47403c74b9185
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2019-01-08 15:16:26 +0000

    increased cache max size. Moved from time to live to max idle to take into account not only cache write access but also read access

--- src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
@@ -35,9 +35,9 @@ public class CachingConfiguration {
 
     private MapConfig newMapConfig(final String name, int definitionCacheTTL) {
         return new MapConfig().setName(name)
-                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
-                .setEvictionPolicy(EvictionPolicy.LRU)
-                .setTimeToLiveSeconds(definitionCacheTTL);
+                .setMaxSizeConfig(new MaxSizeConfig(1000, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                .setEvictionPolicy(EvictionPolicy.LFU)
+                .setMaxIdleSeconds(definitionCacheTTL);
     }
 
 }
