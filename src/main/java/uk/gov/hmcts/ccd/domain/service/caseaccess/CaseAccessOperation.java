498d4f407e8e7cd00781bd3b6d19040df94b646e
Valentin Laurin <me@vlaurin.eu>
2019-01-10 23:23:46 +0000

    RDM-2836: Split updateUserAccess method
    Introduce private function to improve legibility

--- src/main/java/uk/gov/hmcts/ccd/domain/service/caseaccess/CaseAccessOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/caseaccess/CaseAccessOperation.java
@@ -64,26 +64,42 @@ public class CaseAccessOperation {
         final Set<String> validCaseRoles = caseRoleRepository.getCaseRoles(caseDetails.getCaseTypeId());
         final Set<String> targetCaseRoles = caseUser.getCaseRoles();
 
+        validateCaseRoles(validCaseRoles, targetCaseRoles);
+
+        final Long caseId = new Long(caseDetails.getId());
+        final String userId = caseUser.getUserId();
+        final List<String> currentCaseRoles = caseUserRepository.findCaseRoles(caseId, userId);
+
+        grantAddedCaseRoles(userId, caseId, currentCaseRoles, targetCaseRoles);
+        revokeRemovedCaseRoles(userId, caseId, currentCaseRoles, targetCaseRoles);
+    }
+
+    private void validateCaseRoles(Set<String> validCaseRoles, Set<String> targetCaseRoles) {
         targetCaseRoles.stream()
                        .filter(role -> !validCaseRoles.contains(role))
                        .findFirst()
                        .ifPresent(role -> {
                            throw new InvalidCaseRoleException(role);
                        });
+    }
 
-        final Long caseId = new Long(caseDetails.getId());
-        final List<String> currentCaseRoles = caseUserRepository.findCaseRoles(caseId, caseUser.getUserId());
-
-        // Grant added case roles
+    private void grantAddedCaseRoles(String userId,
+                                     Long caseId,
+                                     List<String> currentCaseRoles,
+                                     Set<String> targetCaseRoles) {
         targetCaseRoles.stream()
                        .filter(targetRole -> !currentCaseRoles.contains(targetRole))
-                       .forEach(targetRole -> caseUserRepository.grantAccess(caseId, caseUser.getUserId(), targetRole));
+                       .forEach(targetRole -> caseUserRepository.grantAccess(caseId, userId, targetRole));
+    }
 
-        // Revoke removed case roles
+    private void revokeRemovedCaseRoles(String userId,
+                                        Long caseId,
+                                        List<String> currentCaseRoles,
+                                        Set<String> targetCaseRoles) {
         currentCaseRoles.stream()
                         .filter(currentRole -> !targetCaseRoles.contains(currentRole))
                         .forEach(currentRole -> caseUserRepository.revokeAccess(caseId,
-                                                                                caseUser.getUserId(),
+                                                                                userId,
                                                                                 currentRole));
     }
 }
