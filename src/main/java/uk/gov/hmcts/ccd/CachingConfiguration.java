79ec286bb3a0d81eed8295977ed0ce19e2134b41
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2019-02-01 11:19:11 +0000

    fixed failing tests

---
+++ src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.ccd;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class CachingConfiguration {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new EhCacheCacheManager(ehCacheCacheManager().getObject());
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheCacheManager() {
+        EhCacheManagerFactoryBean cmfb = new EhCacheManagerFactoryBean();
+        cmfb.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        cmfb.setShared(true);
+        return cmfb;
+    }
+}
