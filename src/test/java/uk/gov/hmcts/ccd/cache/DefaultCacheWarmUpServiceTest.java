7c2c4fd9bcc02605512e01aa18ef069e7bce50bf
andrewf <andrzej.folga@hmcts.net>
2019-03-25 13:16:23 +0000

    RDM-4402 WhitespaceAround: 'catch' is not followed by whitespace. Empty blocks may only be represented as {} when not part of a multi-block statement (4.1.3) [WhitespaceAround]

--- src/test/java/uk/gov/hmcts/ccd/cache/DefaultCacheWarmUpServiceTest.java
+++ src/test/java/uk/gov/hmcts/ccd/cache/DefaultCacheWarmUpServiceTest.java
@@ -81,7 +81,7 @@ public class DefaultCacheWarmUpServiceTest {
 
         try {
             defaultCacheWarmUpService.warmUp();
-        } catch(Exception e) {
+        } catch (Exception e) {
             fail();
         }
 
