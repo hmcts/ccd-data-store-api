75be42a6f9f4fc418e40fa563c6117dbb17ea3d0
mutlu <mutlu@localhost>
2019-04-01 17:01:27 +0100

    added unit test for propagation

--- src/test/java/uk/gov/hmcts/ccd/data/definition/DefaultCaseDefinitionRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/ccd/data/definition/DefaultCaseDefinitionRepositoryTest.java
@@ -3,13 +3,14 @@ package uk.gov.hmcts.ccd.data.definition;
 import static org.hamcrest.core.StringStartsWith.startsWith;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
 
 import uk.gov.hmcts.ccd.ApplicationParams;
 import uk.gov.hmcts.ccd.data.SecurityUtils;
+import uk.gov.hmcts.ccd.domain.model.definition.CaseField;
+import uk.gov.hmcts.ccd.domain.model.definition.CaseType;
 import uk.gov.hmcts.ccd.endpoint.exceptions.ResourceNotFoundException;
 import uk.gov.hmcts.ccd.endpoint.exceptions.ServiceException;
 
@@ -17,13 +18,15 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-@Ignore("for now")
 public class DefaultCaseDefinitionRepositoryTest {
     private final String JURISDICTION_ID = "Some Jurisdiction";
 
@@ -41,12 +44,14 @@ public class DefaultCaseDefinitionRepositoryTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        doReturn("").when(applicationParams).caseTypeDefURL("caseTypeId");
         doReturn(new HttpHeaders()).when(securityUtils).authorizationHeaders();
         doReturn(new HttpHeaders()).when(securityUtils).userAuthorizationHeaders();
 
         caseDefinitionRepository = new DefaultCaseDefinitionRepository(applicationParams, securityUtils, restTemplate);
     }
 
+    @Ignore("for now")
     @Test(expected = ResourceNotFoundException.class)
     public void shouldThrowResourceNotFoundExceptionWhenGetCaseTypesForJurisdictionIsCalledAndResourceIsNotFound() {
         HttpClientErrorException exception = new HttpClientErrorException(HttpStatus.NOT_FOUND);
@@ -54,6 +59,7 @@ public class DefaultCaseDefinitionRepositoryTest {
         caseDefinitionRepository.getCaseTypesForJurisdiction(JURISDICTION_ID);
     }
 
+    @Ignore("for now")
     @Test(expected = ResourceNotFoundException.class)
     public void shouldThrowResourceNotFoundExceptionWhenGetBaseTypesIsCalledAndResourceIsNotFound() {
         HttpClientErrorException exception = new HttpClientErrorException(HttpStatus.NOT_FOUND);
@@ -64,9 +70,19 @@ public class DefaultCaseDefinitionRepositoryTest {
     @Test
     public void shouldFailToGetClassificationForUserRole() {
         HttpClientErrorException httpException = new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
-        doThrow(httpException).when(restTemplate).exchange(anyString(), any(), any(), any(Class.class));
+        doThrow(httpException).when(restTemplate).exchange(anyString(), any(HttpMethod.class), any(), any(Class.class));
 
         ServiceException exception = assertThrows(ServiceException.class, () -> caseDefinitionRepository.getUserRoleClassifications("nor_defined"));
         assertThat(exception.getMessage(), startsWith("Error while retrieving classification for user role nor_defined because of "));
+    }
+
+    @Test
+    public void test_propagation_called_once_and_only_once() {
+    	CaseType caseType = new CaseType();
+    	CaseField caseField = Mockito.mock(CaseField.class);
+        caseType.setCaseFields(Arrays.asList(caseField));
+        doReturn(new ResponseEntity<>(caseType, HttpStatus.OK)).when(restTemplate).exchange(anyString(), any(HttpMethod.class), any(), any(Class.class));
+        caseDefinitionRepository.getCaseType("caseTypeId");
+        Mockito.verify(caseField, Mockito.times(1)).propagateACLsToNestedFields();
     }
 }
