0e6fae559082df8475b59f3a0da8ecf626b0f509
mutlu <mutlu@localhost>
2019-05-08 15:20:52 +0100

    style fixes

--- src/test/java/uk/gov/hmcts/ccd/data/casedetails/search/SortDirectionTest.java
+++ src/test/java/uk/gov/hmcts/ccd/data/casedetails/search/SortDirectionTest.java
@@ -1,30 +1,29 @@
 package uk.gov.hmcts.ccd.data.casedetails.search;
 
-import java.util.Optional;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-import org.junit.Assert;
+import java.util.Optional;
 
 public class SortDirectionTest {
 
     @Test
-    public void test_from_null_string() {
-    	Assert.assertEquals(SortDirection.ASC, SortDirection.fromOptionalString(Optional.empty()));
+    public void testFromNullString() {
+        Assert.assertEquals(SortDirection.ASC, SortDirection.fromOptionalString(Optional.empty()));
     }
 
     @Test
-    public void test_from_asc_case_insensitive() {
-    	Assert.assertEquals(SortDirection.ASC, SortDirection.fromOptionalString(Optional.of("AsC")));
+    public void testFromAscCaseInsensitive() {
+        Assert.assertEquals(SortDirection.ASC, SortDirection.fromOptionalString(Optional.of("AsC")));
     }
 
     @Test
-    public void test_from_desc_case_insensitive() {
-    	Assert.assertEquals(SortDirection.DESC, SortDirection.fromOptionalString(Optional.of("dEsC")));
+    public void testFromDescCaseInsensitive() {
+        Assert.assertEquals(SortDirection.DESC, SortDirection.fromOptionalString(Optional.of("dEsC")));
     }
 
     @Test
-    public void test_from_dummy_string() {
-    	Assert.assertEquals(SortDirection.ASC, SortDirection.fromOptionalString(Optional.of("dummy")));
+    public void testFromDummyString() {
+        Assert.assertEquals(SortDirection.ASC, SortDirection.fromOptionalString(Optional.of("dummy")));
     }
 }
