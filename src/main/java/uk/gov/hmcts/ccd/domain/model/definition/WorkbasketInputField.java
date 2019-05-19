46dea0ac1e27843b1f5c26a256dffc45d97cf1a3
Fatih Ozceylan <tzarsmango@users.noreply.github.com>
2019-05-19 22:41:26 +0100

    RDM-4456 Filter searchInput and workbasketInput per user role (#453)
    * RDM-4456 Refactor search input fields and workbasket input fields
    Filter searchInput and workbasketInput per user role
    Alter tests and add tests for new requirements
    
    * RDM-4456 address review comments by Mario
    
    * RDM-4456 address review comments by Mario
    
    * RDM-4456 address review comments by Mario and Mutlu
    
    * RDM-4459 Apply changes for filtering Search/Workbasket results per role
    
    * RDM-4456 Add duplicate field filtering that may occur for users with multiple roles
    Added tests for duplicate case

--- src/main/java/uk/gov/hmcts/ccd/domain/model/definition/WorkbasketInputField.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/definition/WorkbasketInputField.java
@@ -2,50 +2,8 @@ package uk.gov.hmcts.ccd.domain.model.definition;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 
 @ApiModel(description = "")
-public class WorkbasketInputField implements Serializable {
-
-    private String caseFieldId = null;
-    private String caseFieldElementPath = null;
-    private String label = null;
-    private Integer order = null;
-
-    @JsonProperty("case_field_id")
-    public String getCaseFieldId() {
-        return caseFieldId;
-    }
-
-    public void setCaseFieldId(String caseFieldId) {
-        this.caseFieldId = caseFieldId;
-    }
-
-    @JsonProperty("case_field_element_path")
-    public String getCaseFieldElementPath() {
-        return caseFieldElementPath;
-    }
-
-    public void setCaseFieldElementPath(String caseFieldElementPath) {
-        this.caseFieldElementPath = caseFieldElementPath;
-    }
-
-    @JsonProperty("label")
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    @JsonProperty("order")
-    public Integer getOrder() {
-        return order;
-    }
-
-    public void setOrder(Integer order) {
-        this.order = order;
-    }
+public class WorkbasketInputField extends CriteriaField implements Serializable {
 }
