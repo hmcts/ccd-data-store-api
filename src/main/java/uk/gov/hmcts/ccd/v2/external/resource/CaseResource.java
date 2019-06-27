feb1f13f34955efe89185bd875425655de858e8b
andrewf <andrzej.folga@hmcts.net>
2019-06-27 11:47:16 +0100

    RDM-5168 Add missing getters. Add missing callback response and delete draft properties to CaseResource. Populate in constructor.

--- src/main/java/uk/gov/hmcts/ccd/v2/external/resource/CaseResource.java
+++ src/main/java/uk/gov/hmcts/ccd/v2/external/resource/CaseResource.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.springframework.hateoas.ResourceSupport;
 import uk.gov.hmcts.ccd.data.casedetails.SecurityClassification;
+import uk.gov.hmcts.ccd.domain.model.callbacks.AfterSubmitCallbackResponse;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
 import uk.gov.hmcts.ccd.domain.model.std.CaseDataContent;
 import uk.gov.hmcts.ccd.v2.external.controller.CaseController;
@@ -51,6 +52,38 @@ public class CaseResource extends ResourceSupport {
     @JsonProperty("data_classification")
     private Map<String, JsonNode> dataClassification;
 
+    /**
+     * Attribute passed to UI layer, does not need persistence
+     */
+    @JsonProperty("after_submit_callback_response")
+    private AfterSubmitCallbackResponse afterSubmitCallbackResponse;
+
+    /**
+     * Attribute passed to UI layer, does not need persistence
+     */
+    @JsonProperty("callback_response_status_code")
+    private Integer callbackResponseStatusCode;
+
+    /**
+     * Attribute passed to UI layer, does not need persistence
+     */
+    @JsonProperty("callback_response_status")
+    private String callbackResponseStatus;
+
+    /**
+     * Attribute passed to UI layer, does not need persistence
+     */
+    @JsonProperty("delete_draft_response_status_code")
+    private Integer deleteDraftResponseStatusCode;
+
+
+    /**
+     * Attribute passed to UI layer, does not need persistence
+     */
+    @JsonProperty("delete_draft_response_status")
+    private String deleteDraftResponseStatus;
+
+
     public CaseResource(@NonNull CaseDetails caseDetails) {
         copyProperties(caseDetails);
 
@@ -73,5 +106,10 @@ public class CaseResource extends ResourceSupport {
         this.securityClassification = caseDetails.getSecurityClassification();
         this.data = caseDetails.getData();
         this.dataClassification = caseDetails.getDataClassification();
+        this.afterSubmitCallbackResponse = caseDetails.getAfterSubmitCallbackResponse();
+        this.callbackResponseStatusCode = caseDetails.getCallbackResponseStatusCode();
+        this.callbackResponseStatus = caseDetails.getCallbackResponseStatus();
+        this.deleteDraftResponseStatusCode = caseDetails.getDeleteDraftResponseStatusCode();
+        this.deleteDraftResponseStatus = caseDetails.getDeleteDraftResponseStatus();
     }
 }
