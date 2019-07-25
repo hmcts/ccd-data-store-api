4427ab62d3a4ea4dbe7fea0a044b44e6ee9d79a0
vinoth-senthamaraikannan <49725852+vinoth-senthamaraikannan@users.noreply.github.com>
2019-07-25 11:34:23 +0100

    RDM-5118 - Functional Tests for CCD-AM Integration

--- src/aat/java/uk/gov/hmcts/ccd/datastore/tests/functional/CreateCaseTest.java
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/tests/functional/CreateCaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.ccd.datastore.tests.AATHelper;
 import uk.gov.hmcts.ccd.datastore.tests.BaseTest;
+import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseBuilder;
 import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseBuilder.EmptyCase;
 import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType;
 import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType.Event;
@@ -33,5 +34,96 @@ class CreateCaseTest extends BaseTest {
              .body("jurisdiction", equalTo(AATCaseType.JURISDICTION))
              .body("case_type_id", equalTo(AATCaseType.CASE_TYPE))
              .body("state", equalTo(AATCaseType.State.TODO));
+    }
+
+    @Test
+    @DisplayName("should create a case if the caseworker has CREATE access on a CaseType")
+    void shouldCreateCaseWithCreateAccessForCaseTypeACL() {
+        Event.create("AAT_AUTH_3")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(201)
+
+            .assertThat()
+            .body("jurisdiction", equalTo(AATCaseType.JURISDICTION))
+            .body("case_type_id", equalTo("AAT_AUTH_3"))
+            .body("state", equalTo(AATCaseType.State.TODO));
+
+        Event.create("AAT_AUTH_15")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(201)
+
+            .assertThat()
+            .body("jurisdiction", equalTo(AATCaseType.JURISDICTION))
+            .body("case_type_id", equalTo("AAT_AUTH_15"))
+            .body("state", equalTo(AATCaseType.State.TODO));
+
+        Event.create("AAT_AUTH_1")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(201);
+
+        Event.create("AAT_AUTH_5")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(201);
+
+    }
+
+    @Test
+    @DisplayName("should not create a case if the caseworker doesn't have CREATE access on a CaseType")
+    void shouldNotCreateCaseWithNoCreateAccess() {
+        Event.create("AAT_AUTH_2")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(404);
+
+        Event.create("AAT_AUTH_4")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(404);
+
+        Event.create("AAT_AUTH_6")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(404);
+
+        Event.create("AAT_AUTH_8")
+            .as(asAutoTestCaseworker())
+            .withData(AATCaseBuilder.FullCase.build())
+            .submit()
+
+            .then()
+            .log().ifError()
+            .statusCode(404);
     }
 }
