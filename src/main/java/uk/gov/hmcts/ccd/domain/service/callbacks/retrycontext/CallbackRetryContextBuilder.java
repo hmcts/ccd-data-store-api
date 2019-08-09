7e19390e8c1f4fde35f37292a8ac4f8ee8cba4f3
andrewf <andrzej.folga@hmcts.net>
2019-08-09 12:00:43 +0100

    RDM-4522 Add javadoc to CallbackRetryContextBuilder.

--- src/main/java/uk/gov/hmcts/ccd/domain/service/callbacks/retrycontext/CallbackRetryContextBuilder.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/callbacks/retrycontext/CallbackRetryContextBuilder.java
@@ -6,6 +6,25 @@ import uk.gov.hmcts.ccd.ApplicationParams;
 
 import java.util.List;
 
+/**
+ * This builder builds retries context based on the spreadsheet config for retries timeouts.
+ * The intervals are fixed and are multiples of 3 starting from 0s and 1 i.e. 0s, 1s, 3s, 9s, 27s ...
+ * Blank value means default retries (0s interval with 60s default timeout, 1s interval with 60s default timeout, 3s interval with 60s default timeout).
+ * Single 0 value means disable retries (single send at 0s interval with 60s default timeout).
+ * Any comma separated chain of values that DOES NOT include 0 means custom retries.
+ * Any comma separated chain of values that DOES include 0 means default retries (see blank value).
+ *
+ * The examples input callback retry timeouts and output callback retry contexts are as follows:
+ * blank - default retries context (0s interval with 60s default timeout, 1s interval with 60s default timeout, 3s interval with 60s default timeout)
+ * 0 - disable retries (0s interval with 60s default callbacks timeout)
+ * 1 - custom retries (0s interval with 1s custom timeout)
+ * 1,5 - custom retries (0s interval with 1s custom timeout, 1s interval with 5s custom timeout)
+ * 1,5,10 - custom retries (0s interval with 1s custom timeout, 1s interval with 5s custom timeout, 3s interval with 10s custom timeout)
+ * 0,0,0,0 - default retries context (0s interval with 60s default timeout, 1s interval with 60s default timeout, 3s interval with 60s default timeout)
+ * 0,5,0,10 - default retries context (0s interval with 60s default timeout, 1s interval with 60s default timeout, 3s interval with 60s default timeout)
+ * 1,5,10,15 - custom retries (0s interval with 1s custom timeout, 1s interval with 5s custom timeout, 3s interval with 10s custom timeout, 9s interval with 15s custom timeout)
+ * etc
+ */
 @Service
 public class CallbackRetryContextBuilder {
     public static final int CALLBACK_RETRY_INTERVAL_MULTIPLIER = 3;
