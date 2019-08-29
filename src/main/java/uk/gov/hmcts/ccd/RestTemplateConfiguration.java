ac0faf580ca4f886f01639d3badebd328f137398
andrewf <andrzej.folga@hmcts.net>
2019-08-29 12:10:13 +0100

    RDM-4522 Add DefaultHttpRequestRetryHandler to handle org.apache.http.NoHttpResponseException issue with server dropping connections under certain load.

--- src/main/java/uk/gov/hmcts/ccd/RestTemplateConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/RestTemplateConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ccd;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
@@ -140,6 +141,7 @@ class RestTemplateConfiguration {
                                 .useSystemProperties()
                                 .setDefaultRequestConfig(config)
                                 .setConnectionManager(cm)
+                                .setRetryHandler(new DefaultHttpRequestRetryHandler(3, false))
                                 .build();
     }
 }
