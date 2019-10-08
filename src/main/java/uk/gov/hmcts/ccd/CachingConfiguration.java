510db00ab08e851bf6ccf2efe1eb25b7fd7c228e
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2019-10-08 08:36:06 +0100

    Disabling Hazelcast Phone Homes for security purposes

--- src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
@@ -20,6 +20,7 @@ public class CachingConfiguration {
     public Config hazelCastConfig() {
 
         Config config = new Config();
+        config.setProperty("hazelcast.phone.home.enabled", "false");
         NetworkConfig networkConfig = config.setInstanceName("hazelcast-instance-ccd").getNetworkConfig();
         networkConfig.getJoin().getMulticastConfig().setEnabled(false);
         networkConfig.getJoin().getTcpIpConfig().setEnabled(false);
