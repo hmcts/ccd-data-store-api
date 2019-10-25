db50a863f11f6b517d96d3620e59052f98411b49
SivaKanukolluQA <siva.kanukollu@hmcts.net>
2019-10-25 17:17:24 +0100

    RDM-6239: Cucumber Fw

---
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/tests/util/CucumberHooks.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.ccd.datastore.tests.util;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.Before;
+
+public class CucumberHooks {
+
+    private static Scenario scenario;
+
+    public static Scenario getScenario() {
+        return scenario;
+    }
+
+    @Before()
+    public void prepare(Scenario scenario) {
+        this.scenario = scenario;
+    }
+}
