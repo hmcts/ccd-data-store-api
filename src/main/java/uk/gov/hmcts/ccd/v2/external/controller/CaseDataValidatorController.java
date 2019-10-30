8ed42f99fd9aaecb6647549f281a8925dd0f009b
andrewf <andrzej.folga@hmcts.net>
2019-10-30 15:14:22 +0000

    RDM-5696 Organise imports.

--- src/main/java/uk/gov/hmcts/ccd/v2/external/controller/CaseDataValidatorController.java
+++ src/main/java/uk/gov/hmcts/ccd/v2/external/controller/CaseDataValidatorController.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.ccd.v2.external.controller;
 
-import java.util.HashMap;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,15 +8,20 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import uk.gov.hmcts.ccd.domain.model.definition.CaseType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.ccd.domain.model.std.CaseDataContent;
 import uk.gov.hmcts.ccd.domain.service.createevent.MidEventCallback;
 import uk.gov.hmcts.ccd.domain.service.validate.ValidateCaseFieldsOperation;
-import uk.gov.hmcts.ccd.endpoint.exceptions.ValidationException;
 import uk.gov.hmcts.ccd.v2.V2;
 import uk.gov.hmcts.ccd.v2.external.resource.CaseDataResource;
 import uk.gov.hmcts.ccd.v2.internal.resource.UICaseViewResource;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(path = "/case-types")
