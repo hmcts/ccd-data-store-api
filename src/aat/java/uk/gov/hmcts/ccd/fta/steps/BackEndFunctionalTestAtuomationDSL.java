2a74e16943561e4ae93a5b22cc77fd994bd6a21f
mutlu <mutlu.sancaktutar@hmcts.net>
2019-11-06 15:10:58 +0000

    introduce java interface that describes DSL

---
+++ src/aat/java/uk/gov/hmcts/ccd/fta/steps/BackEndFunctionalTestAtuomationDSL.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.ccd.fta.steps;
+
+public interface BackEndFunctionalTestAtuomationDSL {
+
+    // DSL Element:
+    // "an appropriate test context as detailed in the test data source"
+    void initializeAppropriateTestContextAsDetailedInTheTestDataSource();
+
+    // DSL Element:
+    // "a user with [<some specification about user data>]"
+    void verifyThatThereIsAUserInTheContextWithAParticularSpecification(String specificationAboutAUser);
+
+    // DSL Element:
+    // "a request is prepared with appropriate values"
+    public void prepareARequestWithAppropriateValues();
+
+    // DSL Element:
+    // "it is submitted to call the {} operation of {}"
+    public void submitTheRequestToCallAnOperationOfAProduct(String operation, String productName);
+
+    // DSL Element:
+    // "a positive response is received"
+    public void verifyThatAPositiveResponseWasReceived();
+
+    // DSL Element:
+    // "a negative response is received"
+    public void verifyThatANegativeResponseWasReceived();
+
+    // DSL Element:
+    // "the response has all the details as expected"
+    public void verifyThatTheResponseHasAllTheDetailsAsExpected();
+
+    // DSL Element:
+    // "the response [{}]"
+    public void verifyTheResponseInTheContextWithAParticularSpecification(String responseSpecification);
+}
