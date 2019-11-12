eeb0aa6c7b0b4b3ce749f112f8a195af85685047
mutlu <mutlu.sancaktutar@hmcts.net>
2019-11-12 15:32:37 +0000

    introduce a separate class to verify an actual map against an expected one

---
+++ src/aat/java/uk/gov/hmcts/ccd/fta/steps/MapVerifier.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.ccd.fta.steps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapVerifier {
+
+    // TODO: Compare 2 maps and return any differences they may have as a list of
+    // Strings, each String to describe a particular difference.
+    // If the Maps are all equal, then the return an empty list.
+    public static List<String> verifyMap(Map<String, Object> expected, Map<String, Object> actual) {
+        ArrayList<String> differences = new ArrayList<>();
+        return differences;
+    }
+
+}
