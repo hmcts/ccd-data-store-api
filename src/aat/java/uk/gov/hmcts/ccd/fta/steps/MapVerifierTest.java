05b528f4d5e0f50dfb98c5676f2e8afbabcf7131
mutlu <mutlu.sancaktutar@hmcts.net>
2019-11-12 19:54:10 +0000

    added tests for simple cases of unexpected and unavailable fields

--- src/aat/java/uk/gov/hmcts/ccd/fta/steps/MapVerifierTest.java
+++ src/aat/java/uk/gov/hmcts/ccd/fta/steps/MapVerifierTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MapVerifierTest {
@@ -48,6 +49,97 @@ public class MapVerifierTest {
         messages = MapVerifier.verifyMap(new ConcurrentHashMap<String, Object>(), new LinkedHashMap<String, Object>(),
                 0);
         Assert.assertEquals(0, messages.size());
+    }
+
+    @Test
+    public void shouldVerifySimpleMapObjectWithItself() {
+        Map<String, Object> expected = new HashMap<String, Object>();
+        expected.put("key", "value");
+        List<String> messages = MapVerifier.verifyMap(expected, expected, 0);
+        Assert.assertEquals(0, messages.size());
+    }
+
+    @Test
+    public void shouldVerifySimpleMapsOfSameConentWithoutWildcards() {
+        Map<String, Object> expected = new HashMap<String, Object>();
+        Map<String, Object> actual = new ConcurrentHashMap<String, Object>();
+
+        expected.put("key", "value");
+        actual.put("key", "value");
+        List<String> messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertEquals(0, messages.size());
+
+        expected.put("key2", "value2");
+        actual.put("key2", "value2");
+        messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertEquals(0, messages.size());
+
+        expected.put("key3", 333.333);
+        actual.put("key3", 333.333);
+        messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertEquals(0, messages.size());
+    }
+
+    @Test
+    public void shouldNotVerifySimpleMapsWithUnexpectedFields() {
+        Map<String, Object> expected = new HashMap<String, Object>();
+        Map<String, Object> actual = new ConcurrentHashMap<String, Object>();
+
+        actual.put("key1", "value1");
+        List<String> messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertArrayEquals(new Object[] {
+                "actualResponse has unexpected field(s): [key1] This may be an undesirable information exposure!" },
+                messages.toArray());
+
+        actual.put("number", 15);
+        messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertArrayEquals(new Object[] {
+                "actualResponse has unexpected field(s): [key1, number] This may be an undesirable information exposure!" },
+                messages.toArray());
+    }
+
+    @Test
+    public void shouldNotVerifySimpleMapsWithUnavailableFields() {
+        Map<String, Object> expected = new HashMap<String, Object>();
+        Map<String, Object> actual = new ConcurrentHashMap<String, Object>();
+
+        expected.put("key1", "value1");
+        List<String> messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertArrayEquals(new Object[] {
+                "actualResponse lacks [key1] field(s) that was/were actually expected to be there." },
+                messages.toArray());
+
+        expected.put("number", 15);
+        messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertArrayEquals(new Object[] {
+                "actualResponse lacks [key1, number] field(s) that was/were actually expected to be there." },
+                messages.toArray());
+    }
+
+    @Test
+    public void shouldNotVerifySimpleMapsWithUnexpectedAndUnavailableFields() {
+        Map<String, Object> expected = new HashMap<String, Object>();
+        Map<String, Object> actual = new ConcurrentHashMap<String, Object>();
+
+        expected.put("key1", "value1");
+        actual.put("key1", "value1");
+        expected.put("key20", "samevalue");
+        actual.put("key21", "samevalue");
+        List<String> messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertArrayEquals(
+                new Object[] {
+                        "actualResponse has unexpected field(s): [key21] This may be an undesirable information exposure!",
+                        "actualResponse lacks [key20] field(s) that was/were actually expected to be there." },
+                messages.toArray());
+
+        expected.put("key30", "samevalue");
+        actual.put("key31", "samevalue");
+        messages = MapVerifier.verifyMap(expected, actual, 0);
+        Assert.assertArrayEquals(
+                new Object[] {
+                        "actualResponse has unexpected field(s): [key21, key31] This may be an undesirable information exposure!",
+                        "actualResponse lacks [key20, key30] field(s) that was/were actually expected to be there." },
+                messages.toArray());
     }
 
 }
