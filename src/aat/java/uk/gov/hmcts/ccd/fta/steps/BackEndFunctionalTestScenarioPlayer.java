715f51360f61a3cc1de2453a0fe77679caeb0176
Nitish Singla <nitish.singla@hmcts.net>
2019-11-13 16:55:04 +0000

    RDM-6325: Updated the code.

--- src/aat/java/uk/gov/hmcts/ccd/fta/steps/BackEndFunctionalTestScenarioPlayer.java
+++ src/aat/java/uk/gov/hmcts/ccd/fta/steps/BackEndFunctionalTestScenarioPlayer.java
@@ -69,14 +69,16 @@ public class BackEndFunctionalTestScenarioPlayer implements BackEndFunctionalTes
     @Given("a user with [{}]")
     public void verifyThatThereIsAUserInTheContextWithAParticularSpecification(String specificationAboutAUser) {
         UserData aUser = scenarioContext.getTestData().getUser();
-
         String logPrefix = scenarioContext.getCurrentScenarioTag() + ": User ";
         try {
-            AuthenticatedUser authenticatedUserMetadata = aat.getIdamHelper().authenticate(
-                aUser.getUsername(), aUser.getPassword());
-            aUser.setToken(authenticatedUserMetadata.getAccessToken());
-            aUser.setUid(authenticatedUserMetadata.getId());
-            logger.info(logPrefix + "authenticated");
+            if (aUser.getToken() == null) {
+                AuthenticatedUser authenticatedUserMetadata = aat.getIdamHelper().authenticate(
+                    aUser.getUsername(), aUser.getPassword());
+                aUser.setToken(authenticatedUserMetadata.getAccessToken());
+                aUser.setUid(authenticatedUserMetadata.getId());
+                logger.info(logPrefix + "authenticated");
+            }
+
         } catch (FeignException ex) {
             logger.info(logPrefix + "credentials do not exist");
         }
@@ -233,7 +235,7 @@ public class BackEndFunctionalTestScenarioPlayer implements BackEndFunctionalTes
     @Then("the response [{}]")
     public void verifyTheResponseInTheContextWithAParticularSpecification(String responseSpecification) {
         String errorMessage = "Test data does not confirm it meets the specification about the response: "
-                + responseSpecification;
+            + responseSpecification;
         boolean check = scenarioContext.getTestData().meetsSpec(responseSpecification);
         Assert.assertTrue(errorMessage, check);
     }
