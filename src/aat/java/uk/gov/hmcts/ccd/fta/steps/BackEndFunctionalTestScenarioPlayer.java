ea5dd144b6567595139196ec5ce750e1be8f20c8
mutlu <mutlu.sancaktutar@hmcts.net>
2019-11-18 02:16:02 +0000

    restore lost changes from child branches

---
+++ src/aat/java/uk/gov/hmcts/ccd/fta/steps/BackEndFunctionalTestScenarioPlayer.java
@@ -0,0 +1,263 @@
+package uk.gov.hmcts.ccd.fta.steps;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.QueryableRequestSpecification;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.ccd.datastore.tests.AATHelper;
+import uk.gov.hmcts.ccd.datastore.tests.helper.idam.AuthenticatedUser;
+import uk.gov.hmcts.ccd.fta.data.RequestData;
+import uk.gov.hmcts.ccd.fta.data.ResponseData;
+import uk.gov.hmcts.ccd.fta.data.UserData;
+import uk.gov.hmcts.ccd.fta.util.JsonUtils;
+
+@SuppressWarnings({"LocalVariableName"})
+public class BackEndFunctionalTestScenarioPlayer implements BackEndFunctionalTestAutomationDSL {
+
+    private static final String DYNAMIC_CONTENT_PLACEHOLDER = "[[DYNAMIC]]";
+
+    private final BackEndFunctionalTestScenarioContext scenarioContext;
+    private final AATHelper aat;
+    private Scenario scenario;
+
+    private Logger logger = LoggerFactory.getLogger(BackEndFunctionalTestScenarioPlayer.class);
+
+    public BackEndFunctionalTestScenarioPlayer() {
+        aat = AATHelper.INSTANCE;
+        RestAssured.baseURI = aat.getTestUrl();
+        RestAssured.useRelaxedHTTPSValidation();
+        scenarioContext = new BackEndFunctionalTestScenarioContext();
+    }
+
+    @Before()
+    public void prepare(Scenario scenario) {
+        this.scenario = scenario;
+    }
+
+    @Override
+    @Given("an appropriate test context as detailed in the test data source")
+    public void initializeAppropriateTestContextAsDetailedInTheTestDataSource() {
+        scenarioContext.initializeTestDataFor(scenario);
+        String logPrefix = scenarioContext.getCurrentScenarioTag() + ": Test data ";
+        if (scenarioContext.getTestData() != null) {
+            logger.info(logPrefix + "was loaded successfully");
+        } else {
+            logger.info(logPrefix + "was not found");
+        }
+    }
+
+    @Override
+    @Given("a user with [{}]")
+    public void verifyThatThereIsAUserInTheContextWithAParticularSpecification(String specificationAboutAUser) {
+        UserData aUser = scenarioContext.getTestData().getUser();
+
+        String logPrefix = scenarioContext.getCurrentScenarioTag() + ": User ";
+        logger.info("USERNAME: [[" + aUser.getUsername() + "]], PASSWORD: [[" + aUser.getPassword() + "]]");
+        try {
+            AuthenticatedUser authenticatedUserMetadata = aat.getIdamHelper().authenticate(
+                aUser.getUsername(), aUser.getPassword());
+            logger.info("TOKEN: [[" + authenticatedUserMetadata.getAccessToken() + "]], UID: [["
+                + authenticatedUserMetadata.getId() + "]]");
+            aUser.setToken(authenticatedUserMetadata.getAccessToken());
+            aUser.setUid(authenticatedUserMetadata.getId());
+            logger.info(logPrefix + "authenticated");
+        } catch (FeignException ex) {
+            logger.info(logPrefix + "credentials do not exist");
+        }
+
+        scenarioContext.setTheUser(aUser);
+
+        boolean doesTestDataMeetSpec = scenarioContext.getTestData().meetsSpec(specificationAboutAUser);
+        String errorMessage = "Test data does not confirm it meets the specification about a user: "
+            + specificationAboutAUser;
+        Assert.assertTrue(errorMessage, doesTestDataMeetSpec);
+    }
+
+    @Override
+    @When("a request is prepared with appropriate values")
+    public void prepareARequestWithAppropriateValues() throws IOException {
+        UserData theUser = scenarioContext.getTheUser();
+        String s2sToken = aat.getS2SHelper().getToken();
+
+        RequestSpecification aRequest = RestAssured.given();
+        RequestData requestData = scenarioContext.getTestData().getRequest();
+
+        if (requestData.getHeaders() != null) {
+            requestData.getHeaders().forEach((header, value) -> {
+                if (value.toString().equals(DYNAMIC_CONTENT_PLACEHOLDER)) {
+                    // ADD DYNAMIC DATA HERE
+                    if (header.equals("Authorization") && theUser.getToken() != null) {
+                        String authToken = "Bearer " + theUser.getToken();
+                        aRequest.header(header, authToken);
+                        scenarioContext.getTestData().getRequest().getHeaders().put("Authorization", authToken);
+                    } else if (header.equals("ServiceAuthorization") && s2sToken != null) {
+                        aRequest.header(header, s2sToken);
+                        scenarioContext.getTestData().getRequest().getHeaders().put("ServiceAuthorization", s2sToken);
+                    } else {
+                        Assert.fail("Dynamic value for request header '" + header + "' does not exist");
+                    }
+                } else {
+                    aRequest.header(header, value);
+                }
+            });
+        }
+
+        if (requestData.getPathVariables() != null) {
+            requestData.getPathVariables().forEach((pathVariable, value) -> {
+                if (value.toString().equals(DYNAMIC_CONTENT_PLACEHOLDER)) {
+                    // ADD DYNAMIC DATA HERE
+                    if (pathVariable.equals("uid") && theUser.getUid() != null) {
+                        aRequest.pathParam(pathVariable, theUser.getUid());
+                        scenarioContext.getTestData().getRequest().getPathVariables().put("uid", theUser.getUid());
+                    } else {
+                        Assert.fail("Dynamic value for request path variable '" + pathVariable + "' does not exist");
+                    }
+                } else {
+                    aRequest.pathParam(pathVariable, value);
+                }
+            });
+        }
+
+        if (requestData.getQueryParams() != null) {
+            requestData.getQueryParams().forEach((queryParam, value) -> {
+                if (value.toString().equals(DYNAMIC_CONTENT_PLACEHOLDER)) {
+                    // ADD DYNAMIC DATA HERE
+                    Assert.fail("Dynamic value for request query parameter '" + queryParam + "' does not exist");
+                } else {
+                    aRequest.queryParam(queryParam, value);
+                }
+            });
+        }
+
+        if (requestData.getBody() != null) {
+            aRequest.body(new ObjectMapper().writeValueAsBytes(requestData.getBody()));
+        }
+
+        scenarioContext.setTheRequest(aRequest);
+        scenario.write(JsonUtils.getPrettyJsonFromObject(scenarioContext.getTestData().getRequest()));
+    }
+
+    @Override
+    @When("it is submitted to call the [{}] operation of [{}]")
+    public void submitTheRequestToCallAnOperationOfAProduct(String operation, String productName) throws IOException {
+        boolean isCorrectOperation = scenarioContext.getTestData().meetsOperationOfProduct(operation, productName);
+        String errorMessage = "Test data does not confirm it is calling the following operation of a product: "
+            + operation + " -> " + productName;
+        Assert.assertTrue(errorMessage, isCorrectOperation);
+
+        RequestSpecification theRequest = scenarioContext.getTheRequest();
+        String uri = scenarioContext.getTestData().getUri();
+
+        Response response = null;
+        Assert.assertNotNull("No request method in data file", scenarioContext.getTestData().getMethod());
+        switch (scenarioContext.getTestData().getMethod()) {
+            case "GET":
+                response = theRequest.get(uri);
+                break;
+            case "POST":
+                response = theRequest.post(uri);
+                break;
+            case "PUT":
+                response = theRequest.put(uri);
+                break;
+            case "DELETE":
+                response = theRequest.delete(uri);
+                break;
+            default:
+                Assert.fail("Unknown request method in data file");
+        }
+
+        QueryableRequestSpecification queryableRequest = SpecificationQuerier.query(theRequest);
+        scenario.write(queryableRequest.getMethod() + " " + queryableRequest.getURI());
+
+        Map<String, Object> responseHeaders = new HashMap<>();
+        response.getHeaders().forEach(header -> responseHeaders.put(header.getName(), header.getValue()));
+        ResponseData responseData = new ResponseData();
+        responseData.setResponseCode(response.getStatusCode());
+        responseData.setResponseMessage(HttpStatus.valueOf(response.getStatusCode()).getReasonPhrase());
+        responseData.setHeaders(responseHeaders);
+
+        if (!response.getBody().asString().isEmpty()) {
+            responseData.setBody(JsonUtils.readObjectFromJsonText(response.getBody().asString(), Map.class));
+        }
+
+        scenarioContext.setTheResponse(responseData);
+    }
+
+    @Override
+    @Then("a positive response is received")
+    public void verifyThatAPositiveResponseWasReceived() {
+        int responseCode = scenarioContext.getTheResponse().getResponseCode();
+        String errorMessage = "Response code '" + responseCode + "' is not a success code";
+        Assert.assertEquals(errorMessage, 2, responseCode / 100);
+        scenario.write("" + scenarioContext.getTheResponse().getResponseCode());
+    }
+
+    @Override
+    @Then("a negative response is received")
+    public void verifyThatANegativeResponseWasReceived() {
+        int responseCode = scenarioContext.getTheResponse().getResponseCode();
+        String errorMessage = "Response code '" + responseCode + "' is not a negative code";
+        Assert.assertNotEquals(errorMessage, 2, responseCode / 100);
+        scenario.write("" + scenarioContext.getTheResponse().getResponseCode());
+    }
+
+    @Override
+    @Then("the response has all the details as expected")
+    public void verifyThatTheResponseHasAllTheDetailsAsExpected() throws IOException {
+        ResponseData expectedResponse = scenarioContext.getTestData().getExpectedResponse();
+        ResponseData actualResponse = scenarioContext.getTheResponse();
+        Map<String, List> issues = new HashMap<>();
+
+        if (actualResponse.getResponseCode() != expectedResponse.getResponseCode()) {
+            issues.put("responseCode", Collections.singletonList("Response code mismatch, expected: "
+                + expectedResponse.getResponseCode() + ", actual: " + actualResponse.getResponseCode()));
+        }
+
+        MapVerificationResult headerVerification = MapVerifier.verifyMap(expectedResponse.getHeaders(),
+            actualResponse.getHeaders(), 1);
+        if (!headerVerification.isVerified()) {
+            issues.put("headers", headerVerification.getAllIssues());
+        }
+
+        MapVerificationResult bodyVerification = MapVerifier.verifyMap(expectedResponse.getBody(),
+            actualResponse.getBody(), 20);
+        if (!bodyVerification.isVerified()) {
+            issues.put("body", bodyVerification.getAllIssues());
+        }
+
+        scenario.write(JsonUtils.getPrettyJsonFromObject(scenarioContext.getTheResponse()));
+
+        if (issues.get("responseCode") != null || issues.get("headers") != null || issues.get("body") != null) {
+            Assert.fail("Response failures: " + JsonUtils.getPrettyJsonFromObject(issues));
+        }
+    }
+
+    @Override
+    @Then("the response [{}]")
+    public void verifyTheResponseInTheContextWithAParticularSpecification(String responseSpecification) {
+        String errorMessage = "Test data does not confirm it meets the specification about the response: "
+                + responseSpecification;
+        boolean check = scenarioContext.getTestData().meetsSpec(responseSpecification);
+        Assert.assertTrue(errorMessage, check);
+    }
+}
