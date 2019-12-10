09cbf3f1556e0bcaf00798f7262cd14a3309abf5
RBaker <rebecca.baker@hmcts.net>
2019-12-10 11:48:23 +0000

    RDM-6390 updated variable name

--- src/aat/java/uk/gov/hmcts/ccd/fta/steps/BackEndFunctionalTestScenarioPlayer.java
+++ src/aat/java/uk/gov/hmcts/ccd/fta/steps/BackEndFunctionalTestScenarioPlayer.java
@@ -199,9 +199,9 @@ public class BackEndFunctionalTestScenarioPlayer implements BackEndFunctionalTes
                         Long theCaseReference = scenarioContext.getTheCaseReference();
                         aRequest.pathParam(pathVariable, theCaseReference);
                         scenarioContext.getTestData().getRequest().getPathVariables().put("cid", theCaseReference);
-                    } else if (pathVariable.equals("idToRevoke") && theInvokingUser.getUid() != null) {
+                    } else if (pathVariable.equals("idToDelete") && theInvokingUser.getUid() != null) {
                         aRequest.pathParam(pathVariable, theInvokingUser.getUid());
-                        scenarioContext.getTestData().getRequest().getPathVariables().put("idToDelete", theInvokingUser.getUid());
+                        scenarioContext.getTestData().getRequest().getPathVariables().put("idToRevoke", theInvokingUser.getUid());
                     } else {
                         throw new FunctionalTestException("Dynamic value for request path variable '"
                             + pathVariable + "' does not exist");
