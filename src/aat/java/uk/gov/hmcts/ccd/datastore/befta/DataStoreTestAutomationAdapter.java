2ec9d38436300d843853899eb95e2880ee64d84d
mutlu <mutlu.sancaktutar@hmcts.net>
2020-01-01 21:10:04 +0000

    use Exception instead of assertion and improve style

--- src/aat/java/uk/gov/hmcts/ccd/datastore/befta/DataStoreTestAutomationAdapter.java
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/befta/DataStoreTestAutomationAdapter.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.ccd.datastore.befta;
 
-
-import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,14 +12,15 @@ import io.restassured.specification.RequestSpecification;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.TestAutomationConfig;
 import uk.gov.hmcts.befta.data.UserData;
+import uk.gov.hmcts.befta.exception.FunctionalTestException;
 
 public class DataStoreTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
     private Logger logger = LoggerFactory.getLogger(DataStoreTestAutomationAdapter.class);
 
-    private final String BE_FTA_FILE_JURISDICTION1 = "src/aat/resources/CCD_BEFTA_JURISDICTION1.xlsx";
-    private final String BE_FTA_FILE_JURISDICTION2 = "src/aat/resources/CCD_BEFTA_JURISDICTION2.xlsx";
-    private final String BE_FTA_FILE_JURISDICTION3 = "src/aat/resources/CCD_BEFTA_JURISDICTION3.xlsx";
+    private static final String BE_FTA_FILE_JURISDICTION_1 = "src/aat/resources/CCD_BEFTA_JURISDICTION1.xlsx";
+    private static final String BE_FTA_FILE_JURISDICTION_2 = "src/aat/resources/CCD_BEFTA_JURISDICTION2.xlsx";
+    private static final String BE_FTA_FILE_JURISDICTION_3 = "src/aat/resources/CCD_BEFTA_JURISDICTION3.xlsx";
 
     public DataStoreTestAutomationAdapter(TestAutomationConfig config) {
         super(config);
@@ -33,24 +32,26 @@ public class DataStoreTestAutomationAdapter extends DefaultTestAutomationAdapter
     }
 
     private void importDefinitions() {
-        logger.info("Importing {}...", BE_FTA_FILE_JURISDICTION1);
-        importDefinition(BE_FTA_FILE_JURISDICTION1);
-        logger.info("Imported {}.", BE_FTA_FILE_JURISDICTION1);
+        logger.info("Importing {}...", BE_FTA_FILE_JURISDICTION_1);
+        importDefinition(BE_FTA_FILE_JURISDICTION_1);
+        logger.info("Imported {}.", BE_FTA_FILE_JURISDICTION_1);
 
-        logger.info("Importing {}...", BE_FTA_FILE_JURISDICTION2);
-        importDefinition(BE_FTA_FILE_JURISDICTION2);
-        logger.info("Imported {}.", BE_FTA_FILE_JURISDICTION2);
+        logger.info("Importing {}...", BE_FTA_FILE_JURISDICTION_2);
+        importDefinition(BE_FTA_FILE_JURISDICTION_2);
+        logger.info("Imported {}.", BE_FTA_FILE_JURISDICTION_2);
 
-        logger.info("Importing {}...", BE_FTA_FILE_JURISDICTION3);
-        importDefinition(BE_FTA_FILE_JURISDICTION3);
-        logger.info("Imported {}", BE_FTA_FILE_JURISDICTION3);
+        logger.info("Importing {}...", BE_FTA_FILE_JURISDICTION_3);
+        importDefinition(BE_FTA_FILE_JURISDICTION_3);
+        logger.info("Imported {}", BE_FTA_FILE_JURISDICTION_3);
     }
 
     private void importDefinition(String file) {
         Response response = asAutoTestImporter().given().multiPart(new File(file)).when().post("/import");
         String message = "Import failed with response body: " + response.body().prettyPrint();
         message += "\nand http code: " + response.statusCode();
-        Assert.assertEquals(message, 201, response.getStatusCode());
+        if (response.getStatusCode() != 201) {
+            throw new FunctionalTestException(message);
+        }
     }
 
     private RequestSpecification asAutoTestImporter() {
