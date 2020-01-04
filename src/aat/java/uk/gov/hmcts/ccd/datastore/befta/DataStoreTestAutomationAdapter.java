c0a166712f610ddeff03096f995ec90efaa00bef
mutlu <mutlu.sancaktutar@hmcts.net>
2020-01-04 08:36:24 +0000

    add content type

--- src/aat/java/uk/gov/hmcts/ccd/datastore/befta/DataStoreTestAutomationAdapter.java
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/befta/DataStoreTestAutomationAdapter.java
@@ -71,7 +71,9 @@ public class DataStoreTestAutomationAdapter extends DefaultTestAutomationAdapter
         Map<String, String> ccdRoleInfo = new HashMap<>();
         ccdRoleInfo.put("role", role);
         ccdRoleInfo.put("security_classification", classification);
-        Response response = asAutoTestImporter().given().body(ccdRoleInfo).when().put("/api/user-role");
+        Response response = asAutoTestImporter().given()
+                .header("Content-type", "application/json").body(ccdRoleInfo).when()
+                .put("/api/user-role");
         if (response.getStatusCode() != 205 && response.getStatusCode() != 215) {
             String message = "Import failed with response body: " + response.body().prettyPrint();
             message += "\nand http code: " + response.statusCode();
