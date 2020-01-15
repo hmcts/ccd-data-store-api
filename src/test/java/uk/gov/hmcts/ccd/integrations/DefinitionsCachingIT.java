3ab6169cb325605a22a31e77b1ee8e755b053e23
Benjamin Matthews <benjamin.matthews@hmcts.net>
2020-01-15 16:43:08 +0000

    remove integration test import

--- src/test/java/uk/gov/hmcts/ccd/integrations/DefinitionsCachingIT.java
+++ src/test/java/uk/gov/hmcts/ccd/integrations/DefinitionsCachingIT.java
@@ -20,8 +20,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
