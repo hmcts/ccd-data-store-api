14a2104487d68253af7695a05821a547aa7a9725
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-01-16 15:00:25 +0000

    RDM-5700 - Removed duplicate code getQueryByMetaData() and flag JPA_CRITERIA_IN_SEARCH_ENABLED

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -171,7 +171,6 @@ module "ccd-data-store-api" {
     CCD_AM_WRITE_TO_BOTH                  = "${var.ccd_am_write_to_both}"
     CCD_AM_READ_FROM_CCD                  = "${var.ccd_am_read_from_ccd}"
     CCD_AM_READ_FROM_AM                   = "${var.ccd_am_read_from_am}"
-    JPA_CRITERIA_IN_SEARCH_ENABLED        = false
   }
 
 }
