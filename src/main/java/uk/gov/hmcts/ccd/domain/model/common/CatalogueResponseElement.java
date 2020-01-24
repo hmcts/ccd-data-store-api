839cfa936ad51df2ab699f188fb1e498f4e84b5f
Matt Nayler <matt.nayler@cgi.com>
2020-01-24 17:56:20 +0000

    RDM-7224-5: Refactor CatalogueResponse for PR #766
    Update common code for RDM-7224 & RDM-7225
    
    * rename CatalogueResponseCode as CatalogueResponseElement
    * adjust CatalogueResponseElement and CatalogueResponseGroup enum
    properties

---
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/common/CatalogueResponseElement.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.ccd.domain.model.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public enum CatalogueResponseElement {
+
+    // CCD.00 Success codes
+
+    // CCD.01 Validation codes
+    VALIDATION_INVALID_DATA(CatalogueResponseGroup.VALIDATION, 1, "Invalid data provided."),
+
+    // CCD.02 Callback codes
+    CALLBACK_FAILURE(CatalogueResponseGroup.CALLBACK, 1, "Callback failure."),
+    CALLBACK_PAYMENT_REQUIRED(CatalogueResponseGroup.CALLBACK, 2, "Payment required by callback.");
+
+    final CatalogueResponseGroup group;
+    final int sequenceNo;
+    final String message;
+
+    CatalogueResponseElement(final CatalogueResponseGroup group, final int sequenceNo, final String message) {
+        this.group = group;
+        this.sequenceNo = sequenceNo;
+        this.message = message;
+    }
+
+    @JsonIgnore
+    public CatalogueResponseGroup getGroup() {
+        return group;
+    }
+
+    public int getSequenceNo() {
+        return sequenceNo;
+    }
+
+    public String getCode() {
+        return String.format("%s.%02d", getGroup().getCode(), getSequenceNo());
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
