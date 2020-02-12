d6d6410344282664ae99d891c06e1dc69f3e9800
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-02-12 10:48:19 +0000

    RDM-7419: Introduce Cucumber Runner Classes

---
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/cucumber/DataStoreRunner.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.ccd.datastore.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import uk.gov.hmcts.befta.BeftaMain;
+import uk.gov.hmcts.befta.BeftaTestDataLoader;
+import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "json:target/cucumber.json",
+    glue = "uk.gov.hmcts.befta.player",
+    features = {"classpath:features"})
+public class DataStoreRunner extends BeftaMain {
+
+    @BeforeClass
+    public static void init() {
+        setTaAdapter(new DefaultTestAutomationAdapter());
+        BeftaTestDataLoader.main(null);
+    }
+
+}
