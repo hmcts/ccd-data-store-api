c696d705577650d4c7fce7038f75f9023187f4a1
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-03-06 10:44:04 +0000

    AM-450 : Validation service class for document UUID.

---
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/DocumentIdValidationService.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.ccd.domain.service.getcasedocument;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.UUID;
+
+@Named
+@Singleton
+public class DocumentIdValidationService {
+
+
+    /**
+     * Validate a UUID string using
+     *
+     * @param documentId case document Id
+     * @return boolean result for validation
+     */
+
+    public boolean validateUUID(String documentId){
+        try{
+            UUID id = UUID.fromString(documentId);
+            if(!id.toString().equals(documentId)){
+                return false;
+            }
+        }catch (IllegalArgumentException exception){
+            return false;
+        }
+        return true;
+    }
+}
