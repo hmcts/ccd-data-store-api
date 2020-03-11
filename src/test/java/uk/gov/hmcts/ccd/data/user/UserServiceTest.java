edb6ee43c9353ebf9c78cf44c185edf0e606ca89
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-03-11 19:57:15 +0000

    RDM-7162: Automate Jurisdiction assignment – tactical solution Fixed UserProfile throwing exception when user doesn't exist on ccd

--- src/test/java/uk/gov/hmcts/ccd/data/user/UserServiceTest.java
+++ src/test/java/uk/gov/hmcts/ccd/data/user/UserServiceTest.java
@@ -1,18 +1,11 @@
 package uk.gov.hmcts.ccd.data.user;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
-
+import com.google.common.collect.Lists;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import java.util.List;
-
 import uk.gov.hmcts.ccd.data.casedetails.JurisdictionMapper;
 import uk.gov.hmcts.ccd.data.definition.CaseDefinitionRepository;
 import uk.gov.hmcts.ccd.domain.model.aggregated.IDAMProperties;
@@ -21,6 +14,14 @@ import uk.gov.hmcts.ccd.domain.model.aggregated.UserDefault;
 import uk.gov.hmcts.ccd.domain.model.aggregated.UserProfile;
 import uk.gov.hmcts.ccd.domain.model.aggregated.WorkbasketDefault;
 import uk.gov.hmcts.ccd.domain.model.definition.Jurisdiction;
+import uk.gov.hmcts.ccd.endpoint.exceptions.ResourceNotFoundException;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
 
@@ -60,6 +61,7 @@ public class UserServiceTest {
         when(userRepoMock.getUserDetails()).thenReturn(mockIDAMProps);
         UserDefault userDefaultMock = aUserDefault();
         when(userRepoMock.getUserDefaultSettings("email")).thenReturn(userDefaultMock);
+        when(jurisdictionsResolver.getJurisdictions()).thenReturn(Lists.newArrayList("J1", "J2", "J3"));
         when(caseDefinitionRepoMock.getJurisdiction("J1")).thenReturn(j1);
         when(caseDefinitionRepoMock.getJurisdiction("J2")).thenReturn(j2);
         when(caseDefinitionRepoMock.getJurisdiction("J3")).thenReturn(unknownJurisdiction);
@@ -76,6 +78,29 @@ public class UserServiceTest {
         assertThat(workbasketDefault.getJurisdictionId(), is("J1"));
         assertThat(workbasketDefault.getCaseTypeId(), is("CT"));
         assertThat(workbasketDefault.getStateId(), is("ST"));
+    }
+
+    @Test
+    public void testReturnsUserProfileNoWorkBasketDefaults() {
+
+        when(userRepoMock.getUserDetails()).thenReturn(mockIDAMProps);
+        when(userRepoMock.getUserDefaultSettings("email"))
+            .thenThrow(new ResourceNotFoundException("No User profile exists for this userId email"));
+        when(jurisdictionsResolver.getJurisdictions()).thenReturn(Lists.newArrayList("J1", "J2", "J3"));
+        when(caseDefinitionRepoMock.getJurisdiction("J1")).thenReturn(j1);
+        when(caseDefinitionRepoMock.getJurisdiction("J2")).thenReturn(j2);
+        when(caseDefinitionRepoMock.getJurisdiction("J3")).thenReturn(unknownJurisdiction);
+        when(jurisdictionMapperMock.toResponse(j1)).thenReturn(jdp1);
+        when(jurisdictionMapperMock.toResponse(j2)).thenReturn(jdp2);
+        when(jurisdictionMapperMock.toResponse(unknownJurisdiction)).thenReturn(jdp3);
+
+        UserProfile userProfile = userService.getUserProfile();
+
+        assertThat(userProfile.getUser().getIdamProperties(), is(mockIDAMProps));
+        assertThat(userProfile.getJurisdictions(),
+            equalTo(new JurisdictionDisplayProperties[] { jdp1, jdp2, jdp3 }));
+        WorkbasketDefault workbasketDefault = userProfile.getDefaultSettings().getWorkbasketDefault();
+        assertNull(workbasketDefault);
     }
 
     private UserDefault aUserDefault() {
