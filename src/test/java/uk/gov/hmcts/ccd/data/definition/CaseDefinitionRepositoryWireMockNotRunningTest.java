b0fa5a823c4394712f9c1dd65c4c3326d2a4f1b2
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-03-23 15:17:29 +0000

    removed unused import

--- src/test/java/uk/gov/hmcts/ccd/data/definition/CaseDefinitionRepositoryWireMockNotRunningTest.java
+++ src/test/java/uk/gov/hmcts/ccd/data/definition/CaseDefinitionRepositoryWireMockNotRunningTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
-import org.junit.Ignore;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
