bb69af8e20a666d454d19d5401a51763d28272c8
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-03-23 12:02:57 +0000

    AM-450 : New code elements for retrieving the list fo document fields.

--- src/main/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/GetCaseDocumentOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/GetCaseDocumentOperation.java
@@ -23,11 +23,13 @@ import uk.gov.hmcts.ccd.v2.external.domain.CaseDocumentMetadata;
 import uk.gov.hmcts.ccd.v2.external.domain.Permission;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class GetCaseDocumentOperation {
@@ -82,6 +84,61 @@ public class GetCaseDocumentOperation {
     }
 
     public CaseDocument getCaseDocument(CaseDetails caseDetails, String documentId) {
+
+        //Retrieve the caseType and list of casefield containing document casefield
+        //get the caseTypeId and JID & corresponding casetype
+        String caseTypeId = caseDetails.getCaseTypeId();
+        String jurisdictionId = caseDetails.getJurisdiction();
+        final CaseType caseType = caseTypeService.getCaseTypeForJurisdiction(caseTypeId, jurisdictionId);
+        //Step1
+        //Extract the list of complex case Fields having document casefields or comlex casefields
+        //Step4 : Add all the document field list together
+        List<CaseField> finalDocumentCaseFields = Collections.<CaseField>emptyList();
+        List<CaseField> complexCaseFieldList = caseType.getCaseFields()
+            .stream()
+            .filter(f -> ("complex".equalsIgnoreCase(f.getFieldType().getType())) &&
+                (f.getFieldType().getComplexFields().size() > 0))
+            .flatMap(x -> (x.getFieldType().getComplexFields())
+                .stream()
+                .filter(y -> ("document".equalsIgnoreCase(y.getFieldType().getType())) ||
+                    ("complex".equalsIgnoreCase(y.getFieldType().getType()))))
+            .collect(Collectors.toList());
+
+        //If this list contains any caseField with complex caseField again then repeat the above step to extract the full list of document casefields.
+        finalDocumentCaseFields.addAll(complexCaseFieldList);
+        //<more logic to come>
+
+        //Step2
+        // Collection Case fields having collection field type as document.
+        List<CaseField> collectionCaseFields = caseType.getCaseFields()
+            .stream()
+            .filter(f -> ("collection".equalsIgnoreCase(f.getFieldType().getType())) &&
+                (("document".equalsIgnoreCase(f.getFieldType().getCollectionFieldType().getType())) ||
+                ("collection".equalsIgnoreCase(f.getFieldType().getCollectionFieldType().getType()))))
+            .collect(Collectors.toList());
+
+        //If this list contains collection casefield again then repeat the above step to extract the full list of document casefields.
+        finalDocumentCaseFields.addAll(collectionCaseFields);
+        //<more logic to come>
+
+        //Step3 List of root level document case field.
+        List<CaseField> documentCaseFields = caseType.getCaseFields()
+            .stream()
+            .filter(f -> "Document".equalsIgnoreCase(f.getFieldType().getType()))
+            .collect(Collectors.toList());
+        finalDocumentCaseFields.addAll(documentCaseFields);
+
+
+        //Step5: Get the list of json fields from casedata object and extarct list of node names.
+
+        //Step6: perform an intersaction of both these list to find out the common fields.
+
+        //Step7: Loop through the common list of Json node fields to search documentId in specific url child field.
+
+        //Step8: get the list of fields from which this document is found and extract the ACL for each field and perform union to build caseDocument.
+
+
+        //final Optional<CaseField> documentCaseField = caseType.getCaseField(documentField);
 
         //retrieve the case data from details
         Map<String, JsonNode> caseData = caseDetails.getData();
