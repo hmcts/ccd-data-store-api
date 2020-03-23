8930d56ce984cca8ea3396d62c757fa7a0e6a31d
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-03-23 12:02:16 +0000

    AM-450 : Unit testing improvement.

--- src/test/resources/sql/insert_cases.sql
+++ src/test/resources/sql/insert_cases.sql
@@ -583,6 +583,42 @@ VALUES (13, 'TestAddressBookCaseNoReadFieldAccess', 'PROBATE', 'CaseCreated', 'P
        '1504259907353651'
 );
 
+INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference)
+VALUES (21, 'BEFTA_CASETYPE_2_1', 'BEFTA_JURISDICTION_2', 'CaseCreated', 'PUBLIC',
+        '{
+          "PersonFirstName": "Peter",
+          "PersonLastName": "Pullen",
+          "PersonAddress": {
+            "AddressLine1": "Governer House",
+            "AddressLine2": "1 Puddle Lane",
+            "AddressLine3": "London",
+            "Country": "England",
+            "Postcode": "SE1 4EE"
+          },
+          "DocumentField1": {
+            "document_url": "http://localhost:[port]/documents/05e7cd7e-7041-4d8a-826a-7bb49dfd83d1",
+            "document_binary_url": "http://localhost:[port]/documents/05e7cd7e-7041-4d8a-826a-7bb49dfd83d1/binary",
+            "document_filename": "Seagulls_Square.jpg"
+          }
+        }',
+       '{
+         "PersonFirstName": "PRIVATE",
+         "PersonLastName": "PUBLIC",
+         "PersonAddress": {
+           "classification" : "PUBLIC",
+           "value" : {
+               "AddressLine1": "PUBLIC",
+               "AddressLine2": "PUBLIC",
+               "AddressLine3": "PUBLIC",
+               "Country": "PUBLIC",
+               "Postcode": "PUBLIC"
+           }
+         },
+         "DocumentField1": "PUBLIC"
+       }',
+       '1504259907353123'
+);
+
 INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, created_date, last_modified, last_state_modified_date)
 VALUES (16, 'TestAddressBookCase', 'PROBATE', 'CaseCreated', 'PUBLIC',
         '{
@@ -720,6 +756,40 @@ INSERT INTO case_event (
     ) VALUES (
         11,
         'TestAddressBookCaseNoReadEventAccess',
+        1,
+        'Some comment 2',
+        'The summary 2',
+        'Goodness',
+        'GRACIOUS',
+        0,
+        'Justin',
+        'Smith',
+        'state4',
+        'Case in state 4',
+        'PUBLIC',
+        '2017-05-09 15:31:43.000000',
+        '{}'
+    );
+
+    INSERT INTO case_event (
+        case_data_id,
+        case_type_id,
+        case_type_version,
+        description,
+        summary,
+        event_id,
+        event_name,
+        user_id,
+        user_first_name,
+        user_last_name,
+        state_id,
+        state_name,
+        security_classification,
+        created_date,
+        data
+    ) VALUES (
+        21,
+        'BEFTA_CASETYPE_2_1',
         1,
         'Some comment 2',
         'The summary 2',
