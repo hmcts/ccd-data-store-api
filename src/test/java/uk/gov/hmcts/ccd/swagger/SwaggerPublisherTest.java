2ad1b5cae90ccad48cd8bbdaa3350aa078fbb1ec
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-03-27 10:32:13 +0000

    RDM-7621 - fixed test to inline with openId config

--- src/test/java/uk/gov/hmcts/ccd/swagger/SwaggerPublisherTest.java
+++ src/test/java/uk/gov/hmcts/ccd/swagger/SwaggerPublisherTest.java
@@ -8,21 +8,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.ccd.WireMockBaseTest;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@SpringJUnitWebConfig
-@SpringBootTest
-@AutoConfigureMockMvc
-public class SwaggerPublisherTest {
+public class SwaggerPublisherTest extends WireMockBaseTest {
 
     private MockMvc mvc;
 
