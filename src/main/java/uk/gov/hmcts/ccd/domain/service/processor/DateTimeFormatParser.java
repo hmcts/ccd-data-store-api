b0bb310ea0dca398727db3cb9c78d581cada711e
RBaker <rebecca.baker@hmcts.net>
2020-03-27 08:30:59 +0000

    RDM-7630 extended logic to be more robust

--- src/main/java/uk/gov/hmcts/ccd/domain/service/processor/DateTimeFormatParser.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/processor/DateTimeFormatParser.java
@@ -6,16 +6,33 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 
+import lombok.extern.slf4j.*;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DateTimeFormatParser {
 
     private static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS";
     private static final String DATE_FORMAT = "yyyy-MM-dd";
 
     public String convertDateTimeToIso8601(String dateTimeFormat, String value) {
-        DateTimeFormatter inputFormat = new DateTimeFormatterBuilder()
+        LocalDateTime dateTime;
+        try {
+            dateTimeFormat = ((dateTimeFormat == null) ? DATE_TIME_FORMAT : dateTimeFormat);
+            DateTimeFormatter inputFormat = getDateTimeFormatter(dateTimeFormat);
+            dateTime = LocalDateTime.parse(value, inputFormat);
+        } catch (Exception e) {
+            log.warn("Failed to parse following dateTime value {} with format {} attempting to parse with {}", value, dateTimeFormat, DATE_TIME_FORMAT);
+            dateTimeFormat = DATE_TIME_FORMAT;
+            DateTimeFormatter inputFormat = getDateTimeFormatter(dateTimeFormat);
+            dateTime = LocalDateTime.parse(value, inputFormat);
+        }
+        return dateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
+    }
+
+    private DateTimeFormatter getDateTimeFormatter(String dateTimeFormat) {
+        return new DateTimeFormatterBuilder()
             .appendPattern(dateTimeFormat)
             .parseDefaulting(ChronoField.YEAR_OF_ERA, LocalDate.now().getYear())
             .parseDefaulting(ChronoField.MONTH_OF_YEAR, LocalDate.now().getMonthValue())
@@ -25,20 +42,29 @@ public class DateTimeFormatParser {
             .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
             .parseDefaulting(ChronoField.MILLI_OF_SECOND, 0)
             .toFormatter();
-
-        LocalDateTime dateTime = LocalDateTime.parse(value, inputFormat);
-        return dateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
     }
 
     public String convertDateToIso8601(String dateFormat, String value) {
-        DateTimeFormatter inputFormat = new DateTimeFormatterBuilder()
-            .appendPattern(dateFormat)
-            .parseDefaulting(ChronoField.YEAR_OF_ERA, LocalDate.now().getYear())
-            .parseDefaulting(ChronoField.MONTH_OF_YEAR, LocalDate.now().getMonthValue())
-            .parseDefaulting(ChronoField.DAY_OF_MONTH, LocalDate.now().getDayOfMonth())
-            .toFormatter();
-
-        LocalDate date = LocalDate.parse(value, inputFormat);
+        LocalDate date;
+        try {
+            dateFormat = ((dateFormat == null) ? DATE_FORMAT : dateFormat);
+            DateTimeFormatter inputFormat = getDateFormatter(dateFormat);
+            date = LocalDate.parse(value, inputFormat);
+        } catch (Exception e) {
+            log.warn("Failed to parse following date value {} with format {} attempting to parse with {}", value, dateFormat, DATE_FORMAT);
+            dateFormat = DATE_FORMAT;
+            DateTimeFormatter inputFormat = getDateFormatter(dateFormat);
+            date = LocalDate.parse(value, inputFormat);
+        }
         return date.format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+    }
+
+    private DateTimeFormatter getDateFormatter(String dateFormat) {
+        return new DateTimeFormatterBuilder()
+                .appendPattern(dateFormat)
+                .parseDefaulting(ChronoField.YEAR_OF_ERA, LocalDate.now().getYear())
+                .parseDefaulting(ChronoField.MONTH_OF_YEAR, LocalDate.now().getMonthValue())
+                .parseDefaulting(ChronoField.DAY_OF_MONTH, LocalDate.now().getDayOfMonth())
+                .toFormatter();
     }
 }
--- src/test/java/uk/gov/hmcts/ccd/domain/service/processor/DateTimeFormatParserTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/service/processor/DateTimeFormatParserTest.java
@@ -27,9 +27,49 @@ class DateTimeFormatParserTest {
     }
 
     @Test
+    void shouldConvertDateTimeToIso8601FormatNullDisplayContextParameter() {
+        final String dateTimeFormat = null;
+        final String value = "2000-10-20T12:30:59.000";
+        final String result = dateTimeFormatParser.convertDateTimeToIso8601(dateTimeFormat, value);
+        assertAll(
+            () -> assertThat(result, is("2000-10-20T12:30:59.000"))
+        );
+    }
+
+    @Test
+    void shouldConvertDateTimeToIso8601FormatIncorrectDisplayContextParameter() {
+        final String dateTimeFormat = "HHmmssSSS dd/MM/yyyy";
+        final String value = "2000-10-20T12:30:59.000";
+        final String result = dateTimeFormatParser.convertDateTimeToIso8601(dateTimeFormat, value);
+        assertAll(
+            () -> assertThat(result, is("2000-10-20T12:30:59.000"))
+        );
+    }
+
+    @Test
     void shouldConvertDateToIso8601Format() {
         final String dateTimeFormat = "dd/MM/yyyy";
         final String value = "20/10/2000";
+        final String result = dateTimeFormatParser.convertDateToIso8601(dateTimeFormat, value);
+        assertAll(
+            () -> assertThat(result, is("2000-10-20"))
+        );
+    }
+
+    @Test
+    void shouldConvertDateToIso8601FormatNullDisplayContextParameter() {
+        final String dateTimeFormat = null;
+        final String value = "2000-10-20";
+        final String result = dateTimeFormatParser.convertDateToIso8601(dateTimeFormat, value);
+        assertAll(
+            () -> assertThat(result, is("2000-10-20"))
+        );
+    }
+
+    @Test
+    void shouldConvertDateToIso8601FormatIncorrectDisplayContextParameter() {
+        final String dateTimeFormat = "dd/MM/yyyy";
+        final String value = "2000-10-20";
         final String result = dateTimeFormatParser.convertDateToIso8601(dateTimeFormat, value);
         assertAll(
             () -> assertThat(result, is("2000-10-20"))
