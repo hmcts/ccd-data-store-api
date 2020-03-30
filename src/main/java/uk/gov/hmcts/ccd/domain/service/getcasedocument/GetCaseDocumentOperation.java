9aa6b873d45a39b9a7c4b317e83b69a5c4bca8ee
Nitish Singla <nitish.singla@hmcts.net>
2020-03-30 11:45:39 +0100

    Arraylist initialization.

--- src/main/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/GetCaseDocumentOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/GetCaseDocumentOperation.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.ccd.domain.service.getcasedocument;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
