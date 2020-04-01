ebfbec418e2407c4ccd199a2dc0101a995798fd8
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-04-01 12:58:54 +0100

    RDM-8094 - disable issuer validation temporarily

--- src/main/java/uk/gov/hmcts/ccd/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/SecurityConfiguration.java
@@ -101,7 +101,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // We are using issuerOverride instead of issuerUri as SIDAM has the wrong issuer at the moment
         OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
         OAuth2TokenValidator<Jwt> withIssuer = new JwtIssuerValidator(issuerOverride);
-        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withIssuer);
+        // FIXME : enable `withIssuer` once idam migration done RDM-8094
+        // OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withIssuer);
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp);
 
         jwtDecoder.setJwtValidator(validator);
         return jwtDecoder;
