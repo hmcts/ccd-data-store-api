c6aead895c583ff0b5f246c3f10eb25657c38836
mutlu <mutlu.sancaktutar@hmcts.net>
2020-04-17 18:01:15 +0100

    tidy up and provide v1 and v2 dockets.

--- src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
@@ -15,7 +16,6 @@ import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-import uk.gov.hmcts.ccd.CoreCaseDataApplication;
 import uk.gov.hmcts.ccd.endpoint.std.CaseDetailsEndpoint;
 import uk.gov.hmcts.ccd.v2.external.controller.CaseController;
 
@@ -24,25 +24,27 @@ import uk.gov.hmcts.ccd.v2.external.controller.CaseController;
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-            .apis(RequestHandlerSelectors.basePackage(CoreCaseDataApplication.class.getPackage().getName()))
-            .build()
-            .useDefaultResponseMessages(false);
+    public Docket apiV1() {
+        return getNewDocketForPackageOf(CaseDetailsEndpoint.class, "v1", apiV1Info());
     }
 
     @Bean
-    public Docket apiV1() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .groupName("v1")
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(CaseDetailsEndpoint.class.getPackage().getName()))
-            .build()
-            .useDefaultResponseMessages(false)
-            .apiInfo(apiInfo());
+    public Docket apiV2() {
+        return getNewDocketForPackageOf(CaseController.class, "v2", apiV2Info());
     }
 
-    private ApiInfo apiInfo() {
+    private Docket getNewDocketForPackageOf(Class<?> klazz, String groupName, ApiInfo apiInfo) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(groupName)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(klazz.getPackage().getName()))
+                .paths(PathSelectors.any())
+                .build().useDefaultResponseMessages(false)
+                .apiInfo(apiInfo)
+                .globalOperationParameters(Arrays.asList(headerAuthorization(), headerServiceAuthorization()));
+    }
+
+    private ApiInfo apiV1Info() {
         return new ApiInfoBuilder()
             .title("Core Case Data - Data store API")
             .description("Create, modify, retrieve and search cases")
@@ -56,41 +58,6 @@ public class SwaggerConfiguration {
             .build();
     }
 
-    @Bean
-    public Docket apiV2() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .groupName("v2")
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(CaseController.class.getPackage().getName()))
-            .build()
-            .useDefaultResponseMessages(false)
-            .apiInfo(apiV2Info())
-            .globalOperationParameters(Arrays.asList(
-                headerAuthorization(),
-                headerServiceAuthorization()
-            ));
-    }
-
-    private Parameter headerAuthorization() {
-        return new ParameterBuilder()
-            .name("Authorization")
-            .description("Keyword `Bearer` followed by a valid IDAM user token")
-            .modelRef(new ModelRef("string"))
-            .parameterType("header")
-            .required(true)
-            .build();
-    }
-
-    private Parameter headerServiceAuthorization() {
-        return new ParameterBuilder()
-            .name("ServiceAuthorization")
-            .description("Valid Service-to-Service JWT token for a whitelisted micro-service")
-            .modelRef(new ModelRef("string"))
-            .parameterType("header")
-            .required(true)
-            .build();
-    }
-
     private ApiInfo apiV2Info() {
         return new ApiInfoBuilder()
             .title("CCD Data Store API")
@@ -101,6 +68,26 @@ public class SwaggerConfiguration {
             .contact(new Contact("CCD",
                                  "https://tools.hmcts.net/confluence/display/RCCD/Reform%3A+Core+Case+Data+Home",
                                  "corecasedatateam@hmcts.net"))
+            .build();
+    }
+
+    private Parameter headerAuthorization() {
+        return new ParameterBuilder()
+            .name("Authorization")
+            .description("Keyword `Bearer` followed by a valid IDAM user token")
+            .modelRef(new ModelRef("string"))
+            .parameterType("header")
+                .required(false)
+            .build();
+    }
+
+    private Parameter headerServiceAuthorization() {
+        return new ParameterBuilder()
+            .name("ServiceAuthorization")
+            .description("Valid Service-to-Service JWT token for a whitelisted micro-service")
+            .modelRef(new ModelRef("string"))
+            .parameterType("header")
+                .required(false)
             .build();
     }
 }
