9a458aaf0850ac0533867c08b3efcc483e9a58c7
Nitish Singla <nitish.singla@hmcts.net>
2020-04-20 09:37:23 +0100

    AM-588 : Code refactoring.

--- src/main/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/CaseDocumentAttachOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/CaseDocumentAttachOperation.java
@@ -88,6 +88,7 @@ public class CaseDocumentAttachOperation {
         afterCallbackPrepareDocumentMetaData(caseDetails, isCallbackResponseValid);
         filterDocumentFields();
     }
+
     public void filterDocumentFields() {
         filterDocumentFields(caseDocumentsMetadata, documentTokenMap, documentAfterCallback);
     }
