68b2e083816982d25c07496de9db2cdd2eed8dff
Nitish Singla <nitish.singla@hmcts.net>
2020-05-15 11:00:55 +0100

    Am-588: Updating the Content type header.

--- src/main/java/uk/gov/hmcts/ccd/v2/external/controller/CaseController.java
+++ src/main/java/uk/gov/hmcts/ccd/v2/external/controller/CaseController.java
@@ -8,7 +8,6 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
