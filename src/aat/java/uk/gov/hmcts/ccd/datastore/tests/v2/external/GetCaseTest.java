32fcd466d7b4f63352d70b5badd8e6226624167a
MSancaktutar <mutlu.sancaktutar@hmcts.net>
2020-05-28 15:16:37 +0100

    Revert "Removed redundant test - already done by S-110"

--- src/aat/java/uk/gov/hmcts/ccd/datastore/tests/v2/external/GetCaseTest.java
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/tests/v2/external/GetCaseTest.java
@@ -1,55 +1,52 @@
 package uk.gov.hmcts.ccd.datastore.tests.v2.external;
 
-import io.restassured.http.ContentType;
+import static java.lang.Boolean.FALSE;
+import static org.hamcrest.Matchers.equalTo;
+import static uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType.CASE_TYPE;
+import static uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType.JURISDICTION;
+
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import uk.gov.hmcts.ccd.config.JacksonUtils;
 import uk.gov.hmcts.ccd.datastore.tests.AATHelper;
 import uk.gov.hmcts.ccd.datastore.tests.BaseTest;
 import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseBuilder;
 import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseBuilder.FullCase;
 import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType;
 import uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType.Event;
-import uk.gov.hmcts.ccd.domain.model.std.CaseDataContent;
 import uk.gov.hmcts.ccd.v2.V2;
 
-import java.util.function.Supplier;
-
-import static java.lang.Boolean.FALSE;
-import static org.hamcrest.Matchers.equalTo;
-import static uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType.CASE_TYPE;
-import static uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType.Event.CREATE;
-import static uk.gov.hmcts.ccd.datastore.tests.fixture.AATCaseType.JURISDICTION;
-
-@DisplayName("Create case")
-class CreateCaseTest extends BaseTest {
+@DisplayName("Get case by reference")
+class GetCaseTest extends BaseTest {
     private static final String NOT_FOUND_CASE_REFERENCE = "1234123412341238";
     private static final String INVALID_CASE_REFERENCE = "1234123412341234";
-    private static final String INVALID_EVENT_TRIGGER_ID = "invalidEvent";
-    private static final String EVENT_TOKEN = "someToken";
 
-    protected CreateCaseTest(AATHelper aat) {
+    protected GetCaseTest(AATHelper aat) {
         super(aat);
     }
 
     @Test
-    @DisplayName("should create case")
-    void shouldCreateCase() {
-        String eventToken = aat.getCcdHelper().generateTokenCreateCase(asAutoTestCaseworker(), JURISDICTION, CASE_TYPE, CREATE);
+    @DisplayName("should retrieve case when the case reference exists")
+    void shouldRetrieveWhenExists() {
+        // Prepare new case in known state
+        final Long caseReference = Event.create()
+                                        .as(asAutoTestCaseworker())
+                                        .withData(FullCase.build())
+                                        .submitAndGetReference();
 
-        callCreateCase(CASE_TYPE, getBody(CREATE, eventToken, FullCase::build))
+        callGetCase(caseReference.toString())
             .when()
-            .post("/case-types/{caseTypeId}/cases")
+            .get("/cases/{caseReference}")
 
             .then()
             .log().ifError()
-            .statusCode(201)
+            .statusCode(200)
             .assertThat()
 
             // Metadata
             .body("jurisdiction", equalTo(JURISDICTION))
             .body("case_type", equalTo(CASE_TYPE))
+            .body("id", equalTo(caseReference.toString()))
             .body("state", equalTo(AATCaseType.State.TODO))
             .body("security_classification", equalTo("PUBLIC"))
 
@@ -80,44 +77,39 @@ class CreateCaseTest extends BaseTest {
             .body("AddressUKField.Country", equalTo(AATCaseBuilder.ADDRESS_COUNTRY))
 
             .rootPath("_links")
-            .body("self.href", equalTo(String.format("%s/case-types/%s/cases{?ignore-warning}", aat.getTestUrl(), CASE_TYPE)))
+            .body("self.href", equalTo(String.format("%s/cases/%s", aat.getTestUrl(), caseReference)))
         ;
     }
 
-
     @Test
-    @DisplayName("should get 404 when event trigger id invalid")
-    void should404WhenEventTriggerIdInvalid() {
-        String eventToken = aat.getCcdHelper().generateTokenCreateCase(asAutoTestCaseworker(), JURISDICTION, CASE_TYPE, CREATE);
-
-        callCreateCase(CASE_TYPE, getBody(INVALID_EVENT_TRIGGER_ID, eventToken, FullCase::build))
+    @DisplayName("should get 404 when case reference does NOT exist")
+    void should404WhenNotExists() {
+        callGetCase(NOT_FOUND_CASE_REFERENCE)
             .when()
-            .post("/case-types/{caseTypeId}/cases")
+            .get("/cases/{caseReference}")
 
             .then()
             .statusCode(404);
     }
 
-    private RequestSpecification callCreateCase(final String caseTypeId, Supplier<String> supplier) {
+    @Test
+    @DisplayName("should get 400 when case reference invalid")
+    void should400WhenReferenceInvalid() {
+        callGetCase(INVALID_CASE_REFERENCE)
+            .when()
+            .get("/cases/{caseReference}")
+
+            .then()
+            .statusCode(400);
+    }
+
+    private RequestSpecification callGetCase(String caseReference) {
         return asAutoTestCaseworker(FALSE)
             .get()
             .given()
-            .body(supplier.get())
-            .contentType(ContentType.JSON)
             .log().all()
-            .pathParam("caseTypeId", caseTypeId)
-            .accept(V2.MediaType.CREATE_CASE)
+            .pathParam("caseReference", caseReference)
+            .accept(V2.MediaType.CASE)
             .header("experimental", "true");
     }
-
-    private Supplier<String> getBody(String eventId, String eventToken, Supplier<AATCaseType.CaseData> caseDataSupplier) {
-        CaseDataContent caseDataContent = Event.create()
-            .as(asAutoTestCaseworker())
-            .withData(caseDataSupplier.get())
-            .withEventId(eventId)
-            .withToken(eventToken)
-            .toCaseDataContent();
-        return () -> JacksonUtils.convertValueJsonNode(caseDataContent).toString();
-    }
-
 }
