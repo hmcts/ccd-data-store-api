bcda3f02e466858a91acbacc51a49b4f51843775
Dan Lysiak <daniel.lysiak@cgi.com>
2020-05-29 09:37:18 +0100

    Init ES data only when required

--- src/test/java/uk/gov/hmcts/ccd/ElasticsearchBaseTest.java
+++ src/test/java/uk/gov/hmcts/ccd/ElasticsearchBaseTest.java
@@ -1,13 +1,50 @@
 package uk.gov.hmcts.ccd;
 
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
 import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
 
 import java.io.IOException;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static uk.gov.hmcts.ccd.ElasticsearchITConfiguration.INDEX_TYPE;
+import static uk.gov.hmcts.ccd.ElasticsearchITConfiguration.INDICES;
+
 public abstract class ElasticsearchBaseTest extends WireMockBaseTest {
 
+    private static final String DATA_DIR = "elasticsearch/data";
+
+    private EmbeddedElastic embeddedElastic;
+
+    @Autowired
+    public final void setEmbeddedElastic(EmbeddedElastic embeddedElastic) {
+        this.embeddedElastic = embeddedElastic;
+    }
+
     @Before
-    public void setUp() throws IOException {
+    public void initElastic() throws IOException, InterruptedException {
         super.initMock();
+        embeddedElastic.start();
+        initData();
+    }
+
+    @After
+    public void tearDownElastic() {
+        embeddedElastic.stop();
+    }
+
+    private void initData() throws IOException {
+        PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+        for (String idx : INDICES) {
+            Resource[] resources = resourceResolver.getResources(String.format("classpath:%s/%s/*.json", DATA_DIR, idx));
+            for (Resource resource : resources) {
+                String caseString = IOUtils.toString(resource.getInputStream(), UTF_8);
+                embeddedElastic.index(idx, INDEX_TYPE, caseString);
+            }
+        }
     }
 }
--- src/test/java/uk/gov/hmcts/ccd/ElasticsearchITConfiguration.java
+++ src/test/java/uk/gov/hmcts/ccd/ElasticsearchITConfiguration.java
@@ -1,47 +1,41 @@
 package uk.gov.hmcts.ccd;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
 import pl.allegro.tech.embeddedelasticsearch.IndexSettings;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.concurrent.TimeUnit.MINUTES;
 import static pl.allegro.tech.embeddedelasticsearch.PopularProperties.HTTP_PORT;
 
-@Configuration
-@ConditionalOnProperty(value = "search.elastic.embedded", havingValue = "true")
+@TestConfiguration
+@ConditionalOnProperty(value = "search.elastic.embedded")
 public class ElasticsearchITConfiguration {
 
-    private static final String INDEX_TYPE = "_doc";
     private static final String CONFIG_DIR = "elasticsearch/config";
     private static final String INDEX_SETTINGS = "classpath:" + CONFIG_DIR + "/index-settings.json";
-    private static final String DATA_DIR = "elasticsearch/data";
-    private static final String[] INDICES = { "aat_cases", "mapper_cases" };
+    public static final String INDEX_TYPE = "_doc";
+    public static final String[] INDICES = { "aat_cases", "mapper_cases" };
 
     @Value("${search.elastic.version}")
     private String elasticVersion;
     @Value("${search.elastic.port}")
     private int httpPortValue;
 
-    private PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
-
     @Bean
-    public EmbeddedElastic embeddedElastic() throws IOException, InterruptedException {
+    public EmbeddedElastic embeddedElastic() throws IOException {
         EmbeddedElastic.Builder builder = EmbeddedElastic.builder()
             .withElasticVersion(elasticVersion)
             .withSetting(HTTP_PORT, httpPortValue)
             .withEsJavaOpts("-Xms128m -Xmx512m")
-            .withStartTimeout(2, MINUTES);
+            .withStartTimeout(30, TimeUnit.SECONDS);
 
+        PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
         for (String idx : INDICES) {
             builder.withIndex(idx, IndexSettings.builder()
                 .withType(INDEX_TYPE, resourceResolver
@@ -51,17 +45,6 @@ public class ElasticsearchITConfiguration {
                 .build());
         }
 
-        return builder.build().start();
-    }
-
-    @PostConstruct
-    public void initData() throws IOException, InterruptedException {
-        for (String idx : INDICES) {
-            Resource[] resources = resourceResolver.getResources(String.format("classpath:%s/%s/*.json", DATA_DIR, idx));
-            for (Resource resource : resources) {
-                String caseString = IOUtils.toString(resource.getInputStream(), UTF_8);
-                embeddedElastic().index(idx, INDEX_TYPE, caseString);
-            }
-        }
+        return builder.build();
     }
 }
