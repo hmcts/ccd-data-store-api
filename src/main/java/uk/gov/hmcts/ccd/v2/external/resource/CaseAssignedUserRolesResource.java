f5b1f5265bd1b280fa09789cdd41529c007bd885
mutlu <mutlu.sancaktutar@hmcts.net>
2020-06-14 21:39:22 +0100

    correct attribute key

--- src/main/java/uk/gov/hmcts/ccd/v2/external/resource/CaseAssignedUserRolesResource.java
+++ src/main/java/uk/gov/hmcts/ccd/v2/external/resource/CaseAssignedUserRolesResource.java
@@ -1,19 +1,22 @@
 package uk.gov.hmcts.ccd.v2.external.resource;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.hateoas.RepresentationModel;
 import uk.gov.hmcts.ccd.domain.model.std.CaseAssignedUserRole;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-public class CaseAssignedUserRolesResource extends RepresentationModel {
+public class CaseAssignedUserRolesResource extends RepresentationModel<CaseAssignedUserRolesResource> {
 
-    @JsonProperty("case-users")
+    @JsonProperty("case_users")
     private List<CaseAssignedUserRole> caseAssignedUserRoles;
 
     public CaseAssignedUserRolesResource(List<CaseAssignedUserRole> caseAssignedUserRoles) {
