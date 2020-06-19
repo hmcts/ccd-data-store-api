b0bb384ea7a4393076554c07de4546b58f2b6806
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-06-19 11:12:40 +0100

    Correcting idam claim type  (#983)
    * Correcting claim type as some are List
    
    * Test name fix

--- src/test/java/uk/gov/hmcts/ccd/MockUtils.java
+++ src/test/java/uk/gov/hmcts/ccd/MockUtils.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.ccd;
 
+import com.google.common.collect.Lists;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -37,7 +38,7 @@ public class MockUtils {
 
         Jwt jwt =   Jwt.withTokenValue(jwtToken)
             .claim("aClaim", "aClaim")
-            .claim("aud", CCD_GW)
+            .claim("aud", Lists.newArrayList(CCD_GW))
             .header("aHeader", "aHeader")
             .build();
         when(authenticationMock.getPrincipal()).thenReturn(jwt);
