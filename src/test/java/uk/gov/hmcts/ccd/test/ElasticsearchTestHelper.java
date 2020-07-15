a2b3d71cd3c514cf8095b72b5fc6d82792eef321
Alex Ross <Alex.ross@hmcts.net>
2020-07-15 14:01:11 +0100

    Updates

--- src/test/java/uk/gov/hmcts/ccd/test/ElasticsearchTestHelper.java
+++ src/test/java/uk/gov/hmcts/ccd/test/ElasticsearchTestHelper.java
@@ -92,7 +92,7 @@ public class ElasticsearchTestHelper {
     public static final String AUTOTEST1_SOLICITOR = "caseworker-autotest1-solicitor";
 
     private static final String CASE_TYPE_ID_PARAM = "ctid";
-    private static final String USE_CASE_PARAM = "usecase";
+    private static final String USE_CASE_PARAM = "use_case";
 
     private ElasticsearchTestHelper() { }
 
