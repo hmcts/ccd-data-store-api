5a0d2ac020083d4d45d4f905bde7101d5aca0821
it.consulting@tutanota.com <it.consulting@tutanota.com>
2020-07-17 10:48:43 +0100

    RDM-8751: Enhance external search API to retrieve summary information for cases of an organisation
            [RDM-8751] (https://tools.hmcts.net/jira/browse/RDM-8751).

--- src/main/java/uk/gov/hmcts/ccd/data/definition/CachedCaseDefinitionRepository.java
+++ src/main/java/uk/gov/hmcts/ccd/data/definition/CachedCaseDefinitionRepository.java
@@ -1,25 +1,23 @@
 package uk.gov.hmcts.ccd.data.definition;
 
-import static com.google.common.collect.Maps.newHashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
+import uk.gov.hmcts.ccd.domain.model.definition.CaseTypeDefinition;
+import uk.gov.hmcts.ccd.domain.model.definition.FieldTypeDefinition;
+import uk.gov.hmcts.ccd.domain.model.definition.JurisdictionDefinition;
+import uk.gov.hmcts.ccd.domain.model.definition.UserRole;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import uk.gov.hmcts.ccd.domain.model.definition.CaseTypeDefinition;
-import uk.gov.hmcts.ccd.domain.model.definition.FieldTypeDefinition;
-import uk.gov.hmcts.ccd.domain.model.definition.JurisdictionDefinition;
-import uk.gov.hmcts.ccd.domain.model.definition.UserRole;
+import static com.google.common.collect.Maps.newHashMap;
 
 @Service
 @Qualifier(CachedCaseDefinitionRepository.QUALIFIER)
