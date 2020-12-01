7b0071cfe315d488fef16eafa4542f95278a8ec3
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-12-01 14:28:40 +0000

    RDM-10165: fixed sonar cloud code coverage issues

--- src/main/java/uk/gov/hmcts/ccd/domain/model/aggregated/CaseViewEvent.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/aggregated/CaseViewEvent.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.ccd.domain.model.aggregated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.domain.model.callbacks.SignificantItem;
 import uk.gov.hmcts.ccd.domain.model.std.AuditEvent;
 
-import java.time.LocalDateTime;
-
+@Data
+@NoArgsConstructor
 public class CaseViewEvent {
     private Long id;
 
@@ -47,126 +50,6 @@ public class CaseViewEvent {
 
     @JsonProperty("proxied_by_first_name")
     private String proxiedByFirstName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(String eventId) {
-        this.eventId = eventId;
-    }
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserLastName() {
-        return userLastName;
-    }
-
-    public void setUserLastName(String userLastName) {
-        this.userLastName = userLastName;
-    }
-
-    public String getUserFirstName() {
-        return userFirstName;
-    }
-
-    public void setUserFirstName(String userFirstName) {
-        this.userFirstName = userFirstName;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public String getStateName() {
-        return stateName;
-    }
-
-    public void setStateName(String stateName) {
-        this.stateName = stateName;
-    }
-
-    public String getStateId() {
-        return stateId;
-    }
-
-    public void setStateId(String stateId) {
-        this.stateId = stateId;
-    }
-
-    public SignificantItem getSignificantItem() {
-        return significantItem;
-    }
-
-    public void setSignificantItem(SignificantItem significantItem) {
-        this.significantItem = significantItem;
-    }
-
-    public String getProxiedBy() {
-        return proxiedBy;
-    }
-
-    public void setProxiedBy(String proxiedBy) {
-        this.proxiedBy = proxiedBy;
-    }
-
-    public String getProxiedByLastName() {
-        return proxiedByLastName;
-    }
-
-    public void setProxiedByLastName(String proxiedByLastName) {
-        this.proxiedByLastName = proxiedByLastName;
-    }
-
-    public String getProxiedByFirstName() {
-        return proxiedByFirstName;
-    }
-
-    public void setProxiedByFirstName(String proxiedByFirstName) {
-        this.proxiedByFirstName = proxiedByFirstName;
-    }
 
     public static CaseViewEvent createFrom(AuditEvent event) {
         CaseViewEvent caseEvent = new CaseViewEvent();
