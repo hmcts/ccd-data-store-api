afcb351366024cf0c25bc413562b17eafa6308e8
Abi-Laver <abi.laver@cgi.com>
2021-01-06 14:43:02 +0000

    Added WireMockTestConfiguration

--- src/test/java/uk/gov/hmcts/ccd/WireMockBaseTest.java
+++ src/test/java/uk/gov/hmcts/ccd/WireMockBaseTest.java
@@ -1,18 +1,26 @@
 package uk.gov.hmcts.ccd;
 
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformer;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.ResponseDefinition;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.cloud.contract.wiremock.WireMockConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.inject.Inject;
 import java.io.IOException;
 
-@DirtiesContext  // required for Jenkins agent
 @AutoConfigureWireMock(port = 0)
 public abstract class WireMockBaseTest extends BaseTest {
 
@@ -38,5 +46,26 @@ public abstract class WireMockBaseTest extends BaseTest {
         ReflectionTestUtils.setField(applicationParams, "uiDefinitionHost", hostUrl);
         ReflectionTestUtils.setField(applicationParams, "userProfileHost", hostUrl);
         ReflectionTestUtils.setField(applicationParams, "draftHost", hostUrl);
+    }
+
+    @Configuration
+    static class WireMockTestConfiguration {
+        @Bean
+        public WireMockConfigurationCustomizer wireMockConfigurationCustomizer() {
+            return config -> config.extensions(new ResponseDefinitionTransformer() {
+                @Override
+                public String getName() {
+                    return "keep-alive-disabler";
+                }
+
+                @Override
+                public ResponseDefinition transform(Request request, ResponseDefinition responseDefinition,
+                                                    FileSource files, Parameters parameters) {
+                    return ResponseDefinitionBuilder.like(responseDefinition)
+                        .withHeader(HttpHeaders.CONNECTION, "close")
+                        .build();
+                }
+            });
+        }
     }
 }
