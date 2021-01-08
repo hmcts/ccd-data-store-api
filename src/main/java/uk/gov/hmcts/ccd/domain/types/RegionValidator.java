756fed40ffe80fbda8e82342652966a056b24c90
Abi-Laver <abi.laver@cgi.com>
2021-01-08 16:34:27 +0000

    Autowired TextValidator to new basetype validators

--- src/main/java/uk/gov/hmcts/ccd/domain/types/RegionValidator.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/types/RegionValidator.java
@@ -1,17 +1,22 @@
 package uk.gov.hmcts.ccd.domain.types;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseFieldDefinition;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
-import java.util.Collections;
 import java.util.List;
 
 @Named("RegionValidator")
 @Singleton
 public class RegionValidator implements BaseTypeValidator {
     public static final String TYPE_ID = "Region";
+
+    @Autowired
+    @Qualifier("TextValidator")
+    TextValidator textValidator;
 
     @Override
     public BaseType getType() {
@@ -23,36 +28,10 @@ public class RegionValidator implements BaseTypeValidator {
                                            final JsonNode dataValue,
                                            final CaseFieldDefinition caseFieldDefinition) {
 
-        // Empty text should still check against MIN - MIN may or may not be 0
-        if (isNullOrEmpty(dataValue)) {
-            return Collections.emptyList();
-        }
+        List<ValidationResult> validationResult = textValidator.validate(dataFieldId, dataValue, caseFieldDefinition);
 
-        if (!dataValue.isTextual()) {
-            final String nodeType = dataValue.getNodeType().toString().toLowerCase();
-            return Collections.singletonList(new ValidationResult(nodeType + " is not a string",
-                dataFieldId)
-            );
-        }
-
-        final String value = dataValue.textValue();
-
-        List<ValidationResult> response = (validateRegex(caseFieldDefinition, value, dataFieldId));
-        if ((response != null)) {
-            return response;
-        }
-
-        response = (validateMin(caseFieldDefinition, value, dataFieldId));
-        if ((response != null)) {
-            return response;
-        }
-
-        response = (validateMax(caseFieldDefinition, value, dataFieldId));
-        if ((response != null)) {
-            return response;
-        }
-
-        return Collections.emptyList();
+        return validationResult;
 
     }
+
 }
