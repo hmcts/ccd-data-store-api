c4080387f88895c8cdd3a4020cea349cd946cd53
tomxelliott <tomelliott@Toms-MBP.home>
2021-02-25 14:54:23 +0000

    RDM-11029: Alter the approach to checking work basket result field roles

--- src/main/java/uk/gov/hmcts/ccd/domain/model/definition/SearchResultDefinition.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/definition/SearchResultDefinition.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -28,25 +29,34 @@ public class SearchResultDefinition implements Serializable {
             .collect(Collectors.toList());
     }
 
-    public Map<String, String> getFieldsUserRoles() {
-        Map<String, String> fields = new HashMap<>();
+    public Map<String, List<String>> getFieldsUserRoles() {
+        Map<String, List<String>> fields = new HashMap<>();
         for (SearchResultField srf : this.fields) {
-            fields.put(srf.getCaseFieldId(), srf.getRole());
+            if (fields.containsKey(srf.getCaseFieldId())) {
+                fields.get(srf.getCaseFieldId()).add(srf.getRole());
+            } else {
+                List<String> roles = new ArrayList<>();
+                roles.add(srf.getRole());
+                fields.put(srf.getCaseFieldId(), roles);
+            }
         }
         return fields;
     }
 
     public boolean fieldExists(String caseFieldId) {
-        Map<String, String> fields = getFieldsUserRoles();
+        Map<String, List<String>> fields = getFieldsUserRoles();
         return fields.containsKey(caseFieldId);
     }
 
     public boolean fieldHasRole(String caseFieldId, Set<String> roles) {
-        Map<String, String> fields = getFieldsUserRoles();
-        String role = fields.get(caseFieldId);
-        if (role != null && !roles.contains(role)) {
-            return false;
+        Map<String, List<String>> fields = getFieldsUserRoles();
+        List<String> userRoles = fields.get(caseFieldId);
+        if (userRoles != null) {
+            long count = userRoles.stream()
+                .filter(userRole -> userRole == null || roles.contains(userRole))
+                .count();
+            return count != 0;
         }
-        return true;
+        return false;
     }
 }
