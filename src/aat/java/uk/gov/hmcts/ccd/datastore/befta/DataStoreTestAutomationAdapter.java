03e7481e9bfe015eeae0ffedb77645896d0cf960
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-03-04 13:19:43 +0000

    RDM-11171 - Change that got lost in merge

--- src/aat/java/uk/gov/hmcts/ccd/datastore/befta/DataStoreTestAutomationAdapter.java
+++ src/aat/java/uk/gov/hmcts/ccd/datastore/befta/DataStoreTestAutomationAdapter.java
@@ -9,6 +9,8 @@ import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.exception.FunctionalTestException;
 import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
+import uk.gov.hmcts.befta.util.BeftaUtils;
+import uk.gov.hmcts.befta.util.EnvironmentVariableUtils;
 import uk.gov.hmcts.befta.util.ReflectionUtils;
 
 import java.util.Arrays;
@@ -62,6 +64,7 @@ public class DataStoreTestAutomationAdapter extends DefaultTestAutomationAdapter
 
     @Override
     public Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
+        String docAmUrl = EnvironmentVariableUtils.getRequiredVariable("CASE_DOCUMENT_AM_URL");
         if (key.toString().startsWith("caseIdAsIntegerFrom")) {
             String childContext = key.toString().replace("caseIdAsIntegerFrom_","");
             try {
@@ -102,7 +105,6 @@ public class DataStoreTestAutomationAdapter extends DefaultTestAutomationAdapter
                 scenarioTag = scenarioContext.getCurrentScenarioTag();
             }
             return uniqueStringsPerTestData.get(scenarioTag);
-
         } else if (key.toString().startsWith("approximately ")) {
             try {
                 String actualSizeFromHeaderStr = (String) ReflectionUtils.deepGetFieldInObject(scenarioContext,
@@ -131,6 +133,41 @@ public class DataStoreTestAutomationAdapter extends DefaultTestAutomationAdapter
                 return "expectedValueStr " + expectedValueStr + " not present in response ";
             } catch (Exception e) {
                 throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
+            }
+        } else if (key.equals("documentIdInTheResponse")) {
+            try {
+                String href = (String) ReflectionUtils
+                    .deepGetFieldInObject(scenarioContext,
+                        "testData.actualResponse.body.documents[0]._links.self.href");
+                return href.substring(href.length() - 36);
+            } catch (Exception exception) {
+//                logger.error("Exception while getting the Document ID from the response :{}", exception.getMessage());
+                return "Error extracting the Document Id";
+            }
+        } else if (key.toString().equalsIgnoreCase("validSelfLink")) {
+            try {
+                String self = (String) ReflectionUtils.deepGetFieldInObject(scenarioContext,
+                    "testData.actualResponse.body.documents[0]._links.self.href");
+                BeftaUtils.defaultLog("Self: " + self);
+                if (self != null && self.startsWith(docAmUrl + "/cases/documents/")) {
+                    return self;
+                }
+                return docAmUrl + "/cases/documents/<a document id>";
+            } catch (Exception e) {
+                throw new FunctionalTestException("Couldn't get self link from response field", e);
+            }
+
+        } else if (key.toString().equalsIgnoreCase("validBinaryLink")) {
+            try {
+                String binary = (String) ReflectionUtils.deepGetFieldInObject(scenarioContext,
+                    "testData.actualResponse.body.documents[0]._links.binary.href");
+                BeftaUtils.defaultLog("Binary: " + binary);
+                if (binary != null && binary.startsWith(docAmUrl + "/cases/documents/") && binary.endsWith("/binary")) {
+                    return binary;
+                }
+                return docAmUrl + "/cases/documents/<a document id>/binary";
+            } catch (Exception e) {
+                throw new FunctionalTestException("Couldn't get binary link from response field", e);
             }
         }
         return super.calculateCustomValue(scenarioContext, key);
