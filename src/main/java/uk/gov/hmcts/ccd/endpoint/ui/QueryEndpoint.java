e31cee3f09f72f16183fb6774be8f2e02788ba1f
Sita <sita.kandala@hmcts.net>
2021-03-11 15:50:52 +0000

    RDM-7788 incorporated some review comments

--- src/main/java/uk/gov/hmcts/ccd/endpoint/ui/QueryEndpoint.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/ui/QueryEndpoint.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
