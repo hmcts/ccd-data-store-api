17b70ec51252568bf3d0887ddbd78c355898044c
ruban72 <ruban72@hotmail.com>
2021-03-19 19:17:09 +0000

    TA-394: Fix contractTest class path

--- src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/CaseAssignedUserRolesProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/CaseAssignedUserRolesProviderTest.java
@@ -1,20 +1,22 @@
 package uk.gov.hmcts.ccd.v2.external.controller;
 
-import au.com.dius.pact.provider.junit.IgnoreNoPactsToVerify;
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.loader.PactBroker;
-import au.com.dius.pact.provider.junit.target.TestTarget;
-import au.com.dius.pact.provider.spring.SpringRestPactRunner;
-import au.com.dius.pact.provider.spring.target.MockMvcTarget;
-import org.junit.Before;
-import org.junit.runner.RunWith;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
+import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.ccd.ApplicationParams;
 import uk.gov.hmcts.ccd.data.SecurityUtils;
 
@@ -24,18 +26,15 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @Provider("ccdDataStoreAPI_caseAssignedUserRoles")
-@RunWith(SpringRestPactRunner.class)
-@IgnoreNoPactsToVerify
-@PactBroker(scheme = "${pact.broker.scheme}", host = "${pact.broker.baseUrl}",
-    port = "${pact.broker.port}", tags = {"master"})
+@PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
+    host = "${PACT_BROKER_URL:localhost}", port = "${PACT_BROKER_PORT:80}", consumerVersionSelectors = {
+    @VersionSelector(tag = "master")})
 @TestPropertySource(locations = "/application.properties")
 @WebMvcTest({CaseAssignedUserRolesController.class})
 @AutoConfigureMockMvc(addFilters = false)
 @ContextConfiguration(classes = {CaseAssignedUserRolesProviderTestContext.class})
+@IgnoreNoPactsToVerify
 public class CaseAssignedUserRolesProviderTest {
-
-    @Autowired
-    MockMvc mockMvc;
 
     @Autowired
     ApplicationParams applicationParams;
@@ -46,13 +45,25 @@ public class CaseAssignedUserRolesProviderTest {
     @Autowired
     CaseAssignedUserRolesController caseAssignedUserRolesController;
 
-    @TestTarget
-    public final MockMvcTarget target = new MockMvcTarget();
-
-    @Before
-    public void setUp() {
-        target.setControllers(caseAssignedUserRolesController);
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        if (context != null) {
+            context.verifyInteraction();
+        }
     }
+
+    @BeforeEach
+    void before(PactVerificationContext context) {
+        //System.getProperties().setProperty("pact.verifier.publishResults", "true");
+        MockMvcTestTarget testTarget = new MockMvcTestTarget();
+        testTarget.setControllers(
+            caseAssignedUserRolesController);
+        if (context != null) {
+            context.setTarget(testTarget);
+        }
+    }
+
 
     @State("A User Role exists for a Case")
     public void setUpUserRoleExists() {
