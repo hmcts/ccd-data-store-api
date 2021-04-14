69b74ab13681d5967f1e619e4c17f162268d2d6b
Rajaram <rajaram.nagarajan@solirius.com>
2021-04-14 23:36:58 +0100

    RDM-10564: build issue fixed

--- src/test/java/uk/gov/hmcts/ccd/endpoint/std/CaseDetailsEndpointUserRolesIT.java
+++ src/test/java/uk/gov/hmcts/ccd/endpoint/std/CaseDetailsEndpointUserRolesIT.java
@@ -2,13 +2,19 @@ package uk.gov.hmcts.ccd.endpoint.std;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.ccd.MockUtils;
 import uk.gov.hmcts.ccd.WireMockBaseTest;
+import uk.gov.hmcts.ccd.auditlog.AuditRepository;
+import uk.gov.hmcts.ccd.data.casedetails.search.PaginatedSearchMetadata;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
 import uk.gov.hmcts.ccd.domain.model.std.AuditEvent;
 import uk.gov.hmcts.ccd.domain.model.std.CaseDataContent;
@@ -19,8 +25,12 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.ccd.data.casedetails.SecurityClassification.PRIVATE;
@@ -30,6 +40,8 @@ import static uk.gov.hmcts.ccd.domain.service.common.TestBuildersUtil.CaseDataCo
 public class CaseDetailsEndpointUserRolesIT extends WireMockBaseTest {
     private static final String JURISDICTION = "PROBATE";
     private static final String TEST_EVENT_ID = "TEST_EVENT";
+    private static final String GET_PAGINATED_SEARCH_METADATA_CITIZENS
+        = "/citizens/0/jurisdictions/PROBATE/case-types/TestAddressBookCase/cases/pagination_metadata";
     private static final String UID = "123";
     private static final String CASE_TYPE_CREATOR_ROLE = "TestAddressBookCreatorCase";
     private static final String CASE_TYPE_CREATOR_ROLE_NO_CREATE_ACCESS = "TestAddressBookCreatorNoCreateAccessCase";
@@ -39,8 +51,16 @@ public class CaseDetailsEndpointUserRolesIT extends WireMockBaseTest {
     private MockMvc mockMvc;
     private JdbcTemplate template;
 
+    @SpyBean
+    private AuditRepository auditRepository;
+
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        doReturn(authentication).when(securityContext).getAuthentication();
+        SecurityContextHolder.setContext(securityContext);
+
         MockUtils.setSecurityAuthorities(authentication, MockUtils.ROLE_CASEWORKER_PUBLIC);
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
         template = new JdbcTemplate(db);
@@ -168,6 +188,36 @@ public class CaseDetailsEndpointUserRolesIT extends WireMockBaseTest {
     public void shouldReturn404WhenPostCreateCaseWithNoCreateCaseAccessOnCreatorRoleForCitizen() throws Exception {
         shouldReturn404WhenPostCreateCaseWithNoCreateCaseAccess("citizens",
             CASE_TYPE_CREATOR_ROLE_NO_CREATE_ACCESS);
+    }
+
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:sql/insert_cases.sql" })
+    public void shouldReturnPaginatedSearchMetadataForCitizen() throws Exception {
+
+        assertCaseDataResultSetSize();
+        MockUtils.setSecurityAuthorities(authentication, "role-citizen");
+
+        MvcResult result = mockMvc.perform(get(GET_PAGINATED_SEARCH_METADATA_CITIZENS)
+            .contentType(JSON_CONTENT_TYPE)
+            .header(AUTHORIZATION, "Bearer user1"))
+            .andExpect(status().is(200))
+            .andReturn();
+
+
+        String responseAsString = result.getResponse().getContentAsString();
+        PaginatedSearchMetadata metadata = mapper.readValue(responseAsString, PaginatedSearchMetadata.class);
+
+        assertThat(metadata.getTotalPagesCount(), is(4));
+        assertThat(metadata.getTotalResultsCount(), is(7));
+    }
+
+    /**
+     * Checks that we have the expected test data set size, this is to ensure
+     * that state filtering is correct.
+     */
+    private void assertCaseDataResultSetSize() {
+        final int count = template.queryForObject("SELECT count(1) as n FROM case_data",Integer.class);
+        assertEquals("Incorrect case data size", NUMBER_OF_CASES, count);
     }
 
     private void shouldReturn404WhenPostCreateCaseWithNoCreateCaseAccess(String role, String caseType)
