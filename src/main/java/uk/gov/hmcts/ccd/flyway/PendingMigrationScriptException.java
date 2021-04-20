69de8bbb2d63ad308c1eea091bd90c26419f7211
Abi-Laver <abi.laver@cgi.com>
2021-04-20 16:40:27 +0100

    minor changes

--- src/main/java/uk/gov/hmcts/ccd/flyway/PendingMigrationScriptException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.ccd.flyway;
-
-public class PendingMigrationScriptException extends RuntimeException {
-
-    public PendingMigrationScriptException(String script) {
-        super("Found migration not yet applied " + script);
-    }
-}
