ae8c38d31e7a286775773131defb31e5a2a4be02
it.consulting@tutanota.com <it.consulting@tutanota.com>
2021-05-05 10:54:42 +0100

    RDM-11544: Unspec elastic search is giving 400        [RDM-11544] (https://tools.hmcts.net/jira/browse/RDM-11544).

---
+++ src/main/java/uk/gov/hmcts/ccd/data/casedataaccesscontrol/Attributes.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.ccd.data.casedataaccesscontrol;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Builder
+public class Attributes implements Serializable {
+
+    private static final long serialVersionUID = -3552426127042849422L;
+    private List<String> jurisdiction;
+    private List<String> caseType;
+    private List<String> caseId;
+    private List<String> region;
+    private List<String> location;
+    private List<String> contractType;
+}
