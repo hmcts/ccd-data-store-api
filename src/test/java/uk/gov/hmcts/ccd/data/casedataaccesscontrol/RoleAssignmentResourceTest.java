315f80f4659f4ffca20efdcee3ea7d6abc4eaf9a
it.consulting@tutanota.com <it.consulting@tutanota.com>
2021-05-07 12:07:49 +0100

    RDM-10923: Migrate get case access APIs to new Access Control        [RDM-10923] (https://tools.hmcts.net/jira/browse/RDM-10923).

---
+++ src/test/java/uk/gov/hmcts/ccd/data/casedataaccesscontrol/RoleAssignmentResourceTest.java
@@ -0,0 +1,68 @@
+package uk.gov.hmcts.ccd.data.casedataaccesscontrol;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.RoleAssignment;
+import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.RoleAssignmentAttributes;
+import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.RoleAssignments;
+import uk.gov.hmcts.ccd.domain.service.casedataaccesscontrol.RoleType;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@DisplayName("RoleAssignmentResourceTest")
+class RoleAssignmentResourceTest {
+
+    private List<String> caseIds = Arrays.asList(new String[]{"111", "222"});
+    private List<String> userIds = Arrays.asList(new String[]{"111", "222"});
+    private static final String CASE_ID = "111111";
+
+
+    @Test
+    @DisplayName("isAnExpiredRoleAssignment")
+    void shouldPassForIsAnExpiredRoleAssignment() {
+
+        final long oneHour = 3600000;
+        final RoleAssignments roleAssignments = getRoleAssignments(oneHour);
+        roleAssignments.getRoleAssignments().get(0).isAnExpiredRoleAssignment();
+        assertThat(roleAssignments.getRoleAssignments().get(0).isAnExpiredRoleAssignment(), is(true));
+        assertThat(roleAssignments.getRoleAssignments().get(1).isAnExpiredRoleAssignment(), is(true));
+    }
+
+    @Test
+    @DisplayName("isAnExpiredRoleAssignment")
+    void shouldNotPassForIsAnExpiredRoleAssignment() {
+
+        final long oneHour = 0;
+        final RoleAssignments roleAssignments = getRoleAssignments(oneHour);
+        roleAssignments.getRoleAssignments().get(0).isAnExpiredRoleAssignment();
+        assertThat(roleAssignments.getRoleAssignments().get(0).isAnExpiredRoleAssignment(), is(false));
+        assertThat(roleAssignments.getRoleAssignments().get(1).isAnExpiredRoleAssignment(), is(false));
+    }
+
+
+    private RoleAssignments getRoleAssignments(final long oneHour) {
+
+        final Instant currentTIme = Instant.now();
+        final RoleAssignmentAttributes roleAssignmentAttributes =
+            RoleAssignmentAttributes.builder().caseId(Optional.of(CASE_ID)).build();
+
+        final List<RoleAssignment> roleAssignments = Arrays.asList(
+
+            RoleAssignment.builder().actorId("actorId").roleType(RoleType.CASE.name())
+                .attributes(roleAssignmentAttributes)
+                .beginTime(currentTIme.minusMillis(oneHour)).endTime(currentTIme.plusMillis(oneHour)).build(),
+
+            RoleAssignment.builder().actorId("actorId1").roleType(RoleType.CASE.name())
+                .attributes(roleAssignmentAttributes)
+                .beginTime(currentTIme.minusMillis(oneHour)).endTime(currentTIme.plusMillis(oneHour)).build()
+        );
+        return RoleAssignments.builder().roleAssignments(roleAssignments).build();
+    }
+
+}
