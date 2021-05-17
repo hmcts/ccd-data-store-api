562bb2eba24f1357ff4675ccfec564f373a32cbb
rafalkalita <rkalitainfo@gmail.com>
2021-05-17 21:24:08 +0100

    Fix checkstyle of the mapper

--- src/main/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentsMapperImpl.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentsMapperImpl.java
@@ -19,69 +19,72 @@ public class RoleAssignmentsMapperImpl implements RoleAssignmentsMapper {
 
     @Override
     public RoleAssignments toRoleAssignments(RoleAssignmentResponse roleAssignmentResponse) {
-        if ( roleAssignmentResponse == null ) {
+        if (roleAssignmentResponse == null) {
             return null;
         }
 
         RoleAssignmentsBuilder roleAssignments = RoleAssignments.builder();
 
-        roleAssignments.roleAssignments( roleAssignmentResourceListToRoleAssignmentList( roleAssignmentResponse.getRoleAssignments() ) );
+        roleAssignments.roleAssignments(roleAssignmentResourceListToRoleAssignmentList(
+            roleAssignmentResponse.getRoleAssignments()));
 
         return roleAssignments.build();
     }
 
-    protected RoleAssignmentAttributes roleAssignmentAttributesResourceToRoleAssignmentAttributes(RoleAssignmentAttributesResource roleAssignmentAttributesResource) {
-        if ( roleAssignmentAttributesResource == null ) {
+    protected RoleAssignmentAttributes roleAssignmentAttributesResourceToRoleAssignmentAttributes(
+        RoleAssignmentAttributesResource roleAssignmentAttributesResource) {
+        if (roleAssignmentAttributesResource == null) {
             return null;
         }
 
         RoleAssignmentAttributesBuilder roleAssignmentAttributes = RoleAssignmentAttributes.builder();
 
-        roleAssignmentAttributes.jurisdiction( roleAssignmentAttributesResource.getJurisdiction() );
-        roleAssignmentAttributes.caseId( roleAssignmentAttributesResource.getCaseId() );
-        roleAssignmentAttributes.region( roleAssignmentAttributesResource.getRegion() );
-        roleAssignmentAttributes.location( roleAssignmentAttributesResource.getLocation() );
-        roleAssignmentAttributes.contractType( roleAssignmentAttributesResource.getContractType() );
+        roleAssignmentAttributes.jurisdiction(roleAssignmentAttributesResource.getJurisdiction());
+        roleAssignmentAttributes.caseId(roleAssignmentAttributesResource.getCaseId());
+        roleAssignmentAttributes.region(roleAssignmentAttributesResource.getRegion());
+        roleAssignmentAttributes.location(roleAssignmentAttributesResource.getLocation());
+        roleAssignmentAttributes.contractType(roleAssignmentAttributesResource.getContractType());
 
         return roleAssignmentAttributes.build();
     }
 
     protected RoleAssignment roleAssignmentResourceToRoleAssignment(RoleAssignmentResource roleAssignmentResource) {
-        if ( roleAssignmentResource == null ) {
+        if (roleAssignmentResource == null) {
             return null;
         }
 
         RoleAssignmentBuilder roleAssignment = RoleAssignment.builder();
 
-        roleAssignment.id( roleAssignmentResource.getId() );
-        roleAssignment.actorIdType( roleAssignmentResource.getActorIdType() );
-        roleAssignment.actorId( roleAssignmentResource.getActorId() );
-        roleAssignment.roleType( roleAssignmentResource.getRoleType() );
-        roleAssignment.roleName( roleAssignmentResource.getRoleName() );
-        roleAssignment.classification( roleAssignmentResource.getClassification() );
-        roleAssignment.grantType( roleAssignmentResource.getGrantType() );
-        roleAssignment.roleCategory( roleAssignmentResource.getRoleCategory() );
-        roleAssignment.readOnly( roleAssignmentResource.getReadOnly() );
-        roleAssignment.beginTime( roleAssignmentResource.getBeginTime() );
-        roleAssignment.endTime( roleAssignmentResource.getEndTime() );
-        roleAssignment.created( roleAssignmentResource.getCreated() );
+        roleAssignment.id(roleAssignmentResource.getId());
+        roleAssignment.actorIdType(roleAssignmentResource.getActorIdType());
+        roleAssignment.actorId(roleAssignmentResource.getActorId());
+        roleAssignment.roleType(roleAssignmentResource.getRoleType());
+        roleAssignment.roleName(roleAssignmentResource.getRoleName());
+        roleAssignment.classification(roleAssignmentResource.getClassification());
+        roleAssignment.grantType(roleAssignmentResource.getGrantType());
+        roleAssignment.roleCategory(roleAssignmentResource.getRoleCategory());
+        roleAssignment.readOnly(roleAssignmentResource.getReadOnly());
+        roleAssignment.beginTime(roleAssignmentResource.getBeginTime());
+        roleAssignment.endTime(roleAssignmentResource.getEndTime());
+        roleAssignment.created(roleAssignmentResource.getCreated());
         List<String> list = roleAssignmentResource.getAuthorisations();
-        if ( list != null ) {
-            roleAssignment.authorisations( new ArrayList<String>( list ) );
+        if (list != null) {
+            roleAssignment.authorisations(new ArrayList<String>(list));
         }
-        roleAssignment.attributes( roleAssignmentAttributesResourceToRoleAssignmentAttributes( roleAssignmentResource.getAttributes() ) );
+        roleAssignment.attributes(roleAssignmentAttributesResourceToRoleAssignmentAttributes(
+            roleAssignmentResource.getAttributes()));
 
         return roleAssignment.build();
     }
 
     protected List<RoleAssignment> roleAssignmentResourceListToRoleAssignmentList(List<RoleAssignmentResource> list) {
-        if ( list == null ) {
+        if (list == null) {
             return null;
         }
 
-        List<RoleAssignment> list1 = new ArrayList<RoleAssignment>( list.size() );
-        for ( RoleAssignmentResource roleAssignmentResource : list ) {
-            list1.add( roleAssignmentResourceToRoleAssignment( roleAssignmentResource ) );
+        List<RoleAssignment> list1 = new ArrayList<RoleAssignment>(list.size());
+        for (RoleAssignmentResource roleAssignmentResource : list) {
+            list1.add(roleAssignmentResourceToRoleAssignment(roleAssignmentResource));
         }
 
         return list1;
