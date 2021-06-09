81825e6c75665966db0a21d13c48453d02b65a1b
kiran.yenigala@hmcts.net <Yrlks1091327~>
2021-06-09 11:47:09 +0100

    RDM-12366: fix issue with accessProfile in json response and request

---
+++ src/test/java/uk/gov/hmcts/ccd/domain/model/definition/AccessControlListTest.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.ccd.domain.model.definition;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccessControlListTest {
+
+    @Test
+    void shouldValidateAccessControlListParsing() throws JsonProcessingException {
+        AccessControlList accessControlList = new AccessControlList();
+
+        accessControlList.setUpdate(false);
+        accessControlList.setRead(false);
+        accessControlList.setDelete(false);
+        accessControlList.setCreate(true);
+        accessControlList.setAccessProfile("test");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String value = objectMapper.writeValueAsString(accessControlList);
+
+        System.out.println(value);
+    }
+
+    void shouldValidateAccessControlListSerializationWithRole() throws JsonProcessingException {
+        String accessControlJson = "{\n"
+            + "\t\"role\": \"caseworker-probate-public\",\n"
+            + "\t\"create\": true,\n"
+            + "\t\"read\": true,\n"
+            + "\t\"update\": true,\n"
+            + "\t\"delete\": false\n"
+            + "}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        AccessControlList value = objectMapper.readValue(accessControlJson, AccessControlList.class);
+        assertEquals(value.getAccessProfile(), "caseworker-probate-public");
+    }
+
+    void shouldValidateAccessControlListSerializationWithAccessProfile() throws JsonProcessingException {
+        String accessControlJson = "{\n"
+            + "\t\"accessProfile\": \"caseworker-probate-public\",\n"
+            + "\t\"create\": true,\n"
+            + "\t\"read\": true,\n"
+            + "\t\"update\": true,\n"
+            + "\t\"delete\": false\n"
+            + "}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        AccessControlList value = objectMapper.readValue(accessControlJson, AccessControlList.class);
+        assertEquals(value.getAccessProfile(), "caseworker-probate-public");
+    }
+}
