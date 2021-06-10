9e76d03b27bb7ca306af3db7440a1cbf4450fc45
sumansreenivasan <anuManu10>
2021-06-10 12:24:53 +0100

    CCD-1249 : Added IF NOT EXISTS

--- src/main/resources/db/migration/V20210608__CCD-1249_add_index_hearingVenue.sql
+++ src/main/resources/db/migration/V20210608__CCD-1249_add_index_hearingVenue.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX uidx_case_data_hearing_venue ON public.case_data USING btree (btrim(upper((data #>> '{hearingVenue}'::text[]))));
+CREATE INDEX IF NOT EXISTS uidx_case_data_hearing_venue ON public.case_data USING btree (btrim(upper((data #>> '{hearingVenue}'::text[]))));
