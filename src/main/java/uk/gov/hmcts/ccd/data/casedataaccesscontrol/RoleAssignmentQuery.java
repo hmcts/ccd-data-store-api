d95973c8eb2f438e313216154dc2cfe1123de11b
Matt Nayler <matt.nayler@cgi.com>
2021-07-07 11:26:20 +0100

    RDM-10924 adjust RoleAssignmentQuery structure
       [RDM-10924](https://tools.hmcts.net/jira/browse/RDM-10924)
    
    Adjust RoleAssignmentQuery structure to match required JSON format for
     the RoleAssignmentService.

--- src/main/java/uk/gov/hmcts/ccd/data/casedataaccesscontrol/RoleAssignmentQuery.java
+++ src/main/java/uk/gov/hmcts/ccd/data/casedataaccesscontrol/RoleAssignmentQuery.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.RoleType;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -19,18 +18,12 @@ public class RoleAssignmentQuery {
     private List<String> roleCategory;
     private List<String> validAt;
     private List<String> authorisations;
-    private List<Attributes> attributes;
+    private Attributes attributes;
 
 
     public RoleAssignmentQuery(List<String> caseIds, List<String> userIds) {
-        final var attribute = Attributes.builder().caseId(caseIds).build();
-        final var attributes = new ArrayList<Attributes>();
-        final ArrayList roleType = new ArrayList<Attributes>();
-
-        attributes.add(attribute);
-        roleType.add(RoleType.CASE.name());
         this.actorId = userIds;
-        this.attributes = attributes;
-        this.roleType = roleType;
+        this.attributes = Attributes.builder().caseId(caseIds).build();
+        this.roleType = List.of(RoleType.CASE.name());
     }
 }
