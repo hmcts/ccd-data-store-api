fddd68af643cb11f66ff020978a1ca6e555c0c0c
Rajaram <rajaram.nagarajan@solirius.com>
2021-08-10 17:35:33 +0100

    CCD-913: Added request scope persistence for jurisdiction cache
    (cherry picked from commit 8437049de6d24aa645610061d693b8a48a401455)

--- src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
@@ -9,11 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
-@Profile("!test")
 public class CachingConfiguration {
 
     @Autowired
