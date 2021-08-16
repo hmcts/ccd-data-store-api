aa689b756af808376f9b97baeca2232f2ade503f
Abi-Laver <abi.laver@cgi.com>
2021-08-16 17:32:33 +0100

    minor test changes

--- src/test/java/uk/gov/hmcts/ccd/domain/model/search/global/GlobalSearchRequestPayloadTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/model/search/global/GlobalSearchRequestPayloadTest.java
@@ -1,41 +1,21 @@
-package uk.gov.hmcts.ccd.endpoint.std;
+package uk.gov.hmcts.ccd.domain.model.search.global;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import uk.gov.hmcts.ccd.domain.model.search.global.GlobalSearchRequestPayload;
-import uk.gov.hmcts.ccd.domain.model.search.global.GlobalSearchSortByCategory;
-import uk.gov.hmcts.ccd.domain.model.search.global.GlobalSearchSortDirection;
-import uk.gov.hmcts.ccd.domain.model.search.global.SearchCriteria;
-import uk.gov.hmcts.ccd.domain.model.search.global.SortCriteria;
-import uk.gov.hmcts.ccd.domain.service.globalsearch.GlobalSearchService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-public class GlobalSearchEndpointTest {
+public class GlobalSearchRequestPayloadTest {
 
-
-    @Mock
-    private GlobalSearchService service;
-
-    private GlobalSearchEndpoint endpoint;
-
-    public List<String> listOfValidFields;
+    private List<String> validFields;
 
     @Before
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        endpoint = new GlobalSearchEndpoint(service);
-
-        listOfValidFields = new ArrayList<>();
-        listOfValidFields.add("ValidEntry");
-        listOfValidFields.add("ValidEntryTwo");
+        validFields = List.of("ValidEntry", "ValidEntryTwo");
     }
 
     @Test
@@ -43,8 +23,8 @@ public class GlobalSearchEndpointTest {
 
         GlobalSearchRequestPayload payload = new GlobalSearchRequestPayload();
         SearchCriteria searchCriteria = new SearchCriteria();
-        searchCriteria.setCaseManagementBaseLocationIds(listOfValidFields);
-        searchCriteria.setCaseManagementRegionIds(listOfValidFields);
+        searchCriteria.setCaseManagementBaseLocationIds(validFields);
+        searchCriteria.setCaseManagementRegionIds(validFields);
         payload.setSearchCriteria(searchCriteria);
 
         payload.setDefaults();
@@ -67,16 +47,14 @@ public class GlobalSearchEndpointTest {
         payload.setMaxReturnRecordCount(100);
         payload.setStartRecordNumber(1);
         SearchCriteria searchCriteria = new SearchCriteria();
-        searchCriteria.setCaseManagementBaseLocationIds(listOfValidFields);
-        searchCriteria.setCaseManagementRegionIds(listOfValidFields);
+        searchCriteria.setCaseManagementBaseLocationIds(validFields);
+        searchCriteria.setCaseManagementRegionIds(validFields);
         payload.setSearchCriteria(searchCriteria);
         SortCriteria sortCriteriaOne = new SortCriteria();
         sortCriteriaOne.setSortBy(GlobalSearchSortByCategory.CASE_NAME.getCategoryName());
         SortCriteria sortCriteriaTwo = new SortCriteria();
         sortCriteriaTwo.setSortDirection(GlobalSearchSortDirection.DESCENDING.name());
-        List<SortCriteria> sortCriteriaList = new ArrayList<>();
-        sortCriteriaList.add(sortCriteriaOne);
-        sortCriteriaList.add(sortCriteriaTwo);
+        List<SortCriteria> sortCriteriaList = List.of(sortCriteriaOne, sortCriteriaTwo);
         payload.setSortCriteria(sortCriteriaList);
 
         payload.setDefaults();
