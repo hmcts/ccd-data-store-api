2ab9f46dc70406f7c01a03f2791e2df100c04f10
Semenu Aggor <semenu.aggor@HMCTS.NET>
2021-08-27 14:09:49 +0100

    RDM-12718 Rebase global_search branch and update GlobalSearchService interface

--- src/main/java/uk/gov/hmcts/ccd/domain/service/globalsearch/GlobalSearchService.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/globalsearch/GlobalSearchService.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.ccd.domain.service.globalsearch;
 
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.ccd.domain.dto.globalsearch.GlobalSearchResponse;
+import uk.gov.hmcts.ccd.domain.model.search.CaseSearchResult;
 import uk.gov.hmcts.ccd.domain.model.search.global.GlobalSearchRequestPayload;
 
 @Service
@@ -8,4 +10,6 @@ public interface GlobalSearchService {
 
     void assembleSearchQuery(GlobalSearchRequestPayload payload);
 
+    GlobalSearchResponse transformResponse(GlobalSearchRequestPayload requestPayload,
+                                           CaseSearchResult caseSearchResult);
 }
