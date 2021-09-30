79ac905899dfaa06445fb83f55339d5d8ef29ee6
Dan Lysiak <daniel.lysiak1@hmcts.net>
2021-09-30 18:49:47 +0100

    RDM-12864 - Always get user classification based on case

--- src/main/java/uk/gov/hmcts/ccd/domain/service/common/AttributeBasedSecurityClassificationService.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/AttributeBasedSecurityClassificationService.java
@@ -36,13 +36,9 @@ public class AttributeBasedSecurityClassificationService
 
     @Override
     public Optional<SecurityClassification> getUserClassification(CaseDetails caseDetails, boolean create) {
-        Set<AccessProfile> accessProfiles;
-        if (create) {
-            accessProfiles = caseDataAccessControl.generateAccessProfilesByCaseTypeId(caseDetails.getCaseTypeId());
-        } else {
-            accessProfiles = caseDataAccessControl
-                .generateAccessProfilesByCaseReference(caseDetails.getReferenceAsString());
-        }
+        Set<AccessProfile> accessProfiles = caseDataAccessControl
+            .generateAccessProfilesByCaseReference(caseDetails.getReferenceAsString());
+        
         return getMaxSecurityClassification(accessProfiles);
     }
 
