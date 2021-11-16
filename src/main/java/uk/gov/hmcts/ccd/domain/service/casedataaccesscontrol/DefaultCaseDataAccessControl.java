a6d46cc58c5db3090ce2bace043018efe1abed03
kiran.yenigala@hmcts.net <Yrlks1091327~>
2021-11-16 08:59:51 +0000

    RDM-12928: Fix failing befta tests

--- src/main/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/DefaultCaseDataAccessControl.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/DefaultCaseDataAccessControl.java
@@ -289,7 +289,7 @@ public class DefaultCaseDataAccessControl implements NoCacheCaseDataAccessContro
 
     @Override
     public Set<SecurityClassification> getUserClassifications(CaseTypeDefinition caseTypeDefinition) {
-        Set<AccessProfile> accessProfiles = generateAccessProfilesByCaseTypeId(caseTypeDefinition.getId());
+        Set<AccessProfile> accessProfiles = generateOrganisationalAccessProfilesByCaseTypeId(caseTypeDefinition.getId());
         return getSecurityClassifications(accessProfiles);
     }
 
