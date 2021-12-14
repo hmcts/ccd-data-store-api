45a9b164121ee801609794ace721d08d1afa522b
Dan Lysiak <daniel.lysiak1@hmcts.net>
2021-12-14 13:07:42 +0000

    Revert "CCD-2343: Request scope caching improvement for CaseDefinitionRepository"
    This reverts commit 2159752b84b29c09a5d3cf4600dfa8f53b6d86c1.

--- src/aat/resources/features/F-136 - AC CRUD internal work-basket-inputs/F-136.feature
+++ src/aat/resources/features/F-136 - AC CRUD internal work-basket-inputs/F-136.feature
@@ -14,7 +14,7 @@ Feature: F-136: Get Workbasket Input Details with access Internal API CRUD Tests
     And the response [contains a HTTP 404 Not Found],
     And the response has all other details as expected.
 
-  @S-136.2 @Ignore
+  @S-136.2
   Scenario: User getting workbasket inputs with no CaseField R access has field filtered out of response
     Given a user with [an active profile in CCD],
     When a request is prepared with appropriate values,
--- src/main/java/uk/gov/hmcts/ccd/data/definition/CachedCaseDefinitionRepository.java
+++ src/main/java/uk/gov/hmcts/ccd/data/definition/CachedCaseDefinitionRepository.java
@@ -17,7 +17,6 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.Maps.newHashMap;
-import static java.lang.String.format;
 
 @Service
 @Qualifier(CachedCaseDefinitionRepository.QUALIFIER)
@@ -28,14 +27,12 @@ public class CachedCaseDefinitionRepository implements CaseDefinitionRepository 
     private static final Logger LOGGER = LoggerFactory.getLogger(CachedCaseDefinitionRepository.class);
 
     public static final String QUALIFIER = "cached";
-    private static final String CASE_TYPE_KEY_FORMAT = "%s_%d";
 
     private final CaseDefinitionRepository caseDefinitionRepository;
     private final Map<String, List<CaseTypeDefinition>> caseTypesForJurisdictions = newHashMap();
     private final Map<String, CaseTypeDefinitionVersion> versions = newHashMap();
     private final Map<String, UserRole> userRoleClassifications = newHashMap();
     private final Map<String, List<FieldTypeDefinition>> baseTypes = newHashMap();
-    private final Map<String, CaseTypeDefinition> caseTypes = newHashMap();
 
     @Autowired
     public CachedCaseDefinitionRepository(@Qualifier(DefaultCaseDefinitionRepository.QUALIFIER)
@@ -52,13 +49,12 @@ public class CachedCaseDefinitionRepository implements CaseDefinitionRepository 
     @Override
     public CaseTypeDefinition getCaseType(final String caseTypeId) {
         CaseTypeDefinitionVersion latestVersion = this.getLatestVersion(caseTypeId);
-        return this.getCaseType(latestVersion.getVersion(), caseTypeId);
+        return caseDefinitionRepository.getCaseType(latestVersion.getVersion(), caseTypeId);
     }
 
     @Override
     public CaseTypeDefinition getCaseType(int version, String caseTypeId) {
-        return caseTypes.computeIfAbsent(format(CASE_TYPE_KEY_FORMAT, caseTypeId, version),
-            e -> caseDefinitionRepository.getCaseType(version, caseTypeId));
+        return caseDefinitionRepository.getCaseType(version, caseTypeId);
     }
 
     @Override
