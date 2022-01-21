d1955e57db3dc5c849aca9474b8a0343e58987ab
it.consulting@tutanota.com <it.consulting@tutanota.com>
2022-01-21 09:15:41 +0000

    CCD-959: Improve performance of searches executed on all case types (ctid=*)    [CCD-959] (https://tools.hmcts.net/jira/browse/CCD-959).

--- src/test/java/uk/gov/hmcts/ccd/v2/external/controller/CaseControllerTestIT.java
+++ src/test/java/uk/gov/hmcts/ccd/v2/external/controller/CaseControllerTestIT.java
@@ -293,6 +293,7 @@ class CaseControllerTestIT extends WireMockBaseTest {
         final String postCode = "SW1H 9AJ";
 
         Map<String, JsonNode> caseData = new HashMap<>();
+        caseData.put("TextField1", JacksonUtils.MAPPER.readTree("\"" + testFieldValue + "\""));
         caseData.put("PersonFirstName", JacksonUtils.MAPPER.readTree("\"" + firstNameValue + "\""));
         caseData.put("PersonLastName", JacksonUtils.MAPPER.readTree("\"" + lastNameValue + "\""));
         caseData.put("PersonAddress", JacksonUtils.MAPPER.readTree("{\n"
@@ -302,11 +303,11 @@ class CaseControllerTestIT extends WireMockBaseTest {
 
         final CaseDataContent caseDetailsToSave = newCaseDataContent().withData(caseData).build();
         final Event triggeringEvent = anEvent().build();
-        triggeringEvent.setEventId(TEST_EVENT_ID_2);
+        triggeringEvent.setEventId(TEST_EVENT_ID);
         triggeringEvent.setDescription(description);
         triggeringEvent.setSummary(summary);
         caseDetailsToSave.setEvent(triggeringEvent);
-        final String token = generateEventTokenNewCase(UID, JURISDICTION, CASE_TYPE_WITH_SEARCH_PARTY, TEST_EVENT_ID_2);
+        final String token = generateEventTokenNewCase(UID, JURISDICTION, CASE_TYPE_WITH_SEARCH_PARTY, TEST_EVENT_ID);
         caseDetailsToSave.setToken(token);
 
         final MvcResult mvcResult = mockMvc.perform(post(URL)
