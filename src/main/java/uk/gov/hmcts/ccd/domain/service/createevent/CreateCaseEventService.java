790b9ebd63a212b252fd036ceb8cc804f1679593
KleoG <kleogeorgiou@hotmail.com>
2022-02-12 00:04:41 +0000

    RDM-12851 - Refactor code

--- src/main/java/uk/gov/hmcts/ccd/domain/service/createevent/CreateCaseEventService.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/createevent/CreateCaseEventService.java
@@ -252,6 +252,8 @@ public class CreateCaseEventService {
         List<String> preCallbackCaseLinks = getCaseLinks(caseDetailsBeforeCallback.getData(), caseFieldDefinitions);
         List<String> postCallbackCaseLinks = getCaseLinks(caseDetailsAfterCallback.getData(), caseFieldDefinitions);
 
+        // TODO: CaseTypeId needs to be 'case type id of the case to which the case links' so will need to possibly
+        //  be updated to take in the linked case - this will need to be confirmed
         caseLinkService.updateCaseLinks(caseDetailsBeforeCallback.getReference(),
                                         caseDetailsBeforeCallback.getCaseTypeId(),
                                         preCallbackCaseLinks, postCallbackCaseLinks);
