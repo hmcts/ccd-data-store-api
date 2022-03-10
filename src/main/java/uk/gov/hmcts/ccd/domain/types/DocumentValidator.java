4d0c4983e190784524bbfb58e083fa81e5aab7d5
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2022-03-10 08:04:17 +0000

    RDM-13083: Update case validation - Check type of the new Document sub-fields

--- src/main/java/uk/gov/hmcts/ccd/domain/types/DocumentValidator.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/types/DocumentValidator.java
@@ -1,20 +1,20 @@
 package uk.gov.hmcts.ccd.domain.types;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Qualifier;
+import uk.gov.hmcts.ccd.ApplicationParams;
+import uk.gov.hmcts.ccd.data.definition.CachedCaseDefinitionRepository;
+import uk.gov.hmcts.ccd.domain.model.definition.CaseFieldDefinition;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import uk.gov.hmcts.ccd.ApplicationParams;
-import uk.gov.hmcts.ccd.domain.model.definition.CaseFieldDefinition;
+import java.util.stream.Collectors;
 
 
 @Named("DocumentValidator")
@@ -23,13 +23,25 @@ public class DocumentValidator implements BaseTypeValidator {
     public static final String TYPE_ID = "Document";
     static final String DOCUMENT_URL = "document_url";
     private static final String DOCUMENT_BINARY_URL = "document_binary_url";
+    private static final String CATEGORY_ID = "category_id";
+    private static final String UPLOAD_TIMESTAMP = "upload_timestamp";
 
     private static final Logger LOG = LoggerFactory.getLogger(DocumentValidator.class);
 
     private final ApplicationParams applicationParams;
+    private final TextValidator textValidator;
+    private final DateTimeValidator dateTimeValidator;
+    private final CachedCaseDefinitionRepository caseDefinitionRepository;
 
-    public DocumentValidator(ApplicationParams applicationParams) {
+    public DocumentValidator(ApplicationParams applicationParams,
+                             @Qualifier("TextValidator") TextValidator textValidator,
+                             @Qualifier("DateTimeValidator") DateTimeValidator dateTimeValidator,
+                             @Qualifier(CachedCaseDefinitionRepository.QUALIFIER)
+                                 CachedCaseDefinitionRepository caseDefinitionRepository) {
         this.applicationParams = applicationParams;
+        this.textValidator = textValidator;
+        this.dateTimeValidator = dateTimeValidator;
+        this.caseDefinitionRepository = caseDefinitionRepository;
     }
 
     @Override
@@ -91,7 +103,55 @@ public class DocumentValidator implements BaseTypeValidator {
             }
         }
 
+        if (dataValue.has(CATEGORY_ID)) {
+            final JsonNode categoryId = dataValue.get(CATEGORY_ID);
+
+            if (checkIfNullOrEmpty(categoryId)) {
+                LOG.info("{} is empty or null", CATEGORY_ID);
+                return Collections.singletonList(new ValidationResult(
+                    CATEGORY_ID + " is empty or null", dataFieldId));
+            }
+
+            final List<ValidationResult> validationResults =
+                textValidator.validate(dataFieldId, categoryId, caseFieldDefinition);
+
+            if (!validationResults.isEmpty()) {
+                LOG.info("{} validation failure", CATEGORY_ID);
+                return validationResults.stream()
+                    .map(validationResult ->
+                        new ValidationResult(
+                            CATEGORY_ID + " " + validationResult.getErrorMessage(),validationResult.getFieldId()))
+                    .collect(Collectors.toList());
+            }
+        }
+
+        if (dataValue.has(UPLOAD_TIMESTAMP)) {
+            final JsonNode uploadTimeStamp = dataValue.get(UPLOAD_TIMESTAMP);
+
+            if (checkIfNullOrEmpty(uploadTimeStamp)) {
+                LOG.info("{} is empty or null", UPLOAD_TIMESTAMP);
+                return Collections.singletonList(new ValidationResult(
+                    UPLOAD_TIMESTAMP + " is empty or null", dataFieldId));
+            }
+
+            final List<ValidationResult> validationResults =
+                dateTimeValidator.validate(dataFieldId, uploadTimeStamp, caseFieldDefinition);
+
+            if (!validationResults.isEmpty()) {
+                LOG.info("{} validation failure", UPLOAD_TIMESTAMP);
+                return validationResults.stream()
+                    .map(validationResult ->
+                        new ValidationResult(
+                            UPLOAD_TIMESTAMP + " " + validationResult.getErrorMessage(),validationResult.getFieldId()))
+                    .collect(Collectors.toList());
+            }
+        }
+
         return Collections.emptyList();
+    }
+
+    public Boolean checkIfNullOrEmpty(JsonNode dataValue) {
+        return !dataValue.isTextual() || isNullOrEmpty(dataValue);
     }
 
 }
