f2d9110483f20690f3ecfa388e1019acb3a5a6ee
mehmet.duran <mehmet.duran@hmcts.net>
2022-03-22 14:16:51 +0000

    ccd-2526

--- src/main/resources/db/useful-queries/Delete Script.sql
+++ src/main/resources/db/useful-queries/Delete Script.sql
@@ -2,11 +2,14 @@
 -- This script deletes the records from the tables by last_modified and case_data_id criteria.
 -- The select retrieves the case_data_ids to create the temp table to be used as resource of case_data_ids in delete statements.
 -- CaseTypeId array can be modified based on the delete requirement
+-- Either of caseTypeReferences or jurisdictionReferences arrays can be used as per the requirement.
 
 DO $$
 DECLARE
 
-caseTypeIds constant text[] := array['AAT','AAT_AUTH_15','AAT_AUTH_2','AAT_AUTH_4','AAT_AUTH_5','AAT_AUTH_8','AAT_PRIVATE','AAT_PRIVATE_B''AllDataTypes2','CaseProgression',
+jurisdictionReferences constant text[] := array['AUTOTEST1', 'AUTOTEST2', 'BEFTA_JURISDICTION_1', 'BEFTA_JURISDICTION_2', 'BEFTA_JURISDICTION_3', 'BEFTA_MASTER'];
+
+caseTypeReferences constant text[] := array['AAT','AAT_AUTH_15','AAT_AUTH_2','AAT_AUTH_4','AAT_AUTH_5','AAT_AUTH_8','AAT_PRIVATE','AAT_PRIVATE_B','AllDataTypes2','CaseProgression',
                                             			'CaseViewCallbackMessages','ComplexCollectionComplex','ComplexCRUD','Conditionals','MAPPER','MultiplePages','Questions2','SC_Private',
                                             			'SC_Public','SC_Restricted','Tabs','WBSortOrder','AAT_PRIVATE2','BEFTA_CASETYPE_1_1','BEFTA_CASETYPE_2_1','BEFTA_CASETYPE_3_1','BEFTA_CASETYPE_3_2','BEFTA_CASETYPE_3_4',
                                             			'FT_ComplexCollectionComplex','FT_ConditionalPostState','FT_CRUD','FT_CRUD_2','FT_CRUD_3','FT_DateTimeFormats','FT_GlobalSearch','FT_MasterCaseType'];
@@ -17,7 +20,9 @@ CREATE TEMP TABLE tmp_case_data_ids ON COMMIT DROP AS
 		   SELECT id
 			 FROM case_data cd
 			WHERE cd.last_modified <= 'now'::timestamp - '3 MONTH'::interval
-			  AND case_type_id = any(caseTypeIds);
+			  and cd.jurisdiction = any(jurisdictionReferences)
+			  --AND case_type_id = any(caseTypeReferences)
+			  ;
 
     DELETE FROM case_users_audit WHERE case_data_id IN (
         select id from tmp_case_data_ids
