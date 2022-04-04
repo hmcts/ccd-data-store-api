d1f8e9eaa7ff6b658a6225a1b659c0fe09f9f5a9
Hemanth Potipati <hrpotipati@gmail.com>
2022-04-04 09:20:38 +0100

    Add CCD-2661 Step2 to Release-25.2 (#1932)
    * rename and cleanup env vars in base
    
    * generalize names of configs
    
    * correct and adjust cache configuration
    
    * set default max idle 4 hours
    
    * generalize names of configs
    
    * restore mistaken removal
    
    * restore to max idle where better
    
    * adjust cache config
    
    * rename app config for contract tests
    
    * RDM-12806 ccd-test-definitions prerelease for CR&D
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    ccd-test-definitions prerelease for CR&D.
    
    * RDM-12806 temporary changes to configure pipeline
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    Temporary changes to configure preview pipeline to point to a suitable
     definition-store pipeline: hmcts/ccd-definition-store-api#1172
    
    * remove max idle for ref data cache and clear default ttl
    
    * wire for ci/cd with ES-tests
    
    * Fix tests
    
    * minor fix
    
    * reorder test props
    
    * update test definition release number and FTAs
    
    * update test definition release number and FTAs
    
    * RDM-12806 update test definition prerelease number
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    FTA Updates for test definition prerelease changes.
    
    * RDM-12806 temporary changes to configure pipeline
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    Temporary changes to configure preview pipeline to point to a suitable
     definition-store pipeline: hmcts/ccd-definition-store-api#1089
    
    * temp update def store url
    
    * RDM-12806 FTA fix after master merge
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    Fix FTA scripts after merge with master branch, i.e. edits for
     GlobalSearch's SearchCriteria in FT_MasterCaseType.
    
    * implementation of http client status code handling
    
    * RDM-12806 update test definition prerelease number
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    Build against the next `ccd-test-definitions` pre-release,
     see hmcts/ccd-test-definitions#33.
    
    * RDM-12806 ccd-test-definitions pre-release
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    Build against the next `ccd-test-definitions` pre-release,
     see hmcts/ccd-test-definitions#33.
    
    * RDM-12806 ccd-test-definitions release
    
       [RDM-12806](https://tools.hmcts.net/jira/browse/RDM-12806)
    
    Build against the latest `ccd-test-definitions` release.
    
    * CCD-2893:CVE-2022-21724 - ccd-data-store-ap
    
    * CCD-2893:CVE-2022-21724 - ccd-data-store-api
    
    Co-authored-by: Mutlu Sancaktutar <mutlu.sancaktutar@hmcts.net>
    Co-authored-by: Matt Nayler <matt.nayler@cgi.com>
    Co-authored-by: kaanaktas <kaanaktas@gmail.com>
    Co-authored-by: duranmf <mehmet.duran1@hmcts.net>

--- src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/CachingConfiguration.java
@@ -20,7 +20,7 @@ import static uk.gov.hmcts.ccd.data.ReferenceDataRepository.SERVICES_CACHE;
 public class CachingConfiguration {
 
     @Autowired
-    ApplicationParams applicationParams;
+    ApplicationParams appParams;
 
     @Bean
     public Config hazelCastConfig() {
@@ -35,31 +35,36 @@ public class CachingConfiguration {
     }
 
     private void configCaches(Config config) {
-        final int defaultCacheMaxIdle = applicationParams.getDefaultCacheMaxIdleSecs();
-        final int defaultCacheTtl = applicationParams.getDefaultCacheTtlSecs();
-        config.addMapConfig(newMapConfigWithMaxIdle("caseTypeDefinitionsCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("workBasketResultCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("searchResultCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("searchCasesResultCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("searchInputDefinitionCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("workbasketInputDefinitionCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("caseTabCollectionCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("wizardPageCollectionCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("allJurisdictionsCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("userRolesCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("caseTypePseudoRoleToAccessProfileCache", defaultCacheMaxIdle));
-        config.addMapConfig(newMapConfigWithMaxIdle("userInfoCache", applicationParams.getUserCacheTTLSecs()));
-        config.addMapConfig(newMapConfigWithMaxIdle("idamUserRoleCache",
-            applicationParams.getUserCacheTTLSecs()));
-        config.addMapConfig(newMapConfigWithTtl("systemUserTokenCache",
-            applicationParams.getSystemUserTokenCacheTTLSecs()));
-        config.addMapConfig(newMapConfigWithMaxIdle("bannersCache", defaultCacheTtl));
-        config.addMapConfig(newMapConfigWithMaxIdle("jurisdictionUiConfigsCache", defaultCacheTtl));
+        final int defaultMaxIdle = appParams.getDefaultCacheMaxIdleSecs();
+        final int defaultCacheTtl = appParams.getDefaultCacheTtlSecs();
+        final int userCacheTtl = appParams.getUserCacheTTLSecs();
+        final int jurisdictionCacheTtl = appParams.getJurisdictionTTLSecs();
+        final int systemUserTokenCacheTTLSecs = appParams.getSystemUserTokenCacheTTLSecs();
+
+        config.addMapConfig(newMapConfigWithMaxIdle("caseTypeDefinitionsCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("workBasketResultCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("searchResultCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("searchCasesResultCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("searchInputDefinitionCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("workbasketInputDefinitionCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("caseTabCollectionCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("wizardPageCollectionCache", defaultMaxIdle));
+        config.addMapConfig(newMapConfigWithMaxIdle("caseTypePseudoRoleToAccessProfileCache", defaultMaxIdle));
+
+        config.addMapConfig(newMapConfigWithTtl("allJurisdictionsCache", jurisdictionCacheTtl)); // Fixed.
+        config.addMapConfig(newMapConfigWithTtl("userRolesCache", userCacheTtl)); // Fixed.
+        config.addMapConfig(newMapConfigWithTtl("userInfoCache", userCacheTtl)); // Fixed.
+        config.addMapConfig(newMapConfigWithTtl("idamUserRoleCache", userCacheTtl)); // Fixed.
+        config.addMapConfig(newMapConfigWithTtl("systemUserTokenCache", systemUserTokenCacheTTLSecs));
+        config.addMapConfig(newMapConfigWithTtl("bannersCache", defaultCacheTtl)); // Fixed.
+        config.addMapConfig(newMapConfigWithTtl("jurisdictionUiConfigsCache", defaultCacheTtl)); // Fixed.
         config.addMapConfig(newMapConfigWithTtl("caseTypeDefinitionLatestVersionCache", defaultCacheTtl));
         config.addMapConfig(newMapConfigWithTtl("caseRolesCache", defaultCacheTtl));
-        config.addMapConfig(newMapConfigWithTtl("jurisdictionCache", applicationParams.getJurisdictionTTLSecs()));
-        config.addMapConfig(newMapConfigWithTtl(BUILDING_LOCATIONS_CACHE, applicationParams.getRefDataCacheTtlInSec()));
-        config.addMapConfig(newMapConfigWithTtl(SERVICES_CACHE, applicationParams.getRefDataCacheTtlInSec()));
+        config.addMapConfig(newMapConfigWithTtl("jurisdictionCache", jurisdictionCacheTtl));
+        config.addMapConfig(newMapConfigWithTtl(BUILDING_LOCATIONS_CACHE, appParams.getRefDataCacheTtlInSec())
+            .setMaxIdleSeconds(0));
+        config.addMapConfig(newMapConfigWithTtl(SERVICES_CACHE, appParams.getRefDataCacheTtlInSec())
+            .setMaxIdleSeconds(0));
     }
 
     private MapConfig newMapConfigWithMaxIdle(final String name, final Integer maxIdle) {
@@ -67,14 +72,15 @@ public class CachingConfiguration {
     }
 
     private MapConfig newMapConfigWithTtl(final String name, final Integer ttl) {
-        return newMapConfig(name).setTimeToLiveSeconds(ttl);
+        final int defaultCacheMaxIdle = appParams.getDefaultCacheMaxIdleSecs();
+        return newMapConfig(name).setTimeToLiveSeconds(ttl).setMaxIdleSeconds(defaultCacheMaxIdle);
     }
 
     private MapConfig newMapConfig(final String name) {
         final EvictionConfig evictionConfig = new EvictionConfig()
-            .setEvictionPolicy(applicationParams.getDefaultCacheEvictionPolicy())
+            .setEvictionPolicy(appParams.getDefaultCacheEvictionPolicy())
             .setMaxSizePolicy(MaxSizePolicy.PER_NODE)
-            .setSize(applicationParams.getDefaultCacheMaxSize());
+            .setSize(appParams.getDefaultCacheMaxSize());
         return new MapConfig().setName(name)
             .setEvictionConfig(evictionConfig);
     }
