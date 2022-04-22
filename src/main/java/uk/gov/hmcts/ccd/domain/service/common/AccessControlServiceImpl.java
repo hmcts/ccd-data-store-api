fa509f3c2214b563545a7e0b01321e611b84668a
rajaram <rajaram.nagarajan@solirius.com>
2022-04-22 19:31:32 +0100

    CCD-2950: Multiparty fix for child case view field

--- src/main/java/uk/gov/hmcts/ccd/domain/service/common/AccessControlServiceImpl.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/AccessControlServiceImpl.java
@@ -211,17 +211,20 @@ public class AccessControlServiceImpl implements AccessControlService {
 
                 if (caseFieldOpt.isPresent()) {
                     CaseFieldDefinition field = caseFieldOpt.get();
+                    boolean shouldRemoveCaseViewField = false;
+
                     if (!hasAccessControlList(accessProfiles, access, field.getAccessControlLists())) {
+                        shouldRemoveCaseViewField = shouldRemoveCaseViewFieldIfNoReadAccess(caseTypeId,
+                            caseReference, eventId, isMultipartyFixEnabled, multipartyCaseTypes, multipartyEvents,
+                            caseViewFieldId, field, accessProfiles);
                         caseViewField.setDisplayContext(READONLY);
-                        if (shouldRemoveCaseViewFieldIfNoReadAccess(caseTypeId, caseReference, eventId,
-                            isMultipartyFixEnabled, multipartyCaseTypes, multipartyEvents, caseViewFieldId,
-                            field, accessProfiles)) {
+
+                        if (shouldRemoveCaseViewField) {
                             filteredCaseFieldIds.add(caseViewField.getId());
-                            return;
                         }
                     }
 
-                    if (field.isCompoundFieldType()) {
+                    if (!shouldRemoveCaseViewField && field.isCompoundFieldType()) {
                         setChildrenAsReadOnlyIfNoAccess(caseTypeId, caseReference, eventId, isMultipartyFixEnabled,
                             multipartyCaseTypes, multipartyEvents, caseEventTrigger.getWizardPages(), field.getId(),
                             field, access, accessProfiles, caseViewField);
