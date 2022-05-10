1029cb5e8427ea46f5e052d9cd1df44815dae68e
Sita <sita.kandala@hmcts.net>
2022-05-10 17:22:59 +0100

    CCD-2467 update logback Appinsights logging initial commit

--- src/main/java/uk/gov/hmcts/ccd/data/SecurityUtils.java
+++ src/main/java/uk/gov/hmcts/ccd/data/SecurityUtils.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.ccd.data;
 
 import com.auth0.jwt.JWT;
-import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
