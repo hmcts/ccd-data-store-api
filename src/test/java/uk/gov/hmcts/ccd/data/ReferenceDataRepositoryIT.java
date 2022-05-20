61732747a4a0a973ed400fa238b0d709f2a12605
Semenu Aggor <semenu.aggor@HMCTS.NET>
2022-05-20 12:30:27 +0100

    CCD-2952 Fix intermittent failing Reference Data integration tests

--- src/test/java/uk/gov/hmcts/ccd/data/ReferenceDataRepositoryIT.java
+++ src/test/java/uk/gov/hmcts/ccd/data/ReferenceDataRepositoryIT.java
@@ -1,47 +1,24 @@
 package uk.gov.hmcts.ccd.data;
 
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Isolated;
-import uk.gov.hmcts.ccd.WireMockBaseTest;
 import uk.gov.hmcts.ccd.domain.model.refdata.BuildingLocation;
 import uk.gov.hmcts.ccd.domain.model.refdata.CourtVenue;
 import uk.gov.hmcts.ccd.domain.model.refdata.ServiceReferenceData;
 
-import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static java.util.Collections.emptyList;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.ccd.data.ReferenceDataRepository.BUILDING_LOCATIONS_CACHE;
 import static uk.gov.hmcts.ccd.data.ReferenceDataRepository.BUILDING_LOCATIONS_PATH;
-import static uk.gov.hmcts.ccd.data.ReferenceDataRepository.SERVICES_CACHE;
 import static uk.gov.hmcts.ccd.data.ReferenceDataRepository.SERVICES_PATH;
 
 @Isolated("Isolate from other integration tests that may utilise the same ReferenceData cache.")
-class ReferenceDataRepositoryIT extends WireMockBaseTest implements ReferenceDataTestFixtures {
-
-    @Inject
-    private ReferenceDataRepository underTest;
-
-    @BeforeEach
-    void prepare() {
-        List.of(BUILDING_LOCATIONS_CACHE, SERVICES_CACHE)
-            .parallelStream()
-            .forEach(cacheName -> underTest.invalidateCache(cacheName));
-
-        List.of(BUILDING_LOCATIONS_STUB_ID, SERVICES_STUB_ID).forEach(id -> {
-            final Optional<StubMapping> stubMapping = Optional.ofNullable(wireMockServer.getSingleStubMapping(id));
-            stubMapping.ifPresent(mapping -> wireMockServer.removeStub(mapping));
-        });
-        wireMockServer.resetRequests();
-    }
+class ReferenceDataRepositoryIT extends AbstractReferenceDataIT {
 
     @Test
     void testShouldGetBuildingLocationsSuccessfullyFromUpstream() {
