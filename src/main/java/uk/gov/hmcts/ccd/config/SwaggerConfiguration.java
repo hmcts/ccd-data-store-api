bd2c5a123f8fe08892025e76da433a0fd6a1767f
Mike Brown <michael.brown@hmcts.net>
2022-08-02 12:29:57 +0100

    CCD-3509 CVE-2021-22044 lots of changes including Gradle upgrade to 6.8.3, springboot upgrade to 2.6.10, webEndpointServletHandlerMapping() method added to SwaggerConfiguration and spring.mvc.pathmatch.matching-strategy=ANT_PATH_MATCHER for springfox springboot conflict. Conflicts Resolved

--- src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
@@ -1,11 +1,22 @@
 package uk.gov.hmcts.ccd.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
+import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
+import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,6 +30,12 @@ import uk.gov.hmcts.ccd.endpoint.std.CaseDetailsEndpoint;
 import uk.gov.hmcts.ccd.endpoint.ui.QueryEndpoint;
 import uk.gov.hmcts.ccd.v2.external.controller.CaseController;
 import uk.gov.hmcts.ccd.v2.internal.controller.UICaseController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -101,5 +118,35 @@ public class SwaggerConfiguration {
             .accepts(Collections.singleton(MediaType.APPLICATION_JSON))
             .required(true)
             .build();
+    }
+
+    //CCD-3509 CVE-2021-22044 required to fix null pointers in integration tests,
+    //conflict in Springfox after Springboot 2.6.10
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+        ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
+        EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
+        WebEndpointProperties webEndpointProperties, Environment environment) {
+
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
+            basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+            corsProperties.toCorsConfiguration(),
+            new EndpointLinksResolver(allEndpoints, basePath),
+            shouldRegisterLinksMapping, null);
+    }
+
+
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
+                                               String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
+            || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 }
