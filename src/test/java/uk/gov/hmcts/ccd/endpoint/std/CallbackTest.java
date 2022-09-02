4c5469001d56bd2f86fb2edbccec67df15016fad
JamesCollettCGI <james.collett@hmcts.net>
2022-09-02 11:03:38 +0100

    Remove EXPECTED_CALLBACK_DATA_CLASSIFICATION_STRING from CallbackTest.java

--- src/test/java/uk/gov/hmcts/ccd/endpoint/std/CallbackTest.java
+++ src/test/java/uk/gov/hmcts/ccd/endpoint/std/CallbackTest.java
@@ -114,19 +114,6 @@ public class CallbackTest extends WireMockBaseTest {
         "    }\n" +
         "  }";
 
-    private static final String EXPECTED_CALLBACK_DATA_CLASSIFICATION_STRING =
-        "{\n" +
-        "    \"PersonLastName\": \"PRIVATE\",\n" +
-        "    \"PersonAddress\": {\n" +
-        "      \"classification\" : \"PRIVATE\",\n" +
-        "      \"value\" : {\n" +
-        "        \"AddressLine1\": \"PRIVATE\",\n" +
-        "        \"AddressLine2\": \"PRIVATE\"\n" +
-        "      }\n" +
-        "    },\n" +
-        "    \"D8Document\": \"PRIVATE\"" +
-        "  }";
-
     private String modifiedDataString;
 
     private static JsonNode MODIFIED_DATA = null;
