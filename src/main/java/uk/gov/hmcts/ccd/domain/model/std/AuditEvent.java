ad61760f05114f16e26b7733eba672923db6e27b
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2022-09-16 13:33:34 +0100

    Remove unused import

--- src/main/java/uk/gov/hmcts/ccd/domain/model/std/AuditEvent.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/std/AuditEvent.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.ccd.domain.model.std;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
