e93009475937f738777a958cb947ec138950941b
Martin <martin.spasov@hmcts.net>
2022-10-04 16:26:46 +0100

    LAU-496 Fix null pointer exception for lau case refs.

--- src/main/java/uk/gov/hmcts/ccd/domain/model/lau/SearchLog.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/lau/SearchLog.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.ccd.domain.model.lau;
 
+import com.google.common.base.Splitter;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,10 +8,10 @@ import uk.gov.hmcts.ccd.auditlog.aop.AuditContext;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 @NoArgsConstructor
 @Setter
@@ -18,8 +19,6 @@ import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 public class SearchLog implements Serializable {
 
     public static final long serialVersionUID = 432973322;
-
-    private static final String ANY_SPACE_MATCH = "\\s*";
 
     private String userId;
     private List<String> caseRefs;
@@ -38,8 +37,9 @@ public class SearchLog implements Serializable {
     }
 
     public void setCaseRefs(String caseRefs) {
-        this.caseRefs = Arrays.asList(caseRefs.split(ANY_SPACE_MATCH + AuditContext.CASE_ID_SEPARATOR
-            + ANY_SPACE_MATCH));
+        if (!isEmpty(caseRefs)) {
+            this.caseRefs = Splitter.on(AuditContext.CASE_ID_SEPARATOR).trimResults().splitToList(caseRefs);
+        }
     }
 
 }
