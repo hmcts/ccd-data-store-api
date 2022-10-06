771f052146a1b08c3039f621f7adfc87a1f12cec
Martin <martin.spasov@hmcts.net>
2022-10-06 11:34:42 +0100

    Add SearchLog constructor test.

--- src/test/java/uk/gov/hmcts/ccd/domain/model/lau/SearchLogTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/model/lau/SearchLogTest.java
@@ -3,10 +3,12 @@ package uk.gov.hmcts.ccd.domain.model.lau;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 
 import static java.time.LocalDateTime.now;
 import static java.time.ZoneOffset.UTC;
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SearchLogTest {
@@ -21,6 +23,22 @@ class SearchLogTest {
         searchLog.setCaseRefs(caseRefs);
         searchLog.setUserId(userId);
         searchLog.setTimestamp(timestamp);
+
+        assertThat(searchLog.getUserId()).isEqualTo(userId);
+        assertThat(searchLog.getTimestamp()).isEqualTo(timestamp.format(ISO_INSTANT));
+        assertThat(searchLog.getCaseRefs().size()).isEqualTo(3);
+        assertThat(searchLog.getCaseRefs().get(0)).isEqualTo("123");
+        assertThat(searchLog.getCaseRefs().get(1)).isEqualTo("567");
+        assertThat(searchLog.getCaseRefs().get(2)).isEqualTo("789");
+    }
+
+    @Test
+    void shouldCreateSearchLogViaSearchLogConstructor() {
+        final ZonedDateTime timestamp = ZonedDateTime.of(now(), UTC);
+        final List<String> caseRefs = asList("123","567","789");
+        final String userId = "789";
+
+        final SearchLog searchLog = new SearchLog(userId, caseRefs, timestamp);
 
         assertThat(searchLog.getUserId()).isEqualTo(userId);
         assertThat(searchLog.getTimestamp()).isEqualTo(timestamp.format(ISO_INSTANT));
