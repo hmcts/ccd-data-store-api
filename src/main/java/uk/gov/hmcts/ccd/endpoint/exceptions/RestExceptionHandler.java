b962f01c9c108a8d9dace51a45e5938f70dfedcb
mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
2022-10-31 13:14:03 +0000

    CCD-3473 Handling UnknownHostException - getHttpStatus now returns BAD_GATWAY 502 ifUnknownHostException, test added (#2128)
    Co-authored-by: lucianotmarsilio <107107435+lucianotmarsilio@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/ccd/endpoint/exceptions/RestExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/exceptions/RestExceptionHandler.java
@@ -146,6 +146,10 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             return HttpStatus.GATEWAY_TIMEOUT;
         }
 
+        if (isUnknownHostException(causeOfException)) {
+            return HttpStatus.BAD_GATEWAY;
+        }
+
         return HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
@@ -182,6 +186,10 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         Throwable innerException = causeOfException.getCause();
         return innerException instanceof java.net.SocketTimeoutException
             && innerException.getMessage().contains("Read timed out");
+    }
+
+    private boolean isUnknownHostException(final Throwable causeOfException) {
+        return causeOfException instanceof java.net.UnknownHostException;
     }
 
 }
