38d0f4c1578ef102f561c5f525a55dff5b1f35b5
Mike Brown <michael.brown@hmcts.net>
2022-11-15 14:09:20 +0000

    whitespace added for checkstyle

--- src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/Document.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/Document.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Value
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-@ApiModel(value="this annotation and value property required to make swagger use this class")
+@ApiModel(value = "this annotation and value property required to make swagger use this class")
 public class Document {
     String documentURL;
     String documentFilename;
