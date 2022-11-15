7bdf2ea176a5bdea06fbf38fb4bd2fabdc5d307c
Mike Brown <michael.brown@hmcts.net>
2022-11-15 13:13:12 +0000

    attempt to use @ApiModel in Document class

--- src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/Document.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/Document.java
@@ -2,12 +2,14 @@ package uk.gov.hmcts.ccd.domain.model.casefileview;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiModel;
 import lombok.Value;
 
 import java.time.LocalDateTime;
 
 @Value
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@ApiModel(value="DifferentModel", description="Sample model for the documentation")
 public class Document {
     String documentURL;
     String documentFilename;
