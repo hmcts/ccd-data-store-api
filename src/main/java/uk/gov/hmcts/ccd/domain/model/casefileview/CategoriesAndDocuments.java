c9ec0db7f0290d83c4abb44fd8bd81114f8aabd7
Mike Brown <michael.brown@hmcts.net>
2022-11-15 13:48:56 +0000

    dummy theDocument and commented out code removed

--- src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/CategoriesAndDocuments.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/CategoriesAndDocuments.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.ccd.domain.model.casefileview;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Value;
 
 import java.util.List;
@@ -13,25 +12,4 @@ public class CategoriesAndDocuments {
     Integer caseVersion;
     List<Category> categories;
     List<Document> uncategorisedDocuments;
-    Document theDocument;
-
-    public CategoriesAndDocuments(Integer caseVersion,
-        List<Category> categories,
-        List<uk.gov.hmcts.ccd.domain.model.casefileview.Document> uncategorisedDocuments) {
-        this.caseVersion = caseVersion;
-        this.categories = categories;
-        this.uncategorisedDocuments = uncategorisedDocuments;
-        this.theDocument = null;
-    }
-
-//    @ApiModelProperty(dataType = "java.util.List<uk.gov.hmcts.ccd.domain.model.casefileview.Document>")
-    public List<Document> getUncategorisedDocuments() {
-        return this.uncategorisedDocuments;
-    }
-
-//    @ApiModelProperty(dataType = "uk.gov.hmcts.ccd.domain.model.casefileview.Document")
-    public Document getTheDocument() {
-        return this.theDocument;
-    }
-
 }
