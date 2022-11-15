04c64e725aa6438082e767c3d1da64726c97c9cd
Mike Brown <michael.brown@hmcts.net>
2022-11-15 10:34:22 +0000

    attempt to use @ApiModelProperty on getUncategorisedDocuments

--- src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/CategoriesAndDocuments.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/casefileview/CategoriesAndDocuments.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ccd.domain.model.casefileview;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Value;
 
 import java.util.List;
@@ -12,4 +13,9 @@ public class CategoriesAndDocuments {
     Integer caseVersion;
     List<Category> categories;
     List<uk.gov.hmcts.ccd.domain.model.casefileview.Document> uncategorisedDocuments;
+
+    @ApiModelProperty(dataType = "java.util.List<uk.gov.hmcts.ccd.domain.model.casefileview.Document>")
+    public List<Document> getUncategorisedDocuments() {
+        return this.uncategorisedDocuments;
+    }
 }
