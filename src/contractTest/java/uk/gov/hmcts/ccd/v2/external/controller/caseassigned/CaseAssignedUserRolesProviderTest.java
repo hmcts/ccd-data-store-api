fd3c8518418d6d53c47f70878749ab810cbbb15f
Mike Brown <michael.brown@hmcts.net>
2023-02-08 08:46:02 +0000

    publish results hard coding commented

--- src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/caseassigned/CaseAssignedUserRolesProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/caseassigned/CaseAssignedUserRolesProviderTest.java
@@ -59,7 +59,7 @@ public class CaseAssignedUserRolesProviderTest extends WireMockBaseTest {
 
     @BeforeEach
     void before(PactVerificationContext context) {
-        System.getProperties().setProperty("pact.verifier.publishResults", "true");
+        //System.getProperties().setProperty("pact.verifier.publishResults", "true");
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
         testTarget.setControllers(
             caseAssignedUserRolesController);
