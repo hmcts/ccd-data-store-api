e8fa18ca5b9f442216a9afbc61b93a05ad7098ff
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2023-08-31 14:42:58 +0100

    Update apps-insight.tf removed connection-string

--- infrastructure/apps-insight.tf
+++ infrastructure/apps-insight.tf
@@ -7,9 +7,3 @@ data "azurerm_key_vault" "key_vault" {
   name                = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-shared-${var.env}"
 }
-
-resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "app-insights-connection-string"
-  value        = data.azurerm_application_insights.ai.connection_string
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
