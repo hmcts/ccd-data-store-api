bdcac68fd7af08b531d677ac6c57649a10db5c9b
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2023-10-12 11:19:05 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -124,7 +124,7 @@ module "postgresql-v15" {
 
   admin_user_object_id = var.jenkins_AAD_objectId
   business_area        = "cft"
-  common_tags          = local.tags
+  common_tags          = var.common_tags
   component            = var.component
   env                  = var.env
   pgsql_databases = [
