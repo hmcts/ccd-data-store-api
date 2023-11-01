e4abe06b8481990e4c4e0db40d332c52ccfbfdf8
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2023-11-01 10:56:53 +0000

    Update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,3 +1,10 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
 terraform {
   backend "azurerm" {}
 
