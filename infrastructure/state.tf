69ed3f889705af6c9e67325f764e7b3c0fa7c6dc
Muhammad <muhammad.umerji@hmcts.net>
2023-11-08 15:46:33 +0000

    Updating providers in state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,13 +1,17 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
 terraform {
   backend "azurerm" {}
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source = "hashicorp/random"
+      version = "~> 3.76.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
