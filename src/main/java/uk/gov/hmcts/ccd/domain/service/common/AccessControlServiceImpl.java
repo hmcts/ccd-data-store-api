5cf24fbf9405b7bdb62a79130fc8879c579488e9
kerry-annemclaughlin <@Elvis2001>
2023-11-15 14:37:44 +0000

    CCD-4888: ???

--- src/main/java/uk/gov/hmcts/ccd/domain/service/common/AccessControlServiceImpl.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/AccessControlServiceImpl.java
@@ -154,7 +154,7 @@ public class AccessControlServiceImpl implements AccessControlService {
                     }
                 });
             if (!missingFields.isEmpty()) {
-                System.out.println("Missing Fields: " + missingFields);
+                LOG.debug("Missing Fields: " + missingFields);
             }
             return !noAccessGranted;
         }
