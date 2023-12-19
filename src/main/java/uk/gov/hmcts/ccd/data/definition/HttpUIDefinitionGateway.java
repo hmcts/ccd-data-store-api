913a9f7f81f5102ea4e8b349791d3f8b882d2313
aktaskaan <aktaskaan@gmail.com>
2023-12-19 15:26:56 +0000

    refactor HttpUIDefinitionGateway and create generic method to invoke definition requests

--- src/main/java/uk/gov/hmcts/ccd/data/definition/HttpUIDefinitionGateway.java
+++ src/main/java/uk/gov/hmcts/ccd/data/definition/HttpUIDefinitionGateway.java
@@ -1,15 +1,8 @@
 package uk.gov.hmcts.ccd.data.definition;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.hmcts.ccd.ApplicationParams;
-import uk.gov.hmcts.ccd.data.SecurityUtils;
 import uk.gov.hmcts.ccd.domain.model.definition.BannersResult;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseTypeTabsDefinition;
 import uk.gov.hmcts.ccd.domain.model.definition.JurisdictionUiConfigResult;
@@ -24,8 +17,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import java.net.URI;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.List;
 
 /**
@@ -36,177 +27,101 @@ import java.util.List;
 @Singleton
 public class HttpUIDefinitionGateway implements UIDefinitionGateway {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UIDefinitionRepository.class);
-
     private final ApplicationParams applicationParams;
-    private final SecurityUtils securityUtils;
-    @Qualifier("restTemplate")
-    @Autowired
-    private final RestTemplate restTemplate;
+    private final DefinitionStoreClient definitionStoreClient;
 
     @Inject
     HttpUIDefinitionGateway(final ApplicationParams applicationParams,
-                            final SecurityUtils securityUtils,
-                            final RestTemplate restTemplate) {
+                            final DefinitionStoreClient definitionStoreClient) {
         this.applicationParams = applicationParams;
-        this.securityUtils = securityUtils;
-        this.restTemplate = restTemplate;
+        this.definitionStoreClient = definitionStoreClient;
     }
 
     @Override
     public SearchResultDefinition getSearchResult(int version, String caseTypeId) {
         try {
-            final Instant start = Instant.now();
-            final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-            final SearchResultDefinition
-                    searchResult =
-                    restTemplate.exchange(
-                            withVersionQueryParam(applicationParams.displaySearchResultDefURL(caseTypeId), version),
-                            HttpMethod.GET,
-                            requestEntity,
-                            SearchResultDefinition.class).getBody();
-            final Duration duration = Duration.between(start, Instant.now());
-            LOG.debug("Rest API getSearchResultGetHttp called for {}, finished in {}",
-                    caseTypeId,
-                    duration.toMillis());
-            return searchResult;
+            return definitionStoreClient.invokeGetRequest(
+                    withVersionQueryParam(applicationParams.displaySearchResultDefURL(caseTypeId), version),
+                    SearchResultDefinition.class)
+                .getBody();
         } catch (final Exception e) {
             throw new ServiceException(String.format(
-                    "Problem getting SearchResultDefinition definition for case type: %s because of %s",
-                    caseTypeId,
-                    e.getMessage()), e);
+                "Problem getting SearchResultDefinition definition for case type: %s because of %s",
+                caseTypeId,
+                e.getMessage()), e);
         }
     }
 
     @Override
     public SearchInputFieldsDefinition getSearchInputFieldDefinitions(int version, String caseTypeId) {
         try {
-            final Instant start = Instant.now();
-            final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-            final SearchInputFieldsDefinition
-                    definition =
-                    restTemplate.exchange(
-                        withVersionQueryParam(applicationParams.searchInputDefinition(caseTypeId), version),
-                            HttpMethod.GET,
-                            requestEntity,
-                            SearchInputFieldsDefinition.class).getBody();
-            final Duration duration = Duration.between(start, Instant.now());
-            LOG.debug("Rest API getSearchInputDefinitionsGetHttp called for {}, finished in {}",
-                    caseTypeId,
-                    duration.toMillis());
-            return definition;
+            return definitionStoreClient.invokeGetRequest(
+                    withVersionQueryParam(applicationParams.searchInputDefinition(caseTypeId), version),
+                    SearchInputFieldsDefinition.class)
+                .getBody();
         } catch (final Exception e) {
             throw new ServiceException(String.format(
-                    "Problem getting SearchInputs definition for case type: %s because of %s",
-                    caseTypeId,
-                    e.getMessage()), e);
+                "Problem getting SearchInputs definition for case type: %s because of %s",
+                caseTypeId,
+                e.getMessage()), e);
         }
     }
 
     @Override
     public WorkbasketInputFieldsDefinition getWorkbasketInputFieldsDefinitions(int version, String caseTypeId) {
         try {
-            final Instant start = Instant.now();
-            final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-            final WorkbasketInputFieldsDefinition
-                    definition =
-                    restTemplate.exchange(
-                        withVersionQueryParam(applicationParams.workbasketInputDefinition(caseTypeId), version),
-                            HttpMethod.GET,
-                            requestEntity,
-                            WorkbasketInputFieldsDefinition.class).getBody();
-            final Duration duration = Duration.between(start, Instant.now());
-            LOG.debug("Rest API getWorkbasketInputDefinitionsGetHttp called for {}, finished in {}",
-                    caseTypeId,
-                    duration.toMillis());
-            return definition;
+            return definitionStoreClient.invokeGetRequest(
+                    withVersionQueryParam(applicationParams.workbasketInputDefinition(caseTypeId), version),
+                    WorkbasketInputFieldsDefinition.class)
+                .getBody();
         } catch (final Exception e) {
             throw new ServiceException(String.format(
-                    "Problem getting WorkbasketInputs definition for case type: %s because of %s",
-                    caseTypeId,
-                    e.getMessage()), e);
+                "Problem getting WorkbasketInputs definition for case type: %s because of %s",
+                caseTypeId,
+                e.getMessage()), e);
         }
     }
 
     @Override
     public CaseTypeTabsDefinition getCaseTypeTabsCollection(int version, String caseTypeId) {
-        final Instant start = Instant.now();
-        final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-        final CaseTypeTabsDefinition
-                collection =
-                restTemplate.exchange(
-                        withVersionQueryParam(applicationParams.displayCaseTabCollection(caseTypeId), version),
-                        HttpMethod.GET,
-                        requestEntity,
-                        CaseTypeTabsDefinition.class).getBody();
-        final Duration duration = Duration.between(start, Instant.now());
-        LOG.debug("Rest API getCaseTypeTabsCollection called for {}, finished in {}",
-                caseTypeId,
-                duration.toMillis());
-        return collection;
+        return definitionStoreClient.invokeGetRequest(
+                withVersionQueryParam(applicationParams.displayCaseTabCollection(caseTypeId), version),
+                CaseTypeTabsDefinition.class)
+            .getBody();
     }
 
     @Override
     public List<WizardPage> getWizardPageCollection(int version, String caseTypeId, String eventId) {
-        final Instant start = Instant.now();
-        final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-        final WizardPageCollection
-                wpc =
-                restTemplate.exchange(
-                    withVersionQueryParam(applicationParams.displayWizardPageCollection(caseTypeId, eventId), version),
-                        HttpMethod.GET,
-                        requestEntity,
-                        WizardPageCollection.class).getBody();
-        final Duration duration = Duration.between(start, Instant.now());
-        LOG.debug("Rest API getWizardPageCollectionGetHttp called for {}, finished in {}",
-                caseTypeId,
-                duration.toMillis());
-        return wpc == null ? null : wpc.getWizardPages();
+        final WizardPageCollection wizardPageCollection = definitionStoreClient.invokeGetRequest(
+                withVersionQueryParam(applicationParams.displayWizardPageCollection(caseTypeId, eventId), version),
+                WizardPageCollection.class)
+            .getBody();
+        return wizardPageCollection != null ? wizardPageCollection.getWizardPages() : null;
     }
 
     @Override
     public SearchResultDefinition getWorkBasketResult(int version, String caseTypeId) {
         try {
-            final Instant start = Instant.now();
-            final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-            final SearchResultDefinition
-                    searchResult =
-                    restTemplate.exchange(
-                            withVersionQueryParam(applicationParams.displayWorkbasketDefURL(caseTypeId), version),
-                            HttpMethod.GET,
-                            requestEntity,
-                            SearchResultDefinition.class).getBody();
-            final Duration duration = Duration.between(start, Instant.now());
-            LOG.debug("Rest API getWorkBasketResultGetHttp called for {}, finished in {}",
-                    caseTypeId,
-                    duration.toMillis());
-            return searchResult;
+            return definitionStoreClient.invokeGetRequest(
+                    withVersionQueryParam(applicationParams.displayWorkbasketDefURL(caseTypeId), version),
+                    SearchResultDefinition.class)
+                    .getBody();
         } catch (final Exception e) {
             throw new ServiceException(String.format(
-                    "Problem getting WorkBasketResult definition for case type: %s because of %s",
-                    caseTypeId,
-                    e.getMessage()), e);
+                "Problem getting WorkBasketResult definition for case type: %s because of %s",
+                caseTypeId,
+                e.getMessage()), e);
         }
     }
 
     @Override
     public SearchResultDefinition getSearchCasesResultDefinition(int version, String caseTypeId, String useCase) {
         try {
-            Instant start = Instant.now();
-            HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-            SearchResultDefinition
-                searchResult =
-                restTemplate.exchange(
+            return definitionStoreClient.invokeGetRequest(
                     withVersionQueryParam(applicationParams.displaySearchCasesResultDefURL(caseTypeId, useCase),
                         version),
-                    HttpMethod.GET,
-                    requestEntity,
-                    SearchResultDefinition.class).getBody();
-            Duration duration = Duration.between(start, Instant.now());
-            LOG.debug("Rest API getSearchCasesResultGetHttp called for {}, finished in {}",
-                caseTypeId,
-                duration.toMillis());
-            return searchResult;
+                    SearchResultDefinition.class)
+                    .getBody();
         } catch (Exception e) {
             throw new ServiceException(String.format(
                 "Problem getting SearchCasesResult definition for case type: %s because of %s",
@@ -220,27 +135,12 @@ public class HttpUIDefinitionGateway implements UIDefinitionGateway {
         return builder.build().encode().toUri();
     }
 
-    private URI withJurisdictionIds(String url, final List<String> jurisdictionIds) {
-        UriComponentsBuilder builder =
-            UriComponentsBuilder.fromHttpUrl(url).queryParam("ids", String.join(",", jurisdictionIds));
-        return builder.build().encode().toUri();
-    }
-
     public BannersResult getBanners(final List<String> jurisdictionIds) {
         try {
-            final Instant start = Instant.now();
-            final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-            final BannersResult
-                bannersResult =
-                restTemplate.exchange(withJurisdictionIds(applicationParams.bannersURL(), jurisdictionIds),
-                    HttpMethod.GET,
-                    requestEntity,
-                    BannersResult.class).getBody();
-            final Duration duration = Duration.between(start, Instant.now());
-            LOG.debug("Rest API getBanners called for {}, finished in {}",
-                jurisdictionIds,
-                duration.toMillis());
-            return bannersResult;
+            return definitionStoreClient.invokeGetRequest(
+                withJurisdictionIds(applicationParams.bannersURL(), jurisdictionIds),
+                    BannersResult.class)
+                .getBody();
         } catch (final Exception e) {
             throw new ServiceException(String.format(
                 "Problem getting banners for jurisdiction references: %s because of %s",
@@ -251,25 +151,21 @@ public class HttpUIDefinitionGateway implements UIDefinitionGateway {
 
     public JurisdictionUiConfigResult getJurisdictionUiConfigs(final List<String> jurisdictionIds) {
         try {
-            final Instant start = Instant.now();
-            final HttpEntity requestEntity = new HttpEntity(securityUtils.authorizationHeaders());
-            final JurisdictionUiConfigResult
-                jurisdictionUiConfigResult =
-                restTemplate.exchange(
+            return definitionStoreClient.invokeGetRequest(
                     withJurisdictionIds(applicationParams.jurisdictionUiConfigsURL(), jurisdictionIds),
-                    HttpMethod.GET,
-                    requestEntity,
-                    JurisdictionUiConfigResult.class).getBody();
-            final Duration duration = Duration.between(start, Instant.now());
-            LOG.debug("Rest API getJurisdictionUiConfigs called for {}, finished in {}",
-                jurisdictionIds,
-                duration.toMillis());
-            return jurisdictionUiConfigResult;
+                    JurisdictionUiConfigResult.class)
+                    .getBody();
         } catch (final Exception e) {
             throw new ServiceException(String.format(
                 "Problem getting jurisdiction UI configs for jurisdiction references: %s because of %s",
                 jurisdictionIds,
                 e.getMessage()), e);
         }
+    }
+
+    private URI withJurisdictionIds(String url, final List<String> jurisdictionIds) {
+        UriComponentsBuilder builder =
+            UriComponentsBuilder.fromHttpUrl(url).queryParam("ids", String.join(",", jurisdictionIds));
+        return builder.build().encode().toUri();
     }
 }
