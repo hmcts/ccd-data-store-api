54c97ed1fbbf6ee0e53b93e2314a74533002f9f4
JamesCollettCGI <james.collett@hmcts.net>
2024-02-21 12:14:01 +0000

    Add CallbackService temporary logging

--- src/main/java/uk/gov/hmcts/ccd/domain/service/callbacks/CallbackService.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/callbacks/CallbackService.java
@@ -58,6 +58,12 @@ public class CallbackService {
         this.appinsights = appinsights;
     }
 
+    private void jcdebug(String msg) {
+        LOG.debug("JCDEBUG.debug: CallbackService: {}", msg);
+        LOG.warn("JCDEBUG.warn:  CallbackService: {}", msg);
+        LOG.info("JCDEBUG.info:  CallbackService: {}", msg);
+    }
+
     // The retry will be on seconds T=1 and T=3 if the initial call fails at T=0
     @Retryable(value = {CallbackException.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000, multiplier = 3))
     public Optional<CallbackResponse> send(final String url,
@@ -66,7 +72,7 @@ public class CallbackService {
                                            final CaseDetails caseDetailsBefore,
                                            final CaseDetails caseDetails,
                                            final Boolean ignoreWarning) {
-
+        jcdebug("send1");
         return sendSingleRequest(url, callbackType, caseEvent, caseDetailsBefore, caseDetails, ignoreWarning);
     }
 
@@ -78,6 +84,7 @@ public class CallbackService {
                                       final CaseDetails caseDetailsBefore,
                                       final CaseDetails caseDetails,
                                       final Class<T> clazz) {
+        jcdebug("send2");
         return sendSingleRequest(url, callbackType, caseEvent, caseDetailsBefore, caseDetails, clazz);
     }
 
@@ -87,6 +94,7 @@ public class CallbackService {
                                                         final CaseDetails caseDetailsBefore,
                                                         final CaseDetails caseDetails,
                                                         final Boolean ignoreWarning) {
+        jcdebug("sendSingleRequest1");
         if (url == null || url.isEmpty()) {
             return Optional.empty();
         }
@@ -94,21 +102,6 @@ public class CallbackService {
             new CallbackRequest(caseDetails, caseDetailsBefore, caseEvent.getId(), ignoreWarning);
         final Optional<ResponseEntity<CallbackResponse>> responseEntity =
             sendRequest(url, callbackType, CallbackResponse.class, callbackRequest);
-        try {
-            LOG.debug("JCDEBUG.debug: CallbackService.sendSingleRequest: URL = {} , TYPE = {} , EVENT = {} , "
-                + "DETAILSBEFORE = {} , DETAILS = {}", url, callbackType, caseEvent, caseDetailsBefore, caseDetails);
-            LOG.info("JCDEBUG.info: CallbackService.sendSingleRequest: URL = {} , TYPE = {} , EVENT = {} , "
-                + "DETAILSBEFORE = {} , DETAILS = {}", url, callbackType, caseEvent, caseDetailsBefore, caseDetails);
-            LOG.warn("JCDEBUG.warn: CallbackService.sendSingleRequest: URL = {} , TYPE = {} , EVENT = {} , "
-                + "DETAILSBEFORE = {} , DETAILS = {}", url, callbackType, caseEvent, caseDetailsBefore, caseDetails);
-        } catch (Exception e) {
-            LOG.debug("JCDEBUG.debug: CallbackService.sendSingleRequest: Exception creating log: {} {}",
-                e.getMessage(), e.toString());
-            LOG.info("JCDEBUG.info: CallbackService.sendSingleRequest: Exception creating log: {} {}",
-                e.getMessage(), e.toString());
-            LOG.warn("JCDEBUG.warn: CallbackService.sendSingleRequest: Exception creating log: {} {}",
-                e.getMessage(), e.toString());
-        }
         return responseEntity.map(re -> Optional.of(re.getBody())).orElseThrow(() -> {
             LOG.warn("Unsuccessful callback to {} for caseType {} and event {}", url, caseDetails.getCaseTypeId(),
                 caseEvent.getId());
@@ -125,6 +118,7 @@ public class CallbackService {
                                                    final CaseDetails caseDetailsBefore,
                                                    final CaseDetails caseDetails,
                                                    final Class<T> clazz) {
+        jcdebug("sendSingleRequest2");
         final CallbackRequest callbackRequest = new CallbackRequest(caseDetails, caseDetailsBefore, caseEvent.getId());
         final Optional<ResponseEntity<T>> requestEntity = sendRequest(url, callbackType, clazz, callbackRequest);
         return requestEntity.orElseThrow(() -> {
@@ -139,7 +133,7 @@ public class CallbackService {
                                                         final Class<T> clazz,
 
                                                         final CallbackRequest callbackRequest) {
-
+        jcdebug("sendRequest");
         HttpHeaders securityHeaders = securityUtils.authorizationHeaders();
 
         CallbackTelemetryThreadContext.setTelemetryContext(new CallbackTelemetryContext(callbackType));
@@ -178,6 +172,7 @@ public class CallbackService {
 
     public void validateCallbackErrorsAndWarnings(final CallbackResponse callbackResponse,
                                                   final Boolean ignoreWarning) {
+        jcdebug("validateCallbackErrorsAndWarnings");
         if (!isEmpty(callbackResponse.getErrors())
             || (!isEmpty(callbackResponse.getWarnings()) && (ignoreWarning == null || !ignoreWarning))) {
             throw new ApiException("Unable to proceed because there are one or more callback Errors or Warnings")
@@ -187,9 +182,13 @@ public class CallbackService {
     }
 
     private boolean logCallbackDetails(final String url) {
+        jcdebug("logCallbackDetails");
+        return true;
+        /*
         return (applicationParams.getCcdCallbackLogControl().size() > 0
             && (WILDCARD.equals(applicationParams.getCcdCallbackLogControl().get(0))
             || applicationParams.getCcdCallbackLogControl().stream()
             .filter(Objects::nonNull).filter(Predicate.not(String::isEmpty)).anyMatch(url::contains)));
+        */
     }
 }
