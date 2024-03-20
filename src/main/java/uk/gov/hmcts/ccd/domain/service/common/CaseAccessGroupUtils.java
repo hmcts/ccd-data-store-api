749dbbecd1cf7020707ce1e813d112b71c7e3e24
patelila <ila.patel@hmcts.net>
2024-03-20 08:23:12 +0000

    fixing classification to use fundtion getDefaultSecurityClassifications from CaseDataService

--- src/main/java/uk/gov/hmcts/ccd/domain/service/common/CaseAccessGroupUtils.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/CaseAccessGroupUtils.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.ccd.domain.service.common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
