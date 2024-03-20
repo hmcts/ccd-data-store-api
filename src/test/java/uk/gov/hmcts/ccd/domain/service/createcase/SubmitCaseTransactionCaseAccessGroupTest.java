f3de035dec4d724214e3b2a11ec46e7c59aa47be
patelila <ila.patel@hmcts.net>
2024-03-20 14:38:15 +0000

    Review comments.

--- src/test/java/uk/gov/hmcts/ccd/domain/service/createcase/SubmitCaseTransactionCaseAccessGroupTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/service/createcase/SubmitCaseTransactionCaseAccessGroupTest.java
@@ -683,8 +683,6 @@ class SubmitCaseTransactionCaseAccessGroupTest {
 
         inputCaseDetails.setData(dataMap);
 
-        //Map<String, JsonNode> dataOrganisation = Collections.singletonMap("Organisation.OrganisationID",
-        //    new TextNode("550e8400-e29b-41d4-a716-446655440000"));
         Map<String, JsonNode> dataOrganisation = organisationPolicyCaseData("caseAssignedField",
             "\"550e8400-e29b-41d4-a716-446655440000\"");
 
