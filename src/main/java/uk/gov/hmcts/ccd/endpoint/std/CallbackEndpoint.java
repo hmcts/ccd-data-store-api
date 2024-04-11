5e7cc433493184e041f07f6c24d5cd06fb60e172
JamesCollettCGI <james.collett@hmcts.net>
2024-04-11 18:00:27 +0100

    Update CallbackEndpoint.java

--- src/main/java/uk/gov/hmcts/ccd/endpoint/std/CallbackEndpoint.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/std/CallbackEndpoint.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
