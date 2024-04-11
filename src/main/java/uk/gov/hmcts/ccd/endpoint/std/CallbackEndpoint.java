a66063b75996f4558d7e6a802fce16b2e25be269
JamesCollettCGI <james.collett@hmcts.net>
2024-04-11 17:54:57 +0100

    Uodate CallbackEndpoint.java

--- src/main/java/uk/gov/hmcts/ccd/endpoint/std/CallbackEndpoint.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/std/CallbackEndpoint.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
 import uk.gov.hmcts.ccd.domain.model.definition.Document;
