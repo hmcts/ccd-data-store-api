dbb0e0d778807629711ebc5bbf4db7e4f89dc581
JamesCollettCGI <james.collett@hmcts.net>
2024-04-12 17:45:33 +0100

    Fix checkstyle

--- src/main/java/uk/gov/hmcts/ccd/endpoint/std/TestController.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/std/TestController.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.ccd.endpoint.std;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
