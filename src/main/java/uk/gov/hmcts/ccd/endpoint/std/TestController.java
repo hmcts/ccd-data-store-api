82e91889c32f6b93367c2f6ce500e3c72da65b65
JamesCollettCGI <james.collett@hmcts.net>
2024-04-12 22:42:17 +0100

    Updates

--- src/main/java/uk/gov/hmcts/ccd/endpoint/std/TestController.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/std/TestController.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.ccd.endpoint.std;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +16,11 @@ public class TestController {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
-    /**
-     * Use this method to POST and log messages.
+    /*
+     * Method to POST and log messages.
+     *
+     * Tested using curl command below :-
+     * curl -X POST https://ccd-data-store-api-pr-2356.preview.platform.hmcts.net/jcdebug -d "TESTING WORKS"
      */
     @PostMapping("/jcdebug")
     public ResponseEntity<String> postMessage(@RequestBody String message) {
@@ -28,11 +34,37 @@ public class TestController {
         return ResponseEntity.ok("Message: " + (message == null ? "NULL" : message));
     }
 
-    /**
+    /*
      * Use this method to test message logging.
      */
     @GetMapping("/jcdebugtest")
     public String jcdebugtest() {
-        return "jcdebugtest";
+        String rc;
+
+        try {
+            final String url = "https://example.com/api";
+            final String payload = "TESTING POST";
+
+            URL apiUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            // Set content type
+            connection.setRequestProperty("Content-Type", "text/plain");
+
+            // Write the string payload to the HTTP request body
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(payload.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+            rc = "Response Code: " + connection.getResponseCode();
+        } catch (Exception e) {
+            rc = "EXCEPTION";
+            e.printStackTrace();
+        }
+        return "jcdebugtest: " + rc;
     }
 }
