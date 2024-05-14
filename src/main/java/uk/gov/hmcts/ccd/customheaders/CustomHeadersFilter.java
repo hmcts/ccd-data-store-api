54dafe1af28a5ee7984f98776733fe2f4e83b3c7
markdathorne <mark.dathorne@cgi.com>
2024-05-14 22:37:36 +0100

    CCD-5367 try webfilter config

--- src/main/java/uk/gov/hmcts/ccd/customheaders/CustomHeadersFilter.java
+++ src/main/java/uk/gov/hmcts/ccd/customheaders/CustomHeadersFilter.java
@@ -10,10 +10,12 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebFilter("/**")
 public class CustomHeadersFilter implements Filter {
 
     private static final Logger LOG = LoggerFactory.getLogger(CustomHeadersFilter.class);
