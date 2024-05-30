f888ea925db276df2044c75248888df3228cf027
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2024-05-30 10:59:17 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,14 +79,6 @@ module "postgresql_v15" {
   # Setup Access Reader db user
   force_user_permissions_trigger = "2"
 
-  # Sets correct DB owner after migration to fix permissions
-  enable_schema_ownership = var.enable_schema_ownership
-  force_schema_ownership_trigger = "2"
-  kv_subscription = var.kv_subscription
-  kv_name = data.azurerm_key_vault.ccd_shared_key_vault.name
-  user_secret_name = azurerm_key_vault_secret.POSTGRES-USER.name
-  pass_secret_name = azurerm_key_vault_secret.POSTGRES-PASS.name
-
   pgsql_databases = [
     {
       name = var.database_name
