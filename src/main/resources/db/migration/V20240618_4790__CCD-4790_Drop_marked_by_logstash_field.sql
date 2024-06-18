c72291a3ae0c99b97d5cebd50b4fc35edd454901
patelila <ila.patel@hmcts.net>
2024-06-18 12:48:49 +0100

    CCD-4790 Drop marked_by_logstash field in the case_data table

---
+++ src/main/resources/db/migration/V20240618_4790__CCD-4790_Drop_marked_by_logstash_field.sql
@@ -0,0 +1,12 @@
+-- drop INDEX idx_case_data_marked_by_logstash
+-- remove column marked_by_logstash from case_data
+-- as now replaced with case_data_logstash_queue table
+
+DROP INDEX IF exists idx_case_data_marked_by_logstash;
+ALTER table case_data DROP COLUMN marked_by_logstash
+
+
+
+
+
+
