11312cd34b2db5685af2adff6d3001a8d3722d4d
patelila <ila.patel@hmcts.net>
2024-06-18 12:37:08 +0100

    CCD-4775 Modify db trigger to insert the case id's into new table

--- src/main/resources/db/migration/V20240617_4775__CCD-4775_modify_db_trigger_for_IU.sql
+++ src/main/resources/db/migration/V20240617_4775__CCD-4775_modify_db_trigger_for_IU.sql
@@ -1,15 +1,39 @@
-CREATE OR REPLACE FUNCTION insert_logstash_queue ()
-RETURNS TRIGGER AS $$
-BEGIN
-    INSERT INTO public.case_data_logstash_queue ("case_data_id") VALUES (NEW.id);
-    RETURN NEW;
-END;
+--
+-- drop old set_case_data_marked_by_logstash
+-- Name: insert_update_logstash_queue(); Type: FUNCTION; Schema: public; Owner: ccd
 
-$$ LANGUAGE plpgsql;-- drop trigger from case_data and add new one
+DROP TRIGGER IF EXISTS trg_case_data_updated ON public.case_data;
+
+DROP FUNCTION IF exists set_case_data_marked_by_logstash;
+
+DROP FUNCTION IF exists insert_update_logstash_queue;
+
+CREATE OR REPLACE FUNCTION public.insert_update_logstash_queue () RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+        BEGIN
+            IF (
+            	SELECT id FROM case_data_logstash_queue WHERE case_data_id = NEW.id
+            	) <> NEW.id THEN
+                INSERT INTO public.case_data_logstash_queue ("case_data_id") VALUES (NEW.id);
+            END IF;
+            RETURN NEW;
+        END;
+        $$;
+
 DROP TRIGGER IF EXISTS trg_case_data_updated ON public.case_data;
 
 CREATE TRIGGER trg_case_data_updated
 BEFORE INSERT OR UPDATE OF data, data_classification, last_modified, last_state_modified_date, security_classification, state, supplementary_data
 ON public.case_data
-FOR EACH ROW EXECUTE PROCEDURE public.insert_logstash_queue();
+FOR EACH ROW EXECUTE PROCEDURE public.insert_update_logstash_queue();
+
+
+-- drop INDEX idx_case_data_marked_by_logstash
+-- remove column marked_by_logstash from case_data
+-- as now replaced with case_data_logstash_queue table
+
+DROP INDEX IF exists idx_case_data_marked_by_logstash;
+ALTER table case_data DROP COLUMN marked_by_logstash
+
 
