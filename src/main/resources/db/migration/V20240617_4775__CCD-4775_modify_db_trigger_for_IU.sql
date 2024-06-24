1c7618b805e0a5acf52dbb9c9d5724e024cada8e
patelila <ila.patel@hmcts.net>
2024-06-24 17:16:06 +0100

    Updates to migration script.

--- src/main/resources/db/migration/V20240617_4775__CCD-4775_modify_db_trigger_for_IU.sql
+++ src/main/resources/db/migration/V20240617_4775__CCD-4775_modify_db_trigger_for_IU.sql
@@ -11,22 +11,18 @@ DROP FUNCTION IF exists insert_update_logstash_queue;
 CREATE OR REPLACE FUNCTION public.insert_update_logstash_queue () RETURNS TRIGGER
     LANGUAGE plpgsql
     AS $$
-        BEGIN
-            IF (
-            	SELECT id FROM case_data_logstash_queue WHERE case_data_id = NEW.id
-            	) <> NEW.id THEN
-                INSERT INTO public.case_data_logstash_queue ("case_data_id") VALUES (NEW.id);
-            END IF;
-            RETURN NEW;
-        END;
-        $$;
+    BEGIN
+        INSERT INTO public.case_data_logstash_queue ("case_data_id") VALUES (NEW.id);
+        RETURN NEW;
+    END;
+    $$;
 
 DROP TRIGGER IF EXISTS trg_case_data_updated ON public.case_data;
 
 CREATE TRIGGER trg_case_data_updated
-BEFORE INSERT OR UPDATE OF data, data_classification, last_modified, last_state_modified_date, security_classification, state, supplementary_data
+AFTER INSERT OR UPDATE OF data, data_classification, last_modified, last_state_modified_date, security_classification, state, supplementary_data
 ON public.case_data
-FOR EACH ROW EXECUTE PROCEDURE public.insert_update_logstash_queue();
+FOR EACH ROW EXECUTE FUNCTION public.insert_update_logstash_queue();
 
 
 
