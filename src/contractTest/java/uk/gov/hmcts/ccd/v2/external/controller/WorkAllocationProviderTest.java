6d7822b65c838493e206bd235144ef6896e97529
JamesCollettCGI <james.collett@hmcts.net>
2024-08-05 20:39:52 +0100

    Updated 'WorkAllocationProviderTest.java' -- Added SpringBootTest , ActiveProfiles , WireMockBaseTest

--- src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/WorkAllocationProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/WorkAllocationProviderTest.java
@@ -13,10 +13,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.ccd.WireMockBaseTest;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
 import uk.gov.hmcts.ccd.domain.model.std.CaseDataContent;
 
@@ -25,11 +28,22 @@ import java.util.Map;
 @ExtendWith(SpringExtension.class)
 @Provider("ccdDataStoreAPI_WorkAllocation")
 @PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
-    host = "${PACT_BROKER_URL:localhost}", port = "${PACT_BROKER_PORT:9292}", consumerVersionSelectors = {
+    host = "${PACT_BROKER_URL:localhost}",
+    port = "${PACT_BROKER_PORT:80}", consumerVersionSelectors = {
         @VersionSelector(tag = "master")})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
+    "server.port=8123", "spring.application.name=PACT_TEST",
+    "ccd.document.url.pattern=${CCD_DOCUMENT_URL_PATTERN:https?://(((?:api-gateway.preprod.dm.reform.hmcts.net|"
+        + "(dm-store-aat.service.core-compute-aat|dm-store-(pr-[0-9]+|preview).service.core-compute-preview)."
+        + "internal(?::d+)?)/documents/[A-Za-z0-9-]+(?:/binary)?)|((em-hrs-api-aat.service.core-compute-aat|"
+        + "em-hrs-api-(pr-[0-9]+|preview).service.core-compute-preview).internal(?::d+)?/hearing-recordings/"
+        + "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/segments/[0-9]+))}"
+
+})
 @TestPropertySource(locations = "/application.properties")
+@ActiveProfiles("SECURITY_MOCK")
 @IgnoreNoPactsToVerify
-public class WorkAllocationProviderTest {
+public class WorkAllocationProviderTest extends WireMockBaseTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(WorkAllocationProviderTest.class);
 
@@ -87,6 +101,12 @@ public class WorkAllocationProviderTest {
         CaseDetails caseDetails = setUpCaseDetailsFromStateMapForEvent(dataMap);
         startEventOperation.setCaseReferenceOverride((String) dataMap.get(EVENT_ID),
             caseDetails.getReferenceAsString());
+    }
+
+    @State({"A Start for a Caseworker is requested"})
+    public void toStartForACaseworker(Map<String, Object> dataMap) {
+        jcLog("A Start for a Caseworker is requested");
+        setUpSecurityContextForEvent(dataMap);
     }
 
     private CaseDetails setUpCaseDetailsFromStateMapForEvent(Map<String, Object> dataMap) {
