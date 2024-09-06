e53e2e75f0702514874a3f330111fa9e15816d09
JamesCollettCGI <james.collett@hmcts.net>
2024-09-06 23:13:51 +0100

    Add GetCaseProviderTest , StartEventProviderTest , StartProviderTest , SubmitEventProviderTest , SubmitProviderTest

--- src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/WorkAllocationPacts1ProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/WorkAllocationPacts1ProviderTest.java
@@ -1,19 +1,19 @@
 package uk.gov.hmcts.ccd.v2.external.controller;
 
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
-import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
-import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,15 +22,20 @@ import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.ccd.WireMockBaseTest;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
 import uk.gov.hmcts.ccd.domain.model.std.CaseDataContent;
+import uk.gov.hmcts.ccd.domain.types.BaseType;
+import uk.gov.hmcts.ccd.infrastructure.user.UserAuthorisation;
 
 import java.util.Map;
 
+import static org.mockito.Mockito.when;
+
 @ExtendWith(SpringExtension.class)
 @Provider("ccdDataStoreAPI_WorkAllocation")
-@PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
-    host = "${PACT_BROKER_URL:localhost}",
-    port = "${PACT_BROKER_PORT:9292}", consumerVersionSelectors = {
-        @VersionSelector(tag = "master")})
+//@PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
+//    host = "${PACT_BROKER_URL:localhost}",
+//    port = "${PACT_BROKER_PORT:9292}", consumerVersionSelectors = {
+//        @VersionSelector(tag = "master")})
+@PactFolder("target/pacts1")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
     "server.port=8124", "spring.application.name=PACT_TEST",
     "ccd.document.url.pattern=${CCD_DOCUMENT_URL_PATTERN:https?://(((?:api-gateway.preprod.dm.reform.hmcts.net|"
@@ -50,8 +55,13 @@ public class WorkAllocationProviderTest extends WireMockBaseTest {
     private static final String CASEWORKER_USERNAME = "caseworkerUsername";
     private static final String CASEWORKER_PASSWORD = "caseworkerPassword";
     private static final String CASE_DATA_CONTENT = "caseDataContent";
-    public static final String EVENT_ID = "eventId";
     private static final String CASE_TYPE = "caseType";
+
+    @Autowired
+    ContractTestCaseDefinitionRepository contractTestCaseDefinitionRepository;
+
+    @MockBean
+    UserAuthorisation userAuthorisation;
 
     @Autowired
     ContractTestSecurityUtils securityUtils;
@@ -72,11 +82,12 @@ public class WorkAllocationProviderTest extends WireMockBaseTest {
     ObjectMapper objectMapper;
 
     private void jcLog(String message) {
+        System.out.println("JCDEBUG: WorkAllocationProviderTest: " + message);
         LOG.info("JCDEBUG: WorkAllocationProviderTest: " + message);
     }
 
     @TestTemplate
-    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    @ExtendWith(PactVerificationSpringProvider.class)
     void pactVerificationTestTemplate(PactVerificationContext context) {
         jcLog("pactVerificationTestTemplate() ->");
         if (context != null) {
@@ -87,77 +98,46 @@ public class WorkAllocationProviderTest extends WireMockBaseTest {
 
     @BeforeEach
     void before(PactVerificationContext context) {
-        jcLog("before() ->");
-        MockMvcTestTarget testTarget = new MockMvcTestTarget();
-        //System.getProperties().setProperty("pact.verifier.publishResults", "true");
+        jcLog("build() ->");
         if (context != null) {
-            context.setTarget(testTarget);
+            context.setTarget(new HttpTestTarget("localhost", 8123, "/"));
         }
-        jcLog("before() <-");
+        BaseType.setCaseDefinitionRepository(contractTestCaseDefinitionRepository);
+        //System.getProperties().setProperty("pact.verifier.publishResults", "true");
+        when(userAuthorisation.getAccessLevel()).thenReturn(UserAuthorisation.AccessLevel.ALL);
+        when(userAuthorisation.getUserId()).thenReturn("userId");
+        jcLog("build() <-");
     }
 
-    @State({"A case exists"})
+    @State({"a case exists"})
     public void toGetACase(Map<String, Object> dataMap) {
-        jcLog("A Get Case is requested");
+        jcLog("toGetACase() ->");
+        jcLog("toGetACase(): dataMap.size: " + dataMap.size());
         CaseDetails caseDetails = setUpCaseDetailsFromStateMap(dataMap);
         getCaseOperation.setTestCaseReference(caseDetails.getReferenceAsString());
-    }
-
-    @State({"A Start Event for a Caseworker is  requested"})
-    public void toStartEventForACaseworker(Map<String, Object> dataMap) {
-        jcLog("A Start Event for a Caseworker is  requested");
-        CaseDetails caseDetails = setUpCaseDetailsFromStateMapForEvent(dataMap);
-        startEventOperation.setCaseReferenceOverride((String) dataMap.get(EVENT_ID),
-            caseDetails.getReferenceAsString());
-    }
-
-    @State({"A Start for a Caseworker is requested"})
-    public void toStartForACaseworker(Map<String, Object> dataMap) {
-        jcLog("A Start for a Caseworker is requested");
-        setUpSecurityContextForEvent(dataMap);
-    }
-
-    @State({"A Submit Event for a Caseworker is requested"})
-    public void toSubmitEventForACaseworker(Map<String, Object> dataMap) {
-        jcLog("A Submit Event for a Caseworker is requested");
-        CaseDetails caseDetails = setUpCaseDetailsFromStateMapForEvent(dataMap);
-        createEventOperation.setTestCaseReference(caseDetails.getReferenceAsString());
-    }
-
-    @State({"A Submit for a Caseworker is requested"})
-    public void toSubmitForACaseworker(Map<String, Object> dataMap) {
-        jcLog("A Submit for a Caseworker is requested");
-        setUpSecurityContextForEvent(dataMap);
+        jcLog("toGetACase() <-");
     }
 
     private CaseDetails setUpCaseDetailsFromStateMap(Map<String, Object> dataMap) {
+        jcLog("setUpCaseDetailsFromStateMap() ->");
+        jcLog("setUpCaseDetailsFromStateMap(): dataMap.size: " + dataMap.size());
         Map<String, Object> contentDataMap = (Map<String, Object>) dataMap.get(CASE_DATA_CONTENT);
+        jcLog("setUpCaseDetailsFromStateMap(): contentDataMap = "
+            + (contentDataMap == null ? "NULL" : contentDataMap.toString()));
+
         String caseworkerUsername = (String) dataMap.get(CASEWORKER_USERNAME);
         String caseworkerPassword = (String) dataMap.get(CASEWORKER_PASSWORD);
         String caseType = (String) dataMap.get(CASE_TYPE);
+
         CaseDataContent caseDataContent = objectMapper.convertValue(contentDataMap, CaseDataContent.class);
+        jcLog("setUpCaseDetailsFromStateMap(): caseDataContent = "
+            + (caseDataContent == null ? "NULL" : caseDataContent.toString()));
 
         securityUtils.setSecurityContextUserAsCaseworkerByCaseType(caseType, caseworkerUsername,
             caseworkerPassword);
         securityUtils.setSecurityContextUserAsCaseworkerByEvent(caseDataContent.getEventId(), caseworkerUsername,
             caseworkerPassword);
+        jcLog("setUpCaseDetailsFromStateMap() <-");
         return contractTestCreateCaseOperation.createCaseDetails(caseType, caseDataContent, true);
-    }
-
-    private CaseDetails setUpCaseDetailsFromStateMapForEvent(Map<String, Object> dataMap) {
-        Map<String, Object> contentDataMap = (Map<String, Object>) dataMap.get(CASE_DATA_CONTENT);
-        setUpSecurityContextForEvent(dataMap);
-        String caseType = (String) dataMap.get(CASE_TYPE);
-        CaseDataContent caseDataContent = objectMapper.convertValue(contentDataMap, CaseDataContent.class);
-        return contractTestCreateCaseOperation.createCaseDetails(caseType, caseDataContent, true);
-    }
-
-    private void setUpSecurityContextForEvent(Map<String, Object> dataMap) {
-        String caseworkerUsername = (String) dataMap.get(CASEWORKER_USERNAME);
-        String caseworkerPassword = (String) dataMap.get(CASEWORKER_PASSWORD);
-        String eventId = (String) dataMap.get(EVENT_ID);
-        String caseTypeId = (String) dataMap.get(CASE_TYPE);
-        securityUtils.setSecurityContextUserAsCaseworkerByEvent(eventId, caseworkerUsername, caseworkerPassword);
-        securityUtils.setSecurityContextUserAsCaseworkerByCaseType(caseTypeId, caseworkerUsername, caseworkerPassword);
     }
 }
