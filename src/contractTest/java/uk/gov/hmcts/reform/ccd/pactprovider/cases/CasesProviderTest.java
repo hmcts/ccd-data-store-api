891cf2e6c9a51b6a5524f650c3cf79736c386961
JamesCollettCGI <james.collett@hmcts.net>
2024-09-27 18:26:35 +0100

    Clean Employment Tribunals Pact Testing pipeline -- Remove WA Pact Tests

--- src/contractTest/java/uk/gov/hmcts/reform/ccd/pactprovider/cases/CasesProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/ccd/pactprovider/cases/CasesProviderTest.java
@@ -41,7 +41,7 @@ import static org.mockito.Mockito.when;
     url = "${PACT_BROKER_FULL_URL:http://localhost:9292}",
     consumerVersionSelectors = {@VersionSelector(tag = "master")})
 @IgnoreNoPactsToVerify
-//@PactFolder("target/pacts/reform/employmenttribunals/GetCaseProviderTest")
+//@PactFolder("target/pacts/reform/employmenttribunals_aka_cases/GetCaseProviderTest")
 @ExtendWith(SpringExtension.class)
 public class CasesProviderTest {
 
