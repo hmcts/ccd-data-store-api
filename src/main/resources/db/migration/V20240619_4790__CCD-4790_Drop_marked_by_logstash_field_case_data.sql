763e958389828f1f03f3f55bcace78a99b09e2ee
RebeccaBaker <38425793+RebeccaBaker@users.noreply.github.com>
2024-11-12 11:04:22 +0000

    Update V20240619_4790__CCD-4790_Drop_marked_by_logstash_field_case_data.sql

--- src/main/resources/db/migration/V20240619_4790__CCD-4790_Drop_marked_by_logstash_field_case_data.sql
+++ src/main/resources/db/migration/V20240619_4790__CCD-4790_Drop_marked_by_logstash_field_case_data.sql
@@ -1,3 +1,11 @@
+--
+-- drop old set_case_data_marked_by_logstash
+-- Name: insert_update_logstash_queue(); Type: FUNCTION; Schema: public; Owner: ccd 
+
+DROP TRIGGER IF EXISTS trg_case_data_updated ON public.case_data;
+
+DROP FUNCTION IF exists set_case_data_marked_by_logstash; 
+
 -- drop INDEX idx_case_data_marked_by_logstash
 -- remove column marked_by_logstash from case_data
 -- as now replaced with case_data_logstash_queue table
