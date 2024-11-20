4cc7dcaf2ed6c9388d5dc951f66099d530d42c1f
patelila <ila.patel@hmcts.net>
2024-11-20 16:41:22 +0000

    removed Ignore as causing ERROR: Pipeline aborted due to quality gate failure: ERROR

--- src/test/java/uk/gov/hmcts/ccd/endpoint/ui/QueryEndpointIT.java
+++ src/test/java/uk/gov/hmcts/ccd/endpoint/ui/QueryEndpointIT.java
@@ -1745,7 +1745,6 @@ public class QueryEndpointIT extends WireMockBaseTest {
                .andReturn();
     }
 
-    @Ignore
     @Test
     @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
         scripts = {"classpath:sql/insert_case_event_history_external.sql"})
