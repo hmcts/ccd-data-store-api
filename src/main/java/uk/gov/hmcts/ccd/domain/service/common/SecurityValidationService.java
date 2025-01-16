f9e7bf362128ef06433dad36939570d5e7ac37d8
kaanaktas <aktaskaan@gmail.com>
2025-01-16 09:56:23 +0000

    improve the SecurityClassification update flow to avoid unnecessary checks for dataClassification in cases where it's not needed

--- src/main/java/uk/gov/hmcts/ccd/domain/service/common/SecurityValidationService.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/SecurityValidationService.java
@@ -38,6 +38,12 @@ public class SecurityValidationService {
         this.authorisedGetCaseOperation = authorisedGetCaseOperation;
     }
 
+    public void updateSecurityClassificationIfValid(CallbackResponse callbackResponse, CaseDetails caseDetails) {
+        if (callbackResponse.getSecurityClassification() != null) {
+            caseDetails.setSecurityClassification(callbackResponse.getSecurityClassification());
+        }
+    }
+
     public void setDataClassificationFromCallbackIfValid(final CallbackResponse callbackResponse,
                                                          final CaseDetails caseDetails,
                                                          final Map<String, JsonNode> deducedDataClassification) {
@@ -60,21 +66,6 @@ public class SecurityValidationService {
         }
 
         caseDetails.setDataClassification(JacksonUtils.convertValue(callbackResponse.getDataClassification()));
-    }
-
-    public void updateSecurityClassificationIfValid(CallbackResponse callbackResponse, CaseDetails caseDetails) {
-        if (caseHasClassificationEqualOrLowerThan(callbackResponse.getSecurityClassification()).test(caseDetails)) {
-            caseDetails.setSecurityClassification(callbackResponse.getSecurityClassification());
-        } else {
-            LOG.warn("CallbackCaseClassification={} has lower classification than caseClassification={} for "
-                    + "caseReference={}, jurisdiction={} and caseType={}",
-                callbackResponse.getSecurityClassification(),
-                caseDetails.getSecurityClassification(),
-                caseDetails.getReference(),
-                caseDetails.getJurisdiction(),
-                caseDetails.getCaseTypeId());
-            throw new ValidationException(VALIDATION_ERR_MSG);
-        }
     }
 
     private void validateObject(JsonNode callbackDataClassification, JsonNode defaultDataClassification) {
