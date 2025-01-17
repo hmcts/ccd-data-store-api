35619fb9263971f8a165a29122d570476e8d8b74
dharmendra kumar <dharmendra.kumar@hmcts.net>
2025-01-17 15:25:43 +0000

    Add request attributes in POC client

--- src/main/java/uk/gov/hmcts/ccd/clients/PocApiClient.java
+++ src/main/java/uk/gov/hmcts/ccd/clients/PocApiClient.java
@@ -6,7 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.ccd.customheaders.UserAuthHeadersInterceptorConfig;
 import uk.gov.hmcts.ccd.domain.model.aggregated.POCCaseEvent;
 import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.RoleAssignments;
@@ -17,12 +19,12 @@ import uk.gov.hmcts.ccd.domain.model.std.AuditEvent;
 public interface PocApiClient {
 
     @GetMapping(value = "/ccd/cases/{case-ref}")
-    CaseDetails getCase(@PathVariable("case-ref") String caseRef, RoleAssignments roleAssignments);
+    CaseDetails getCase(@PathVariable("case-ref") String caseRef, @RequestAttribute("roleAssignments") RoleAssignments roleAssignments);
 
     @PostMapping(value = "/ccd/cases", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    CaseDetails createEvent(@RequestBody POCCaseEvent caseEvent, RoleAssignments roleAssignments);
+    CaseDetails createEvent(@RequestBody POCCaseEvent caseEvent, @RequestAttribute("roleAssignments") RoleAssignments roleAssignments);
 
     @GetMapping(value = "/ccd/cases/{case-ref}/history")
-    List<AuditEvent> getEvents(@PathVariable("case-ref") String caseReference, RoleAssignments roleAssignments);
+    List<AuditEvent> getEvents(@PathVariable("case-ref") String caseReference, @RequestAttribute("roleAssignments") RoleAssignments roleAssignments);
 }
