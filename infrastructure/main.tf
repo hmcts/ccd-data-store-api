0a85a959d210a0f9f26101ca1097ad83d8e05cb1
Sammi Chong <shinyee.chong@hmcts.net>
2025-04-08 16:29:06 +0100

    Add thresholds

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,6 +17,8 @@ locals {
 
   sharedASPResourceGroup = "${var.raw_product}-shared-${var.env}"
 
+  db_name = "${local.app_full_name}-postgres-db-v15"
+
 }
 
 data "azurerm_key_vault" "ccd_shared_key_vault" {
@@ -115,12 +117,18 @@ module "postgresql_v15" {
     }
 
   ]
-  pgsql_version    = "15"
-  product          = var.product
-  name             = "${local.app_full_name}-postgres-db-v15"
-  pgsql_sku        = var.pgsql_sku
-  pgsql_storage_mb = var.pgsql_storage_mb
-  auto_grow_enabled = var.auto_grow_enabled
+  pgsql_version               = "15"
+  product                     = var.product
+  name                        = local.db_name
+  pgsql_sku                   = var.pgsql_sku
+  pgsql_storage_mb            = var.pgsql_storage_mb
+  auto_grow_enabled           = var.auto_grow_enabled
+  action_group_name           = join("-", [var.action_group_name, local.db_name, var.env])
+  email_address_key           = var.email_address_key
+  email_address_key_vault_id  = data.azurerm_key_vault.ccd_shared_key_vault.id
+  cpu_threshold               = var.cpu_threshold
+  memory_threshold            = var.memory_threshold
+  storage_threshold           = var.storage_threshold
 }
 
 ////////////////////////////////////
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -105,3 +105,33 @@ variable "kv_subscription" {
   type        = string
   description = "Update this with the name of the subscription where the single server key vault is. Defaults to DCD-CNP-DEV."
 }
+
+variable "action_group_name" {
+  description = "The name of the Action Group to create."
+  type        = string
+  default     = "action_group"
+}
+
+variable "email_address_key" {
+  description = "Email address key in azure Key Vault."
+  type        = string
+  default     = "db-alert-monitoring-email-address"
+}
+
+variable "cpu_threshold" {
+  default     = 5
+  type        = number
+  description = "Average CPU utilisation threshold"
+}
+
+variable "memory_threshold" {
+  default     = 5
+  type        = number
+  description = "Average memory utilisation threshold"
+}
+
+variable "storage_threshold" {
+  default     = 0.5
+  type        = number
+  description = "Average storage utilisation threshold"
+}
