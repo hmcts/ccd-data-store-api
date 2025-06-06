8434154962d2ea58c68de62be2d9bf391bf29844
Dinesh Patel <dinesh1.patel@btinternet.com>
2025-06-06 09:42:54 +0100

    revert changes in SwaggerConfiguration

--- src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
@@ -110,20 +110,6 @@ public class SwaggerConfiguration {
             .build();
     }
 
-    /*
-    private RequestParameter queryParameters() {
-        return new RequestParameterBuilder()
-            .name("queryParameters")
-            .description("created_date, last_modified, "  +
-                "jurisdiction, case_type_id, state," +
-                " reference")
-            .in("query")
-            .accepts(Collections.singleton(MediaType.APPLICATION_JSON))
-            .required(true)
-            .build();
-    }
-    */
-
     private RequestParameter headerServiceAuthorization() {
         return new RequestParameterBuilder()
             .name("ServiceAuthorization")
