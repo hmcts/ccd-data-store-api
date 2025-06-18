8680f75829933914fd6b3fe724ced544b3668571
Alex McAusland <a.m@user.com>
2025-06-18 20:41:05 +0100

    remove classification field level filtering impl

--- src/main/java/uk/gov/hmcts/ccd/domain/service/common/SecurityClassificationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/SecurityClassificationServiceImpl.java
@@ -1,14 +1,8 @@
 package uk.gov.hmcts.ccd.domain.service.common;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.Maps;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import org.slf4j.Logger;
@@ -28,16 +22,9 @@ import uk.gov.hmcts.ccd.domain.service.casedataaccesscontrol.CaseDataAccessContr
 import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Comparator.comparingInt;
 import static uk.gov.hmcts.ccd.domain.service.common.SecurityClassificationUtils.caseHasClassificationEqualOrLowerThan;
-import static uk.gov.hmcts.ccd.domain.service.common.SecurityClassificationUtils.getDataClassificationForData;
-import static uk.gov.hmcts.ccd.domain.service.common.SecurityClassificationUtils.getSecurityClassification;
 
 @Service
 public class SecurityClassificationServiceImpl implements SecurityClassificationService {
-
-    private static final JsonNodeFactory JSON_NODE_FACTORY = new JsonNodeFactory(false);
-    private static final String VALUE = "value";
-    private static final String CLASSIFICATION = "classification";
-    private static final ObjectNode EMPTY_NODE = JSON_NODE_FACTORY.objectNode();
 
     private static final Logger LOG = LoggerFactory.getLogger(SecurityClassificationServiceImpl.class);
 
@@ -122,111 +109,5 @@ public class SecurityClassificationServiceImpl implements SecurityClassification
         return classifications.stream()
             .filter(classification -> classification != null)
             .max(comparingInt(SecurityClassification::getRank));
-    }
-
-    private JsonNode filterNestedObject(JsonNode data, JsonNode dataClassification,
-                                        SecurityClassification userClassification) {
-        if (isAnyNull(data, dataClassification)) {
-            return EMPTY_NODE;
-        }
-        Iterator<Map.Entry<String, JsonNode>> dataIterator = data.fields();
-        while (dataIterator.hasNext()) {
-            Map.Entry<String, JsonNode> dataElement = dataIterator.next();
-            String dataElementKey = dataElement.getKey();
-            JsonNode dataClassificationElement = dataClassification.get(dataElementKey);
-            if (isAnyNull(dataClassificationElement)) {
-                dataIterator.remove();
-            } else if (dataClassificationElement.has(VALUE)) {
-                JsonNode dataClassificationValue = dataClassificationElement.get(VALUE);
-                JsonNode dataElementValue = dataElement.getValue();
-                if (dataClassificationValue.isObject()) {
-                    filterObject(userClassification, dataIterator, dataClassificationElement, dataElementValue);
-                } else {
-                    filterCollection(userClassification,
-                        dataIterator,
-                        dataClassificationElement,
-                        dataElementValue);
-                }
-            } else if (dataClassificationElement.isTextual()) {
-                filterSimpleField(userClassification, dataIterator, dataClassificationElement);
-            } else {
-                dataIterator.remove();
-            }
-        }
-        return data;
-    }
-
-    private void filterCollection(SecurityClassification userClassification,
-                                  Iterator<Map.Entry<String, JsonNode>> dataIterator,
-                                  JsonNode dataClassificationElement,
-                                  JsonNode dataElementValue) {
-        // Apply collection-level classification
-        filterSimpleField(userClassification,
-            dataIterator,
-            dataClassificationElement.get(CLASSIFICATION));
-
-        Iterator<JsonNode> dataCollectionIterator = dataElementValue.iterator();
-        while (dataCollectionIterator.hasNext()) {
-            JsonNode collectionElement = dataCollectionIterator.next();
-            JsonNode dataClassificationForData = getDataClassificationForData(collectionElement,
-                dataClassificationElement.get(VALUE).iterator());
-            if (dataClassificationForData.isNull()) {
-                dataCollectionIterator.remove();
-                continue;
-            }
-            JsonNode relevantDataClassificationValue = dataClassificationForData.get(VALUE);
-            JsonNode collectionElementValue = collectionElement.get(VALUE);
-            if (relevantDataClassificationValue != null) {
-                if (collectionElementValue.isObject()) {
-                    filterNestedObject(collectionElementValue,
-                        relevantDataClassificationValue,
-                        userClassification);
-                } else {
-                    LOG.warn("Invalid security classification structure for collection item: {}",
-                        relevantDataClassificationValue.toString());
-                    dataCollectionIterator.remove();
-                }
-            } else {
-                // For collection of simple field type, the classification is stored as `classification`, not `value`
-                relevantDataClassificationValue = dataClassificationForData.get(CLASSIFICATION);
-
-                if (null != relevantDataClassificationValue) {
-                    filterSimpleField(userClassification,
-                        dataCollectionIterator,
-                        relevantDataClassificationValue);
-                } else {
-                    dataCollectionIterator.remove();
-                }
-            }
-            if (collectionElementValue.equals(EMPTY_NODE)) {
-                dataCollectionIterator.remove();
-            }
-        }
-    }
-
-    private void filterObject(SecurityClassification userClassification,
-                              Iterator<Map.Entry<String, JsonNode>> dataIterator,
-                              JsonNode dataClassificationParent,
-                              JsonNode dataElementValue) {
-        filterNestedObject(dataElementValue,
-            dataClassificationParent.get(VALUE),
-            userClassification);
-        if (dataElementValue.equals(EMPTY_NODE)) {
-            filterSimpleField(userClassification,
-                dataIterator,
-                dataClassificationParent.get(CLASSIFICATION));
-        }
-    }
-
-    private void filterSimpleField(SecurityClassification userClassification, Iterator iterator,
-                                   JsonNode dataClassificationValue) {
-        Optional<SecurityClassification> securityClassification = getSecurityClassification(dataClassificationValue);
-        if (!securityClassification.isPresent() || !userClassification.higherOrEqualTo(securityClassification.get())) {
-            iterator.remove();
-        }
-    }
-
-    private boolean isAnyNull(JsonNode... jsonNodes) {
-        return newArrayList(jsonNodes).stream().anyMatch(Objects::isNull);
     }
 }
