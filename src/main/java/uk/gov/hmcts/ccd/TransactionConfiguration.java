7e868255d5a82a085d732b0d6ce6ca62d38dd2a4
JamesCollettCGI <james.collett@hmcts.net>
2025-06-24 12:16:42 +0100

    Revert temporary changes and set default DATA_STORE_TX_TIMEOUT_DEFAULT value to 30 seconds

--- src/main/java/uk/gov/hmcts/ccd/TransactionConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/TransactionConfiguration.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.ccd;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,21 +13,12 @@ import javax.persistence.EntityManagerFactory;
 @EnableTransactionManagement
 public class TransactionConfiguration {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TransactionConfiguration.class);
-
-    private void jclog(String message) {
-        LOG.info("JCDEBUG: Info: TransactionConfiguration: {}", message);
-    }
-
-    // JC Note: DATA_STORE_TX_TIMEOUT_DEFAULT does not appear to be defined in cnp-flux-config or data-store values.yaml
-
     @Bean
     public PlatformTransactionManager transactionManager(final EntityManagerFactory emf,
                                                          @Value("${ccd.tx-timeout.default}") String defaultTimeout) {
         final JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(emf);
         transactionManager.setDefaultTimeout(Integer.parseInt(defaultTimeout));
-        jclog("getDefaultTimeout = " + transactionManager.getDefaultTimeout());
         return transactionManager;
     }
 
