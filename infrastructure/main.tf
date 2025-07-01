ccbd70594680aa3750c68142da92b34406cc28b8
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2025-07-01 16:14:41 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -87,6 +87,7 @@ module "postgresql_v15" {
   force_user_permissions_trigger     = "2"
   enable_db_report_privileges        = true
   kv_subscription                    = var.kv_subscription
+  kv_name                            = data.azurerm_key_vault.ccd_shared_key_vault.name
   user_secret_name                   = azurerm_key_vault_secret.POSTGRES-USER-V15.name
   pass_secret_name                   = azurerm_key_vault_secret.POSTGRES-PASS-V15.name
   force_db_report_privileges_trigger = "1"
