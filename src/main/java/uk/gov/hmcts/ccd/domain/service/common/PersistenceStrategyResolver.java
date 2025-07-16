c0f915cdcabd12afd7e0a8436ecae53f78df439b
Alex McAusland <a.m@user.com>
2025-07-16 16:29:46 +0100

    trim unused

--- src/main/java/uk/gov/hmcts/ccd/domain/service/common/PersistenceStrategyResolver.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/PersistenceStrategyResolver.java
@@ -38,17 +38,22 @@ public class PersistenceStrategyResolver {
     @NotNull
     private Map<String, URI> caseTypeServiceUrls;
 
+
+    @Autowired
+    public PersistenceStrategyResolver(@Qualifier(DefaultCaseDetailsRepository.QUALIFIER)
+                                           CaseDetailsRepository caseDetailsRepository ) {
+        this.caseDetailsRepository = caseDetailsRepository;
+    }
+
+    /**
+     * Sets the case type service URLs when the application starts.
+     * The keys in the map are converted to lowercase to ensure case-insensitivity.
+     */
     public void setCaseTypeServiceUrls(Map<String, URI> caseTypeServiceUrls) {
         this.caseTypeServiceUrls = new HashMap<>();
         caseTypeServiceUrls.forEach((key, value) ->
             this.caseTypeServiceUrls.put(key.toLowerCase(), value)
         );
-    }
-
-    @Autowired
-    public PersistenceStrategyResolver(@Qualifier(DefaultCaseDetailsRepository.QUALIFIER) CaseDetailsRepository caseDetailsRepository
-    ) {
-        this.caseDetailsRepository = caseDetailsRepository;
     }
 
     public boolean isDecentralised(CaseDetails details) {
@@ -62,36 +67,6 @@ public class PersistenceStrategyResolver {
     public boolean isDecentralised(String reference) {
         var details = caseDetailsRepository.findByReferenceWithNoAccessControl(reference).get();
         return getCaseTypeServiceUrl(details.getCaseTypeId()).isPresent();
-    }
-
-
-
-    /**
-     * Resolves the persistence strategy for a given case reference.
-     * <p>
-     * It first retrieves the case details to determine the case type.
-     * The `CachedCaseDetailsRepository` is used to ensure this lookup is efficient.
-     * It then checks the configuration to see if the case type has a decentralised
-     * persistence URL associated with it.
-     *
-     * @param caseReference The unique 16-digit reference of the case.
-     * @return An {@code Optional<String>} containing the base URL of the owning
-     *         service if the case type is decentralised. Otherwise, returns
-     *         an empty Optional to indicate that the case should be handled
-     *         by the internal CCD database.
-     */
-    public Optional<URI> resolveUri(String caseReference) {
-
-        // 1. Get CaseDetails using the repository. The injected 'cached' version handles caching.
-        Optional<CaseDetails> caseDetailsOptional = caseDetailsRepository.findByReference(caseReference);
-
-        if (caseDetailsOptional.isEmpty()) {
-            log.warn("Could not find case details for reference: {}. Assuming centralised persistence.", caseReference);
-            return Optional.empty();
-        }
-
-        Optional<URI> url = getCaseTypeServiceUrl(caseDetailsOptional.get().getCaseTypeId());
-        return url;
     }
 
     public URI resolveUriOrThrow(String caseReference) {
