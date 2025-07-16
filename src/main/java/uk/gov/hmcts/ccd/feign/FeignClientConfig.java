e3a640d04a05172fa9360e79a5d58ebe5b47b48f
Priyanka Verma <priyanka.verma@hmcts.net>
2025-07-16 12:10:44 +0100

    Integration test fix

--- src/main/java/uk/gov/hmcts/ccd/feign/FeignClientConfig.java
+++ src/main/java/uk/gov/hmcts/ccd/feign/FeignClientConfig.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-
 @Configuration
 @Slf4j
 public class FeignClientConfig {
