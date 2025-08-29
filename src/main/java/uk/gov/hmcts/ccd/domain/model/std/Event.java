23b2aa093d091d3fc4249f368aa92edf8ad94348
JamesCollettCGI <james.collett@hmcts.net>
2025-08-29 12:01:58 +0100

    Add initial logging to Event.java

--- src/main/java/uk/gov/hmcts/ccd/domain/model/std/Event.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/model/std/Event.java
@@ -2,9 +2,12 @@ package uk.gov.hmcts.ccd.domain.model.std;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.ToString;
+import uk.gov.hmcts.ccd.domain.service.common.JcLogger;
 
 @ToString
 public class Event {
+    private final JcLogger jclogger = new JcLogger("Event", true);
+
     @JsonProperty("id")
     private String eventId;
     @JsonProperty("summary")
@@ -12,27 +15,38 @@ public class Event {
     @JsonProperty("description")
     private String description;
 
+    private void jcdebug(final String method, final String value) {
+        jclogger.jclog(method + " " + value);
+        jclogger.jclog(method + " " + jclogger.getCallStackAsString());
+    }
+
     public String getEventId() {
+        jcdebug("getEventId()", eventId);
         return eventId;
     }
 
     public void setEventId(String eventId) {
+        jcdebug("setEventId()", eventId);
         this.eventId = eventId;
     }
 
     public String getSummary() {
+        jcdebug("getSummary()", summary);
         return summary;
     }
 
     public void setSummary(String summary) {
+        jcdebug("setSummary()", summary);
         this.summary = summary;
     }
 
     public String getDescription() {
+        jcdebug("getDescription()", description);
         return description;
     }
 
     public void setDescription(String description) {
+        jcdebug("setDescription()", description);
         this.description = description;
     }
 
---
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/common/JcLogger.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.ccd.domain.service.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import uk.gov.hmcts.ccd.config.JacksonUtils;
+
+public class JcLogger {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JcLogger.class);
+    private static final ObjectMapper MAPPER = JacksonUtils.MAPPER;
+
+    private final String className;
+    private final boolean logEnabled;
+
+    public JcLogger(final String className, final boolean logEnabled) {
+        this.className = className;
+        this.logEnabled = logEnabled;
+    }
+
+    /**
+     * Log message.
+     */
+    public void jclog(String message) {
+        if (logEnabled) {
+            LOG.info("| JCDEBUG: Info: {}: {}", className, message);
+            // LOG.warn("| JCDEBUG: Warn: {}: {}", className, message);
+            // LOG.error("| JCDEBUG: Error: {}: {}", className, message);
+            // LOG.debug("| JCDEBUG: Debug: {}: {}", className, message);
+        }
+    }
+
+    /**
+     * Get call stack as String (tab separated).
+     */
+    public String getCallStackAsString() {
+        final StringBuilder sb = new StringBuilder();
+        final StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        // Skip the first two elements to exclude getStackTrace() and getCallStackAsString()
+        for (int i = 2; i < stackTrace.length; i++) {
+            sb.append(stackTrace[i].toString()).append("\t");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Print Object to String (in JSON format).
+     */
+    public String printObjectToString(final Object object) {
+        try {
+            return MAPPER.writeValueAsString(object);
+        } catch (Exception e) {
+            return "ERROR_WRITING_OBJECT";
+        }
+    }
+
+}
