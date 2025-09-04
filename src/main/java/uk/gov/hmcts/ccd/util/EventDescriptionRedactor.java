14c155f043162e7a913785ff4b4bdac2f95fb29d
JamesCollettCGI <james.collett@hmcts.net>
2025-09-04 16:27:36 +0100

    Sonar temporary exclude EventDescriptionRedactor.java

--- src/main/java/uk/gov/hmcts/ccd/util/EventDescriptionRedactor.java
+++ src/main/java/uk/gov/hmcts/ccd/util/EventDescriptionRedactor.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.ccd.util;
 
 public class EventDescriptionRedactor {
+    // TODO: Resolve compliance with S5852 (polynomial runtime regex).
+    //       See EmailValidator , EmailValidatorTest , BaseType.
     private static final String EMAIL_PATTERN = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
 
     public String redact(final String description) {
