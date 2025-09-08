f626eaf2716d0fe054a3a1e81597927d6f80782c
Alex McAusland <a.m@user.com>
2025-09-08 20:35:27 +0100

    tidy

--- src/main/java/uk/gov/hmcts/ccd/data/persistence/dto/DecentralisedCaseEvent.java
+++ src/main/java/uk/gov/hmcts/ccd/data/persistence/dto/DecentralisedCaseEvent.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.ccd.data.persistence.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
@@ -10,17 +10,10 @@ import uk.gov.hmcts.ccd.domain.model.definition.CaseDetails;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Data
 @Builder
+@AllArgsConstructor
 public class DecentralisedCaseEvent {
 
     private CaseDetails caseDetailsBefore;
     private CaseDetails caseDetails;
     private DecentralisedEventDetails eventDetails;
-
-    @JsonCreator
-    public DecentralisedCaseEvent(CaseDetails caseDetailsBefore, CaseDetails caseDetails,
-                                  DecentralisedEventDetails eventDetails) {
-        this.caseDetailsBefore = caseDetailsBefore;
-        this.caseDetails = caseDetails;
-        this.eventDetails = eventDetails;
-    }
 }
