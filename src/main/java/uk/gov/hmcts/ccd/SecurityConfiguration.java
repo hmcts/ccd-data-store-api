de69c876fafbd18c2941057c507b1f6e2eb32f93
Dinesh Patel <dinesh1.patel@btinternet.com>
2025-09-24 14:49:15 +0100

    update config and tests

--- src/main/java/uk/gov/hmcts/ccd/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/SecurityConfiguration.java
@@ -62,8 +62,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         "/swagger-resources/**",
         "/swagger-ui/**",
         "/webjars/**",
-        "/v3/api-docs",
-        "/v3/api-docs/**",
+        "/v2/api-docs",
+        "/v2/api-docs/**",
         "/testing-support/cleanup-case-type/**"
     };
 
