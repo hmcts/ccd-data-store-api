4822cd4cee2d9104007bb18a142faf5c21d57ab9
patelila <ila.patel@hmcts.net>
2025-12-09 16:31:14 +0000

    checkstyle error fix

--- src/main/java/uk/gov/hmcts/ccd/domain/service/caselinking/CaseLinkService.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/caselinking/CaseLinkService.java
@@ -7,8 +7,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import uk.gov.hmcts.ccd.data.casedetails.CaseDetailsRepository;
 import uk.gov.hmcts.ccd.data.casedetails.DefaultCaseDetailsRepository;
 import uk.gov.hmcts.ccd.data.caselinking.CaseLinkEntity;
