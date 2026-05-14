a24bbdd4cb80c28edabe7039fa19ab1f4bacd65b
Sammi Chong <133241942+SammiChong@users.noreply.github.com>
2026-05-14 10:24:13 +0100

    Update DateCaseClosedRepository.java

--- src/main/java/uk/gov/hmcts/ccd/data/caseclosed/DateCaseClosedRepository.java
+++ src/main/java/uk/gov/hmcts/ccd/data/caseclosed/DateCaseClosedRepository.java
@@ -2,14 +2,12 @@ package uk.gov.hmcts.ccd.data.caseclosed;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Transactional
 @Repository
 public interface DateCaseClosedRepository extends CrudRepository<DateCaseClosedEntity, Long> {
 
-    List<DateCaseClosedEntity> findClosedCasesByStateChangedDate(LocalDateTime stateChangedDate);
+    List<DateCaseClosedEntity> findByStateChangedDateLessThanEqual(LocalDateTime stateChangedDate);
 }
