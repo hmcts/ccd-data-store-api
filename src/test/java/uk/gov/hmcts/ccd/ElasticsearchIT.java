b62088de5a2a09618b2a7439670f8c446aab8196
markdathornehmcts <mark.dathorne@hmcts.net>
2026-06-29 15:42:04 +0100

    CCD-5867 merge master changes to fix build

--- src/test/java/uk/gov/hmcts/ccd/ElasticsearchIT.java
+++ src/test/java/uk/gov/hmcts/ccd/ElasticsearchIT.java
@@ -569,7 +569,7 @@ public class ElasticsearchIT extends ElasticsearchBaseTest {
             ElasticsearchTestRequest searchRequest = caseReferenceRequest(DEFAULT_CASE_REFERENCE);
 
             CaseSearchResultViewResource caseSearchResultViewResource = executeRequest(searchRequest, CASE_TYPE_A,
-                "TEST");
+                "TEST", false);
             SearchResultViewItem caseDetails = caseSearchResultViewResource.getCases().getFirst();
             assertAll(
                 () -> assertThat(caseSearchResultViewResource.getTotal(), is(1L)),
@@ -584,7 +584,7 @@ public class ElasticsearchIT extends ElasticsearchBaseTest {
             ElasticsearchTestRequest searchRequest = caseReferenceRequest(DEFAULT_CASE_REFERENCE);
 
             CaseSearchResultViewResource caseSearchResultViewResource =
-                executeRequest(searchRequest, CASE_TYPE_A, "RDM-8782");
+                executeRequest(searchRequest, CASE_TYPE_A, "RDM-8782", false);
             SearchResultViewItem caseDetails = caseSearchResultViewResource.getCases().getFirst();
 
             List<String> expectedFields = Collections.singletonList(EMAIL_FIELD);
@@ -612,7 +612,7 @@ public class ElasticsearchIT extends ElasticsearchBaseTest {
             ElasticsearchTestRequest searchRequest = caseReferenceRequest(DEFAULT_CASE_REFERENCE);
 
             CaseSearchResultViewResource caseSearchResultViewResource = executeRequest(searchRequest, CASE_TYPE_A,
-                "RDM-8782NOACCESS");
+                "RDM-8782NOACCESS", false);
             SearchResultViewItem caseDetails = caseSearchResultViewResource.getCases().getFirst();
 
             assertAll(
