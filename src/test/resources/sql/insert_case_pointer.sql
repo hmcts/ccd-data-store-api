db46f5829735d3283ef20ad38516a31754378501
patelila <ila.patel@hmcts.net>
2026-07-02 14:53:12 +0100

    Fix case pointer test fixtures for logstash marker constraint

--- src/test/resources/sql/insert_case_pointer.sql
+++ src/test/resources/sql/insert_case_pointer.sql
@@ -1,7 +1,7 @@
 DELETE FROM case_event;
 DELETE FROM case_data;
 
-INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, created_date, last_modified, last_state_modified_date)
+INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, created_date, last_modified, last_state_modified_date, marked_by_logstash)
 VALUES (1, 'PCS', 'PROBATE', '', 'RESTRICTED',
         '{
         }',
@@ -10,5 +10,6 @@ VALUES (1, 'PCS', 'PROBATE', '', 'RESTRICTED',
        '1644062237356399',
        '2016-06-22 20:44:52.824',
        '2016-06-24 20:44:52.824',
-       '2016-06-24 20:44:52.824'
+       '2016-06-24 20:44:52.824',
+       true
 );
