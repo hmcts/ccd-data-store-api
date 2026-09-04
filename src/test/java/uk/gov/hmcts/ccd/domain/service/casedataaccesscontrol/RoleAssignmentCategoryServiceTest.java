ba08148b05288b310f3b74e71ae05fe3dad24d93
padmapriyanalam <padmapriya.nalam@gmail.com>
2026-09-04 10:32:52 +0100

    POFCC-155 -New Enforcement category Test

--- src/test/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentCategoryServiceTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentCategoryServiceTest.java
@@ -11,12 +11,19 @@ import uk.gov.hmcts.ccd.data.casedataaccesscontrol.RoleAssignmentRepository;
 import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.RoleCategory;
 import uk.gov.hmcts.ccd.security.idam.IdamRepository;
 
+import uk.gov.hmcts.ccd.data.casedataaccesscontrol.RoleAssignmentResponse;
+import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.RoleAssignment;
+import uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.RoleAssignments;
+
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.GrantType.STANDARD;
 import static uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.RoleCategory.CITIZEN;
+import static uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.RoleCategory.ENFORCEMENT;
 import static uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.RoleCategory.JUDICIAL;
 import static uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.RoleCategory.LEGAL_OPERATIONS;
 import static uk.gov.hmcts.ccd.domain.model.casedataaccesscontrol.enums.RoleCategory.PROFESSIONAL;
@@ -108,6 +115,29 @@ class RoleAssignmentCategoryServiceTest {
             RoleCategory roleCategory = roleAssignmentCategoryService.getRoleCategory(USER_ID);
 
             assertThat(roleCategory, is(LEGAL_OPERATIONS));
+        }
+
+        @Test
+        void shouldGetRoleCategoryForEnforcementUser() {
+
+            given(idamRepository.getUserRoles(USER_ID))
+                .willReturn(singletonList("some-user"));
+
+            RoleAssignment enforcementRole = RoleAssignment.builder()
+                .roleName("bailiff-manager")
+                .grantType(STANDARD.name())
+                .build();
+
+            given(roleAssignmentRepository.getRoleAssignments(USER_ID))
+                .willReturn(new RoleAssignmentResponse());
+            given(roleAssignmentsMapper.toRoleAssignments(any(RoleAssignmentResponse.class)))
+                .willReturn(RoleAssignments.builder()
+                    .roleAssignments(singletonList(enforcementRole))
+                    .build());
+
+            RoleCategory roleCategory = roleAssignmentCategoryService.getRoleCategory(USER_ID);
+
+            assertThat(roleCategory, is(ENFORCEMENT));
         }
 
     }
