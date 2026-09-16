e49572b0976aea4a9b96f9fe1712e2c2e1916ee6
matt-goodwin-solirius <matt.goodwin@solirius.com>
2026-09-16 16:09:54 +0100

    Added the requested test covering roleAssignmentsMapper.toRoleAssignments() returning null

--- src/test/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentServiceTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/service/casedataaccesscontrol/RoleAssignmentServiceTest.java
@@ -531,6 +531,23 @@ class RoleAssignmentServiceTest {
             assertTrue(caseAssignedUserRole.isEmpty());
         }
 
+        @Test
+        void shouldReturnEmptyWhenMappedRoleAssignmentsAreNull() {
+
+            // GIVEN
+            given(roleAssignmentRepository.findRoleAssignmentsByCasesAndUsers(caseIds, userIds))
+                .willReturn(mockedRoleAssignmentResponse);
+            given(roleAssignmentsMapper.toRoleAssignments(mockedRoleAssignmentResponse))
+                .willReturn(null);
+
+            // WHEN
+            final List<CaseAssignedUserRole> caseAssignedUserRole =
+                roleAssignmentService.findRoleAssignmentsByCasesAndUsers(caseIds, userIds);
+
+            // THEN
+            assertTrue(caseAssignedUserRole.isEmpty());
+        }
+
     }
 
 
