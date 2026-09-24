b3a21c644c35c5520f8bee30c44ccbe9fe1a37bc
patelila <ila.patel@hmcts.net>
2026-09-24 12:01:04 +0100

    CCD-4262: include master contracts in Pact provider verification

--- src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/caseassigned/CaseAssignedUserRolesProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/ccd/v2/external/controller/caseassigned/CaseAssignedUserRolesProviderTest.java
@@ -36,7 +36,10 @@ import static org.mockito.Mockito.when;
 
 @Provider("ccdDataStoreAPI_caseAssignedUserRoles")
 @PactBroker(url = "${PACT_BROKER_FULL_URL:http://localhost:9292}",
-    consumerVersionSelectors = {@VersionSelector(tag = "${PACT_BRANCH_NAME:Dev}")},
+    consumerVersionSelectors = {
+        @VersionSelector(tag = "master"),
+        @VersionSelector(tag = "${PACT_BRANCH_NAME:master}")
+    },
     providerTags = "${pactbroker.providerTags:master}",
     enablePendingPacts = "${pactbroker.enablePending:true}"
 )
--- src/contractTest/java/uk/gov/hmcts/reform/ccd/pactprovider/cases/CasesProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/ccd/pactprovider/cases/CasesProviderTest.java
@@ -39,7 +39,10 @@ import static org.mockito.Mockito.when;
 
 @Provider("ccd_data_store_api_cases")
 @PactBroker(url = "${PACT_BROKER_FULL_URL:http://localhost:9292}",
-    consumerVersionSelectors = {@VersionSelector(tag = "${PACT_BRANCH_NAME:Dev}")})
+    consumerVersionSelectors = {
+        @VersionSelector(tag = "master"),
+        @VersionSelector(tag = "${PACT_BRANCH_NAME:master}")
+    })
 @IgnoreNoPactsToVerify
 @ExtendWith(SpringExtension.class)
 public class CasesProviderTest {
